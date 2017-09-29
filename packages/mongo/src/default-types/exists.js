a66a517ca35ac20f6fa3128adb14c0281cf4c099
Decrapifier <drodriguez@smartprocure.us>
2017-09-29 17:57:11 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/2

--- packages/mongo/src/default-types/exists.js
+++ packages/mongo/src/default-types/exists.js
@@ -1,25 +1,35 @@
 module.exports = {
   hasValue: x => true,
-  filter: context => context.data.value ? {
-    $and: [{
-      [context.field]: {
-        $exists: context.data.value,
-        $ne: ''
-      }
-    }, {
-      [context.field]: {
-        $ne: null
-      }
-    }]
-  } : {
-    $or: [{
-      [context.field]: {
-        $exists: false
-      }
-    }, {
-      [context.field]: ''
-    }, {
-      [context.field]: null
-    }]
-  }
+  filter: context =>
+    context.data.value
+      ? {
+          $and: [
+            {
+              [context.field]: {
+                $exists: context.data.value,
+                $ne: '',
+              },
+            },
+            {
+              [context.field]: {
+                $ne: null,
+              },
+            },
+          ],
+        }
+      : {
+          $or: [
+            {
+              [context.field]: {
+                $exists: false,
+              },
+            },
+            {
+              [context.field]: '',
+            },
+            {
+              [context.field]: null,
+            },
+          ],
+        },
 }
--- packages/mongo/src/default-types/results.js
+++ packages/mongo/src/default-types/results.js
@@ -9,33 +9,39 @@ module.exports = {
     let sortField = context.config.sortField || '_score'
     let sortDir = context.config.sortDir || 'desc'
     let sort = {
-        [sortField]: sortDir === 'asc' ? 1 : -1
-      }
+      [sortField]: sortDir === 'asc' ? 1 : -1,
+    }
 
     return Promise.all([
-      search([{
-        $sort: sort
-      }, {
-        $skip: startRecord
-      }, {
-        $limit: pageSize
-      }]),
-      search([{
-        $group: {
-          _id: null,
-          count: {
-            $sum: 1
-          }
-        }
-      }])
+      search([
+        {
+          $sort: sort,
+        },
+        {
+          $skip: startRecord,
+        },
+        {
+          $limit: pageSize,
+        },
+      ]),
+      search([
+        {
+          $group: {
+            _id: null,
+            count: {
+              $sum: 1,
+            },
+          },
+        },
+      ]),
     ]).spread((results, count) => ({
       // TODO - handle aggregate wrapped stuff, e.g. result.result or result.result[0] etc
       response: {
         totalRecords: _.get(count, '0.count'),
         startRecord: startRecord + 1,
         endRecord: startRecord + results.length,
-        results: results
-      }
+        results: results,
+      },
     }))
-  }
+  },
 }
