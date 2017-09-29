0f5f11901cc9326215f72c30c2fdb5d2e107d652
Samuel Greene <sgreene@smartprocure.us>
2017-09-29 03:52:19 +0200

    facet default type

---
+++ packages/provider-mongo/src/default-types/facet.js
@@ -0,0 +1,42 @@
+let _ = require('lodash'),
+  Promise = require('bluebird')
+
+module.exports = {
+  hasValue: context => _.get(context.data, 'values.length'),
+  filter: context => ({
+    [context.field]: {
+      [context.data.mode === 'exclude' ? '$nin' : '$in']: context.data.values
+    }
+  }),
+  result: (context, search) => Promise.all([
+    search([{
+      $group: {
+        _id: '$' + context.field,
+        count: {
+          $sum: 1
+        }
+      }
+    }, {
+      $limit: context.config.size || 10
+    }]),
+    search([{
+      $group: {
+        _id: '$' + context.field
+      }
+    }, {
+      $group: {
+        _id: 1,
+        count: {
+          $sum: 1
+        }
+      }
+    }])
+  ]).spread((options, cardinality) => ({
+    total: 'NOT SUPPORTED YET',
+    cardinality: _.get(cardinality, '0.count'),
+    options: _.map(options, x => ({
+      name: x._id,
+      count: x.count
+    }))
+  }))
+}
