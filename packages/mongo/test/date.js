bdf5be7bc50d00645b8bb59f59cb38a007095d6d
Samuel Greene <sgreene@smartprocure.us>
2017-09-29 04:25:56 +0200

    unit tests for date and missing dependencies

---
+++ packages/mongo/test/date.js
@@ -0,0 +1,58 @@
+let {expect} = require('chai'),
+  {date} = require('../src/types')
+
+let dateBuilder = data => ({
+  key: 'test',
+  type: 'date',
+  field: 'test',
+  data
+})
+
+describe('date', () => {
+  it('should check for values', () => {
+    expect(
+      !!date.hasValue(
+        dateBuilder({
+          from: '2017-09-28'
+        })
+      )
+    ).to.be.true
+    expect(
+      !!date.hasValue(
+        dateBuilder({
+          from: null
+        })
+      )
+    ).to.be.false
+  })
+  describe('filter', () => {
+    it('basic', () => {
+      expect(
+        date.filter(
+          dateBuilder({
+            from: '2017-09-28'
+          })
+        )
+      ).to.deep.equal({
+        test: {
+          $gte: new Date('2017-09-28')
+        }
+      })
+    })
+    it('basic datemath', () => {
+      expect(
+        date.filter(
+          dateBuilder({
+            useDateMath: true,
+            from: '2017-09-28||-1d'
+          })
+        )
+      ).to.deep.equal({
+        test: {
+          $gte: new Date('2017-09-27')
+        }
+      })
+    })
+    // TODO: lastQuarter, thisQuarter, nextQuarter
+  })
+})
