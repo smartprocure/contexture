da5573d3300a77e14e3cfd0240073484144b14ba
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 05:48:24 +0200

    two level match example type

---
+++ packages/provider-elasticsearch/src/example-types/twoLevelMatch.js
@@ -0,0 +1,40 @@
+let _ = require('lodash/fp')
+let esTwoLevel = require('./esTwoLevelAggregation').result
+
+module.exports = {
+  validContext: context =>
+    !!(
+      context.config.key_field &&
+      context.config.value_field &&
+      context.config.key_value
+    ),
+  result: (context, search) => {
+    let filter = {
+      [_.get('key_type', context.config) || 'term']: {
+        [context.config.key_field]: context.config.key_value
+      }
+    }
+    return esTwoLevel(
+      _.merge(
+        {
+          config: {
+            key_type: 'filters',
+            key_data: {
+              filters: {
+                pass: filter,
+                fail: {
+                  bool: {
+                    must_not: filter
+                  }
+                }
+              },
+              field: null
+            }
+          }
+        },
+        context
+      ),
+      search
+    )
+  }
+}
---
+++ packages/provider-elasticsearch/test/example-types/twoLevelMatch.js
@@ -0,0 +1,184 @@
+const twoLevelMatch = require('../../src/example-types/twoLevelMatch'),
+  utils = require('./testUtils')
+let {expect} = require('chai')
+
+describe('twoLevelMatch', function() {
+  describe('validContext', function() {
+    it('should validate a context with config key_field, value_field and key_value', function() {
+      expect(
+        twoLevelMatch.validContext({
+          config: {
+            key_field: true,
+            value_field: true,
+            key_value: true
+          }
+        })
+      ).to.be.true
+    })
+    it('any context missing a config value for key_field, value_field or key_value should be invalid', function() {
+      utils.noValidContexts(twoLevelMatch)([
+        {
+          config: {
+            key_field: true,
+            value_field: true,
+            key_value: false
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            value_field: true
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            key_value: true,
+            value_field: false
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            key_value: true
+          }
+        },
+        {
+          config: {
+            value_field: true,
+            key_value: true,
+            key_field: false
+          }
+        },
+        {
+          config: {
+            value_field: true,
+            key_value: true
+          }
+        }
+      ])
+    })
+  })
+
+  describe('result', function() {
+    const context = {
+      key: 'test',
+      type: 'twoLevelMatch',
+      config: {
+        key_field: 'Vendor.City.untouched',
+        value_field: 'LineItem.TotalPrice',
+        value_type: 'stats',
+        key_value: 'Washington'
+      }
+    }
+
+    const callsArguments = [
+      {
+        aggs: {
+          twoLevelAgg: {
+            aggs: {
+              twoLevelAgg: {
+                stats: {
+                  field: 'LineItem.TotalPrice'
+                }
+              }
+            },
+            filters: {
+              filters: {
+                fail: {
+                  bool: {
+                    must_not: {
+                      term: {
+                        'Vendor.City.untouched': 'Washington'
+                      }
+                    }
+                  }
+                },
+                pass: {
+                  term: {
+                    'Vendor.City.untouched': 'Washington'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    ]
+
+    it('works for stub config values and gets the expected structure (empty results)', function() {
+      return utils.sequentialResultTest(
+        [
+          {
+            aggregations: {
+              twoLevelAgg: {
+                buckets: []
+              }
+            }
+          }
+        ],
+        context,
+        {results: []},
+        callsArguments
+      )
+    })
+    it('works for stub config values and gets the expected structure (two results)', function() {
+      return utils.sequentialResultTest(
+        [
+          {
+            aggregations: {
+              twoLevelAgg: {
+                buckets: {
+                  pass: {
+                    doc_count: 50,
+                    twoLevelAgg: {
+                      count: 6,
+                      min: 60,
+                      max: 98,
+                      avg: 78.5,
+                      sum: 471
+                    }
+                  },
+                  fail: {
+                    doc_count: 50,
+                    twoLevelAgg: {
+                      count: 6,
+                      min: 60,
+                      max: 98,
+                      avg: 78.5,
+                      sum: 471
+                    }
+                  }
+                }
+              }
+            }
+          }
+        ],
+        context,
+        {
+          results: [
+            {
+              avg: 78.5,
+              count: 6,
+              doc_count: 50,
+              key: 'pass',
+              max: 98,
+              min: 60,
+              sum: 471
+            },
+            {
+              avg: 78.5,
+              count: 6,
+              doc_count: 50,
+              key: 'fail',
+              max: 98,
+              min: 60,
+              sum: 471
+            }
+          ]
+        },
+        callsArguments
+      )
+    })
+  })
+})
