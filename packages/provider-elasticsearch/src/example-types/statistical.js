f56917a7c5df5288bae77753c3218f7384d3bd58
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 06:26:18 +0200

    statistical example type

---
+++ packages/provider-elasticsearch/src/example-types/statistical.js
@@ -0,0 +1,14 @@
+let _ = require('lodash/fp')
+
+module.exports = {
+  result: (context, search) =>
+    search({
+      aggs: {
+        statistical: {
+          stats: {
+            field: _.get('config.field', context) || context.field
+          }
+        }
+      }
+    }).then(results => results.aggregations.statistical)
+}
---
+++ packages/provider-elasticsearch/test/example-types/statistical.js
@@ -0,0 +1,47 @@
+let sequentialResultTest = require('./testUtils').sequentialResultTest
+
+describe('statistical', function() {
+  let statsTest = (...x) =>
+    sequentialResultTest(
+      [
+        {
+          aggregations: {
+            statistical: {
+              count: 6,
+              min: 60,
+              max: 98,
+              avg: 78.5,
+              sum: 471
+            }
+          }
+        }
+      ],
+      ...x
+    )
+  it('should work', () =>
+    statsTest(
+      {
+        key: 'test',
+        type: 'statistical',
+        field: 'LineItem.TotalPrice'
+      },
+      {
+        count: 6,
+        min: 60,
+        max: 98,
+        avg: 78.5,
+        sum: 471
+      },
+      [
+        {
+          aggs: {
+            statistical: {
+              stats: {
+                field: 'LineItem.TotalPrice'
+              }
+            }
+          }
+        }
+      ]
+    ))
+})
