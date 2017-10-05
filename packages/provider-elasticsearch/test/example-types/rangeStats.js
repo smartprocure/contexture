a0c6ce02e61066115edd5f7151206dca61f8d6bc
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 06:20:10 +0200

    range stats example type

---
+++ packages/provider-elasticsearch/test/example-types/rangeStats.js
@@ -0,0 +1,183 @@
+const rangeStats = require('../../src/example-types/rangeStats'),
+  _ = require('lodash/fp'),
+  utils = require('./testUtils')
+
+describe('rangeStats', () => {
+  describe('validContext', () => {
+    it('should validate a context with config key_field, value_field and ranges', () => {
+      utils.validContexts(rangeStats)([
+        {
+          config: {
+            key_field: true,
+            value_field: true,
+            ranges: true
+          }
+        },
+        {
+          config: {
+            key_field: 'key_field',
+            value_field: 'value_field',
+            ranges: [1]
+          }
+        }
+      ])
+    })
+    it('should validate a context without config key_field, value_field or ranges', () => {
+      utils.noValidContexts(rangeStats)([
+        {
+          config: {
+            key_field: true,
+            value_field: true,
+            ranges: false
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            value_field: true
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            value_field: false,
+            ranges: true
+          }
+        },
+        {
+          config: {
+            key_field: true,
+            ranges: true
+          }
+        },
+        {
+          config: {
+            key_field: false,
+            value_field: true,
+            ranges: true
+          }
+        },
+        {
+          config: {
+            value_field: true,
+            ranges: true
+          }
+        }
+      ])
+    })
+  })
+
+  describe('validContext', () => {
+    const test = _.curry(utils.sequentialResultTest)([
+      {
+        aggregations: {
+          twoLevelAgg: {
+            buckets: [
+              {
+                key: '0.0-500.0',
+                from: 0,
+                from_as_string: '0.0',
+                to: 500,
+                to_as_string: '500.0',
+                doc_count: 476899106,
+                twoLevelAgg: {
+                  count: 476899106,
+                  min: -500000000,
+                  max: 937998784,
+                  avg: 973.7296742278231,
+                  sum: 464370811124.9201
+                }
+              },
+              {
+                key: '500.0-10000.0',
+                from: 500,
+                from_as_string: '500.0',
+                to: 10000,
+                to_as_string: '10000.0',
+                doc_count: 110489302,
+                twoLevelAgg: {
+                  count: 110489302,
+                  min: -999299968,
+                  max: 2100000000,
+                  avg: 3038.799582495458,
+                  sum: 335754844787.8146
+                }
+              }
+            ]
+          }
+        }
+      }
+    ])
+    it('should work', () =>
+      test(
+        {
+          key: 'test',
+          type: 'rangeStats',
+          config: {
+            key_field: 'LineItem.UnitPrice',
+            value_field: 'LineItem.TotalPrice',
+            ranges: [
+              {
+                from: '0',
+                to: '500'
+              },
+              {
+                from: '500',
+                to: '10000'
+              }
+            ]
+          }
+        },
+        {
+          results: [
+            {
+              key: '0.0-500.0',
+              doc_count: 476899106,
+              count: 476899106,
+              min: -500000000,
+              max: 937998784,
+              avg: 973.7296742278231,
+              sum: 464370811124.9201
+            },
+            {
+              key: '500.0-10000.0',
+              doc_count: 110489302,
+              count: 110489302,
+              min: -999299968,
+              max: 2100000000,
+              avg: 3038.799582495458,
+              sum: 335754844787.8146
+            }
+          ]
+        },
+        [
+          {
+            aggs: {
+              twoLevelAgg: {
+                range: {
+                  field: 'LineItem.UnitPrice',
+                  ranges: [
+                    {
+                      from: '0',
+                      to: '500'
+                    },
+                    {
+                      from: '500',
+                      to: '10000'
+                    }
+                  ]
+                },
+                aggs: {
+                  twoLevelAgg: {
+                    stats: {
+                      field: 'LineItem.TotalPrice'
+                    }
+                  }
+                }
+              }
+            }
+          }
+        ]
+      ))
+  })
+})
