23036594c10543b60ba6fd56240aea4a03dbf993
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 06:15:36 +0200

    percentiles example type

---
+++ packages/elasticsearch/src/example-types/percentiles.js
@@ -0,0 +1,23 @@
+let _ = require('lodash/fp')
+
+module.exports = {
+  validContext: context => context.config.field,
+  result: (context, search, schema, provider, options) =>
+    search({
+      aggs: {
+        agg_percentiles: {
+          percentiles: _.extend(
+            {
+              field: context.config.field,
+              keyed: context.config.keyed || false
+            },
+            _.get('config.percents', context)
+              ? {percents: context.config.percents}
+              : {}
+          )
+        }
+      }
+    }).then(response => ({
+      percentiles: _.get('aggregations.agg_percentiles', response)
+    }))
+}
---
+++ packages/elasticsearch/test/example-types/percentiles.js
@@ -0,0 +1,127 @@
+let sequentialResultTest = require('./testUtils').sequentialResultTest
+
+describe('percentiles', () => {
+  let percentilesTest1 = (...x) =>
+    sequentialResultTest(
+      [
+        {
+          aggregations: {
+            agg_percentiles: {
+              keyed: false,
+              values: {
+                10.0: 44,
+                30.0: 63,
+                75.0: 80.5
+              }
+            }
+          }
+        }
+      ],
+      ...x
+    )
+  let percentilesTest2 = (...x) =>
+    sequentialResultTest(
+      [
+        {
+          aggregations: {
+            agg_percentiles: {
+              keyed: true,
+              values: [
+                {
+                  key: 10.0,
+                  value: 44
+                },
+                {
+                  key: 30.0,
+                  value: 63
+                },
+                {
+                  key: 70.0,
+                  value: 80.5
+                }
+              ]
+            }
+          }
+        }
+      ],
+      ...x
+    )
+  it('percentiles should work', () =>
+    percentilesTest1(
+      {
+        key: 'test',
+        type: 'percentiles',
+        config: {
+          field: 'LineItem.UnitPrice',
+          percents: [10, 30, 75]
+        }
+      },
+      {
+        percentiles: {
+          keyed: false,
+          values: {
+            10.0: 44,
+            30.0: 63,
+            75.0: 80.5
+          }
+        }
+      },
+      [
+        {
+          aggs: {
+            agg_percentiles: {
+              percentiles: {
+                keyed: false,
+                field: 'LineItem.UnitPrice',
+                percents: [10, 30, 75]
+              }
+            }
+          }
+        }
+      ]
+    ))
+  it('percentiles should work with keyed', () =>
+    percentilesTest2(
+      {
+        key: 'test',
+        type: 'percentiles',
+        config: {
+          keyed: true,
+          field: 'LineItem.UnitPrice',
+          percents: [10, 30, 75]
+        }
+      },
+      {
+        percentiles: {
+          keyed: true,
+          values: [
+            {
+              key: 10.0,
+              value: 44
+            },
+            {
+              key: 30.0,
+              value: 63
+            },
+            {
+              key: 70.0,
+              value: 80.5
+            }
+          ]
+        }
+      },
+      [
+        {
+          aggs: {
+            agg_percentiles: {
+              percentiles: {
+                keyed: true,
+                field: 'LineItem.UnitPrice',
+                percents: [10, 30, 75]
+              }
+            }
+          }
+        }
+      ]
+    ))
+})
