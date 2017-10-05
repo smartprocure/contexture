4fccd6405460cd0c9604eafaaf900b1f1cf649b6
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 07:34:30 +0200

    text example type

---
+++ packages/elasticsearch/src/example-types/text.js
@@ -0,0 +1,95 @@
+let _ = require('lodash/fp'),
+  unidecode = require('unidecode')
+
+module.exports = {
+  hasValue: context =>
+    context.data.value || _.get('values.length', context.data),
+  filter: context => {
+    let fieldName = context.field.replace('.untouched', '')
+    let filterParts =
+      context.data.values || context.data.value.toLowerCase().split(' ')
+
+    let lookAtUntouched = /startsWith|endsWith|is|isNot|containsWord/.test(
+      context.data.operator
+    )
+
+    let useQueryString =
+      /\b(contains|containsExact)\b/.test(context.data.operator) ||
+      (context.data.operator == 'containsWord' && filterParts.length > 2)
+
+    if (useQueryString) {
+      let result = {
+        query_string: {
+          query: _.map(x => '"' + x + '"', filterParts).join(' '),
+          default_field: fieldName,
+          default_operator: context.data.join == 'any' ? 'OR' : 'AND'
+        }
+      }
+      if (context.data.operator == 'containsExact')
+        result.query_string.analyzer = 'exact'
+      if (context.data.join == 'none') {
+        result = {
+          bool: {
+            must_not: result
+          }
+        }
+      }
+      return result
+    }
+
+    if (lookAtUntouched) fieldName += '.lowercased'
+
+    if (
+      /endsWith|wordEndsWith/.test(context.data.operator) &&
+      filterParts.length > 2
+    )
+      throw new Error("You can't have more than 2 ends with filters")
+
+    let join = {
+      all: 'must',
+      any: 'should',
+      none: 'must_not'
+    }[context.data.join || 'all']
+
+    let filter = {
+      bool: {
+        [join]: _.map(f => {
+          let criteria = f
+            .toLowerCase()
+            .replace('*', '')
+            .replace('+', '')
+            .replace('-', '')
+          if (lookAtUntouched)
+            criteria = (context.data.value || f).toLowerCase()
+
+          let prefix = /startsWith|wordStartsWith|is|isNot/.test(
+            context.data.operator
+          )
+            ? ''
+            : '*'
+          let suffix = /endsWith|wordEndsWith|is|isNot/.test(
+            context.data.operator
+          )
+            ? ''
+            : '*'
+
+          return {
+            wildcard: {
+              [fieldName]: unidecode(prefix + criteria + suffix)
+            }
+          }
+        }, filterParts)
+      }
+    }
+
+    if (/doesNotContain|isNot/.test(context.data.operator)) {
+      filter = {
+        bool: {
+          must_not: filter
+        }
+      }
+    }
+
+    return filter
+  }
+}
