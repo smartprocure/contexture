ff3162644d09b77158948c0242a7771a220dbf82
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 08:06:38 +0200

    add a type fn which includes all examples and passes config along if applicable

---
+++ packages/provider-elasticsearch/src/types.js
@@ -0,0 +1,11 @@
+let F = require('futil-js')
+
+module.exports = config =>
+  F.mapValuesIndexed(
+    (x, type) => F.callOrReturn(x, config[type]),
+    require('include-all')({
+      dirname: `${__dirname}/example-types`,
+      filter: /(.+)\.js$/,
+      optional: true
+    })
+  )
