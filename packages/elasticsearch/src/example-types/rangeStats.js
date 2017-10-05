f08d5d86cf87bd97d2d621af8491c61378362f10
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 06:20:10 +0200

    range stats example type

---
+++ packages/elasticsearch/src/example-types/rangeStats.js
@@ -0,0 +1,27 @@
+let _ = require('lodash/fp')
+let esTwoLevel = require('./esTwoLevelAggregation').result
+
+module.exports = {
+  validContext: context =>
+    !!(
+      context.config.key_field &&
+      context.config.value_field &&
+      context.config.ranges
+    ),
+  result: (context, search, schema, provider, options) =>
+    esTwoLevel(
+      _.merge(
+        {
+          config: {
+            key_type: 'range',
+            key_data: {
+              ranges: context.config.ranges
+            },
+            value_type: 'stats'
+          }
+        },
+        context
+      ),
+      search
+    )
+}
