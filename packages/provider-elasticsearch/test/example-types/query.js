1eb13614d962b535a7d4ebb86c9da8d236919db9
Samuel Greene <sgreene@smartprocure.us>
2017-10-05 08:28:09 +0200

    query example type

---
+++ packages/provider-elasticsearch/test/example-types/query.js
@@ -0,0 +1,41 @@
+let query = require('../../src/example-types/query')
+let {expect} = require('chai')
+
+describe('query', () => {
+  it('should filter properly', () => {
+    expect(
+      query.filter({
+        key: 'test',
+        type: 'query',
+        field: '_all',
+        data: {
+          query: 'cable'
+        }
+      })
+    ).to.deep.equal({
+      query_string: {
+        query: 'cable',
+        default_operator: 'AND',
+        default_field: '_all'
+      }
+    })
+    expect(
+      query.filter({
+        key: 'test',
+        type: 'query',
+        field: '_all',
+        data: {
+          query: 'cable',
+          exact: true
+        }
+      })
+    ).to.deep.equal({
+      query_string: {
+        query: 'cable',
+        default_operator: 'AND',
+        default_field: '_all.exact',
+        analyzer: 'exact'
+      }
+    })
+  })
+})
