e6174768431bcb25418c1838fabfb6339838612e
Samuel Greene <sgreene@smartprocure.us>
2017-10-21 01:44:47 +0200

    flat tree utils

---
+++ packages/client/test/tree.js
@@ -0,0 +1,52 @@
+import {flattenTree, keyPath} from '../src/tree'
+import _ from 'lodash/fp'
+import chai from 'chai'
+const expect = chai.expect
+
+describe('tree', () => {
+  let tree = {
+    key: 'root',
+    join: 'and',
+    children: [{
+      key: 'analysis',
+      join: 'and',
+      children: [{
+        key: 'results',
+        type: 'results'
+      }]
+    }, {
+      key: 'criteria',
+      join: 'and',
+      children: [{
+        key: 'agencies',
+        field: 'Organization.Name',
+        type: 'facet',
+        data: {
+          values: ['City of Deerfield']
+        },
+        config: {
+          size: 24
+        }
+      }]
+    }]
+  }
+  it('should find', () => {
+    let findTest = _.find(keyPath('test'))
+    var test = findTest([{
+      key: 'a',
+      val: 1
+    }, {
+      key: 'test',
+      val: 2
+    }])
+    expect(test).to.deep.equal({
+      key: 'test',
+      val: 2
+    })
+  })
+  it('should tree', () => {
+    let r = flattenTree(tree)
+    r['root->criteria'].join = 'or'
+    expect(tree.children[1].join).to.equal('or')
+  })
+})
