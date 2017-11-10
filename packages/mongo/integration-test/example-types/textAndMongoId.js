d9c93722d0594a8f23d83a4c5675599a3577ed1d
Samuel Greene <sgreene@smartprocure.us>
2017-11-10 23:53:25 +0100

    add integration tests for populate
    https://github.com/smartprocure/contexture-mongo/pull/13

--- packages/mongo/integration-test/example-types/textAndMongoId.js
+++ packages/mongo/integration-test/example-types/textAndMongoId.js
@@ -1,20 +1,19 @@
 let { expect } = require('chai')
 let types = require('../../types')()
-let Promise = require('bluebird')
 let Contexture = require('contexture')
 let provider = require('../../src')
 let _ = require('lodash/fp')
-let util = require('util')
 let testSetup = require('../setup')
 
 let schemaName = 'Documents'
 let collection = 'document'
 
-describe('Grouping text and mongoId', () => {
-  it('should work', async () => {
-    let {db, ids: [id]} = await testSetup({collection})
-
-    let process = Contexture({
+let contextureTestSetup = async ({collection}) => {
+  let {db, ids} = await testSetup({collection})
+  return {
+    db,
+    ids,
+    process: Contexture({
       schemas: {
         [schemaName]: {
           mongo: {
@@ -29,9 +28,12 @@ describe('Grouping text and mongoId', () => {
         })
       }
     })
+  }
+}
 
-    let expectedCode = '112233'
-
+describe('Grouping text and mongoId', () => {
+  it('should work', async () => {
+    let {ids: [id], process} = await contextureTestSetup({collection})
     let dsl = {
       type: 'group',
       schema: schemaName,
@@ -56,10 +58,51 @@ describe('Grouping text and mongoId', () => {
         type: 'results'
       }]
     }
-
     let context = await process(dsl, { debug: true })
     let response = _.last(context.items).context.response
     expect(response.totalRecords).to.equal(1)
     expect(response.results[0]._id.toString()).to.equal(id.toString())
+  })
+
+  it('should work with populate', async () => {
+    let {ids: [id, id2], process} = await contextureTestSetup({collection})
+    let dsl = {
+      type: 'group',
+      schema: schemaName,
+      join: 'and',
+      items: [{
+        key: 'text',
+        type: 'text',
+        field: 'code',
+        data: {
+          operator: 'containsWord',
+          value: '22'
+        }
+      }, {
+        key: 'specificUser',
+        type: 'mongoId',
+        field: '_id',
+        data: {
+          value: id
+        }
+      }, {
+        key: 'results',
+        type: 'results',
+        config: {
+          populate: {
+            child: {
+              schema: 'Documents',
+              foreignField: '_id',
+              localField: 'nextCode'
+            }
+          }
+        }
+      }]
+    }
+    let context = await process(dsl, { debug: true })
+    let response = _.last(context.items).context.response
+    expect(response.totalRecords).to.equal(1)
+    expect(response.results[0]._id.toString()).to.equal(id.toString())
+    expect(response.results[0].child[0]._id.toString()).to.equal(id2.toString())
   })
 })
