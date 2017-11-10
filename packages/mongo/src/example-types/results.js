8e2505a06a90d2f26e394ac812f05d01f0101ad2
Decrapifier <drodriguez@smartprocure.us>
2017-11-10 23:08:31 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/13

--- packages/mongo/src/example-types/results.js
+++ packages/mongo/src/example-types/results.js
@@ -3,7 +3,7 @@ let _ = require('lodash/fp')
 let Promise = require('bluebird')
 
 module.exports = {
-  result: (context, search, schema, {getSchema}) => {
+  result(context, search, schema, { getSchema }) {
     let {
       config: {
         page = 1,
@@ -20,40 +20,44 @@ module.exports = {
     }
 
     return Promise.all([
-      search(_.reject(_.isEmpty, [
-        {
-          $sort: sort,
-        },
-        {
-          $skip: startRecord,
-        },
-        {
-          $limit: pageSize,
-        },
-        ...F.mapIndexed(
-            (x, as) => {
-              let targetSchema = getSchema(x.schema) //|| toSingular(as), //<-- needs compromise-fp
-              if (!targetSchema)
-                throw Error(`Couldn't find schema configuration for ${x.schema}`)
-              if (!targetSchema.mongo)
-                throw Error('Populating a non mongo provider schema on a mongo provider schema is not supported')
-              let targetCollection = _.get('mongo.collection', targetSchema)
-              if (!targetCollection)
-                throw Error(`The ${targetCollection} schema has a mongo configuration, but doesn't have a 'collection' property`)
+      search(
+        _.reject(_.isEmpty, [
+          {
+            $sort: sort,
+          },
+          {
+            $skip: startRecord,
+          },
+          {
+            $limit: pageSize,
+          },
+          ...F.mapIndexed((x, as) => {
+            let targetSchema = getSchema(x.schema) //|| toSingular(as), //<-- needs compromise-fp
+            if (!targetSchema)
+              throw Error(`Couldn't find schema configuration for ${x.schema}`)
+            if (!targetSchema.mongo)
+              throw Error(
+                'Populating a non mongo provider schema on a mongo provider schema is not supported'
+              )
+            let targetCollection = _.get('mongo.collection', targetSchema)
+            if (!targetCollection)
+              throw Error(
+                `The ${
+                  targetCollection
+                } schema has a mongo configuration, but doesn't have a 'collection' property`
+              )
 
-              return ({
-                $lookup: {
-                  as,
-                  from: targetCollection,
-                  localField: x.localField, // || '_id',
-                  foreignField: x.foreignField, // || context.schema, <-- needs schema lookup
-                }
-
-              })
-            },
-          populate
-        ),
-      ])),
+            return {
+              $lookup: {
+                as,
+                from: targetCollection,
+                localField: x.localField, // || '_id',
+                foreignField: x.foreignField, // || context.schema, <-- needs schema lookup
+              },
+            }
+          }, populate),
+        ])
+      ),
       search([
         {
           $group: {
