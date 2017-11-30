f69c45bb73974c760861f43e9c96a2b185ef9e85
Sean Clayton <clayton.sean@gmail.com>
2017-11-30 18:45:00 -0500

    Add README story
    https://github.com/smartprocure/contexture-react/pull/5

--- packages/react/stories/index.js
+++ packages/react/stories/index.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
+import { withInfo } from '@storybook/addon-info'
 
-import { Welcome } from '@storybook/react/demo'
 import { Button } from '../src/index'
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-))
+import readme from '../README.md'
+
+storiesOf('Docs', module).add(
+  'README.md',
+  withInfo({
+    text: readme,
+    inline: true,
+    source: false,
+  })(() => null)
+)
 
 storiesOf('Button', module)
   .add('with text', () => (
