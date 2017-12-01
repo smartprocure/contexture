3521778016ebbde1d99599204274953ac1188835
Samuel Greene <sgreene@smartprocure.us>
2017-12-01 21:33:14 +0100

    add Popover and kill button
    https://github.com/smartprocure/contexture-react/pull/6

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,7 +1,2 @@
 import React from 'react'
 
-export let Button = ({ children, onClick }) => (
-  <button className="Button" style={{ color: 'red' }} onClick={onClick}>
-    {children}
-  </button>
-)
--- packages/react/stories/index.js
+++ packages/react/stories/index.js
@@ -1,29 +1,26 @@
 import React from 'react'
 
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { withInfo } from '@storybook/addon-info'
+import {storiesOf} from '@storybook/react'
+import {action} from '@storybook/addon-actions'
+import {withInfo} from '@storybook/addon-info'
 
-import { Button } from '../src/index'
+import * as F from 'futil-js'
 
 storiesOf('Docs', module)
   .add(
     'README.md',
-    withInfo({ text: require('../README.md'), inline: true, source: false })(
+    withInfo({text: require('../README.md'), inline: true, source: false})(
       () => null
     )
   )
   .add(
     'CHANGELOG.md',
-    withInfo({ text: require('../CHANGELOG.md'), inline: true, source: false })(
+    withInfo({text: require('../CHANGELOG.md'), inline: true, source: false})(
       () => null
     )
   )
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ))
+import Popover from '../src/components/Popover'
+
+storiesOf('Internal Components', module)
+  .add('Popover', () => <Popover show={() => true}>Contents</Popover>)
