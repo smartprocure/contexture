47cde0b002919018f30c780988a8ac6162ec1a0b
Daniel Rodríguez <sadasant@gmail.com>
2017-12-05 20:01:49 +0000

    Using directory-metagen instead of include-all
    https://github.com/smartprocure/contexture-elasticsearch/pull/17

--- packages/elasticsearch/src/types.js
+++ packages/elasticsearch/src/types.js
@@ -3,9 +3,5 @@ let F = require('futil-js')
 module.exports = (config = {}) =>
   F.mapValuesIndexed(
     (x, type) => F.callOrReturn(x, config[type]),
-    require('include-all')({
-      dirname: `${__dirname}/example-types`,
-      filter: /(.+)\.js$/,
-      optional: true,
-    })
+    require('./example-types/__all')
   )
