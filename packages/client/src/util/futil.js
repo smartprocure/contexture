493839c49c3fb0cce761ef65ad76765af68719ff
Sean Clayton <clayton.sean@gmail.com>
2017-12-05 10:59:06 -0500

    Add back shortestKey
    https://github.com/smartprocure/contexture-client/pull/3

--- packages/client/src/util/futil.js
+++ packages/client/src/util/futil.js
@@ -2,3 +2,8 @@ import _ from 'lodash/fp'
 
 export let invokes = _.curry((name, fn) => g => (...a) => g(...a)[name](fn))
 export let catches = invokes('catch')
+
+// let shortestKey = _.flow(
+//   _.keys,
+//   _.minBy('length')
+// )
