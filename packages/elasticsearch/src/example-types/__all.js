47c3e1f80ea8e1c1e185133b8e0546dcaa7b17b1
Decrapifier <drodriguez@smartprocure.us>
2017-12-06 01:05:44 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-elasticsearch/pull/18

--- packages/elasticsearch/src/example-types/__all.js
+++ packages/elasticsearch/src/example-types/__all.js
@@ -26,8 +26,8 @@ module.exports = {
   statistical: require('./statistical'),
   terms: require('./terms'),
   termsDelta: require('./termsDelta'),
-  terms_stats: require('./terms_stats'),
   termsStatsHits: require('./termsStatsHits'),
+  terms_stats: require('./terms_stats'),
   text: require('./text'),
   twoLevelMatch: require('./twoLevelMatch'),
 }
