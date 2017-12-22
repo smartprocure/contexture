8fa8b3533b1b07585220d867b53ef919848812d2
Samuel Greene <sgreene@smartprocure.us>
2017-12-22 16:32:23 +0100

    new regex utils
    https://github.com/smartprocure/contexture-elasticsearch/pull/20

--- packages/provider-elasticsearch/src/regex.js
+++ packages/provider-elasticsearch/src/regex.js
@@ -1,15 +1,38 @@
 let _ = require('lodash/fp')
+let F = require('futil-js')
+let Combinatorics = require('js-combinatorics')
+
+let toSafeRegex = caseSensitive =>
+  _.flow(
+    _.replace(/[.?+*|{}\[\]()]/g, '\\$&'),
+    _.map(
+      x =>
+        !caseSensitive && x.match(/[A-Za-z]/)
+          ? `[${_.toUpper(x)}${_.toLower(x)}]`
+          : x
+    ),
+    _.join('')
+  )
+  
+let regexAnd = _.flow(
+ Combinatorics.permutation,
+  x => x.toArray(),
+  _.map(_.join('.*')),
+  _.map(F.parens),
+  _.join('|')
+)
+
+let buildRegexForWords = (caseSensitive, anyOrder) => _.flow(
+  _.split(' '),
+  _.map(toSafeRegex(caseSensitive)),
+  anyOrder
+    ? regexAnd
+    : _.join('.*'), // This enforces order, for any order we either need `&` (intersection which is behind a flag and not available here) or to do every combination of patterns joined by .* and or'ed together
+  x => `.*${x}.*`
+) 
 
 module.exports = {
-  toSafeRegex: caseSensitive =>
-    _.flow(
-      _.replace(/[.?+*|{}[]()]/g, '\\$&'),
-      _.map(
-        x =>
-          !caseSensitive && x.match(/[A-Za-z]/)
-            ? `[${_.toUpper(x)}${_.toLower(x)}]`
-            : x
-      ),
-      _.join('')
-    ),
+  toSafeRegex,
+  regexAnd,
+  buildRegexForWords
 }
