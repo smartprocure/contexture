aa77e64272ce1516f619886e6b2f444d939b0e86
Samuel Greene <sgreene@smartprocure.us>
2017-12-26 20:38:18 +0100

    terms_stats to terms include with schema overrides
    https://github.com/smartprocure/contexture-elasticsearch/pull/22

--- packages/provider-elasticsearch/src/example-types/terms_stats.js
+++ packages/provider-elasticsearch/src/example-types/terms_stats.js
@@ -1,62 +1,48 @@
 var _ = require('lodash/fp')
 var esTwoLevel = require('./esTwoLevelAggregation').result
+let { buildRegexForWords } = require('../regex')
+let { getField } = require('../fields')
 
 module.exports = {
   validContext: context =>
     context.config.key_field && context.config.value_field,
-  result(context, search) {
-    var filter = null
-
-    if (context.config.filter) {
-      var filterParts = context.config.filter
-        .trim()
-        .replace(/\s\s+/g, ' ')
-        .toLowerCase()
-        .split(' ')
-      var rawFieldName = (
-        _.get('name', context.config.key_field) || context.config.key_field
-      ).replace('.untouched', '.lowercased')
-      filter = {
-        bool: {
-          must: _.map(
-            f => ({
-              wildcard: {
-                [rawFieldName]: `*${f.replace(/\*|-|\+/g, '')}*`,
-              },
-            }),
-            filterParts
-          ),
-        },
-      }
-    }
-
-    return esTwoLevel(
+  async result(context, search, schema) {
+    let x = await esTwoLevel(
       _.merge(
         {
           config: {
-            filter_agg: filter,
             key_type: 'terms',
-            key_data: {
-              size: context.config.size || 10,
-              order: {
-                // Disable nested path checking for now as we don't need it anymore:
-                //  key +
-                //      may need >inner>inner. when adding additional inner for nested filters per old code
-                //      xor paths - if both are nested or both not, the inner one wot be in inner
-                //      ((!getNestedPath(context.config.value_field) != !getNestedPath(context.config.key_field)) ? '>inner.' : '.') +
-                //  order
-                [`twoLevelAgg.${context.config.order || 'sum'}`]:
-                  context.config.sortDir || 'desc',
+            key_data: _.extend(
+              {
+                field: getField(schema, context.key_field),
+                size: context.config.size || 10,
+                order: {
+                  // Disable nested path checking for now as we don't need it anymore:
+                  //  key +
+                  //      may need >inner>inner. when adding additional inner for nested filters per old code
+                  //      xor paths - if both are nested or both not, the inner one wot be in inner
+                  //      ((!getNestedPath(context.config.value_field) != !getNestedPath(context.config.key_field)) ? '>inner.' : '.') +
+                  //  order
+                  [`twoLevelAgg.${context.config.order || 'sum'}`]:
+                    context.config.sortDir || 'desc',
+                },
               },
-            },
+              context.config.filter && {
+                include: buildRegexForWords(
+                  context.config.caseSensitive, // false in all known instances
+                  context.config.anyOrder // false in all known instances
+                )(context.config.filter),
+              }
+            ),
             value_type: 'stats',
           },
         },
         context
       ),
       search
-    ).then(x => ({
+    )
+    return {
       terms: x.results,
-    }))
+    }
   },
 }
