5c72fd8859d682244ef9901ee7663e4fbb0bd049
Daniel Rodríguez <sadasant@gmail.com>
2018-01-11 17:06:41 +0000

    Geo type without context.data and context.config
    https://github.com/smartprocure/contexture-elasticsearch/pull/31

--- packages/elasticsearch/src/example-types/geo.js
+++ packages/elasticsearch/src/example-types/geo.js
@@ -7,9 +7,9 @@ let geo = ({
   },
 } = {}) => ({
   hasValue: context =>
-    !!(context.data.location && context.data.radius && context.data.operator),
+    !!(context.location && context.radius && context.operator),
   filter: context =>
-    Promise.resolve(geocodeLocation(context.data.location))
+    Promise.resolve(geocodeLocation(context.location))
       .then(response => {
         // Check for API key limit/expiration
         if (response.error_message) throw response.error_message
@@ -19,10 +19,10 @@ let geo = ({
         let result = {
           geo_distance: {
             [context.field]: `${location.lat},${location.lng}`,
-            distance: `${context.data.radius}mi`,
+            distance: `${context.radius}mi`,
           },
         }
-        if (context.data.operator !== 'within') {
+        if (context.operator !== 'within') {
           result = {
             bool: {
               must_not: result,
@@ -36,11 +36,7 @@ let geo = ({
         console.error('An error occured within the geo provider: ', err)
       ),
   validContext: context =>
-    !!(
-      context.data.location &&
-      context.data.radius > 0 &&
-      context.data.operator
-    ),
+    !!(context.location && context.radius > 0 && context.operator),
   result: context => ({
     place: _.get('_meta.preprocessorResult.results.0', context),
   }),
