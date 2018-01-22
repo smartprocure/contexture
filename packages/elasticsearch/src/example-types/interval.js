61cd4234d9cd7d0514f2f109b5607732b05ab118
Geovanny Fajardo <gfajardo@smartprocure.us>
2018-01-21 21:57:58 -0500

    implment interval type
    https://github.com/smartprocure/contexture-elasticsearch/pull/58

---
+++ packages/elasticsearch/src/example-types/interval.js
@@ -0,0 +1,73 @@
+let _ = require('lodash/fp')
+
+/*
+ 1. An empty value as the upper boundary represents infinity.
+ 2. An empty value as the lower boundary represents negative infinity.
+ 3. Zero has to be respected as a boundary value.
+*/
+let boundaryFilter = value => {
+  if (_.isString(value) && _.isEmpty(value)) value = NaN
+  return _.isNaN(_.toNumber(value)) ? null : _.toNumber(value)
+}
+
+module.exports = {
+  hasValue: context => context.min || context.max,
+  filter: context => ({
+    range: {
+      [context.field]: _.pickBy(_.isNumber, {
+        gte: boundaryFilter(context.min),
+        lte: boundaryFilter(context.max),
+      })
+    }
+  }),
+  result: async (context, search) => {
+    let field = _.get('field', context)
+    let result = await search({
+      aggs: {
+        statistical: {
+          stats: {
+            field,
+            missing: 0,
+          },
+        }
+      },
+    })
+
+    let statistical = _.get('aggregations.statistical', result)
+    let interval = Math.round(Math.abs(statistical.max - statistical.min) / 50)
+    let histogram = []
+
+    if (interval) {
+      let pricesResult = await search({
+        aggs: {
+          prices: {
+            histogram: {
+              field,
+              interval: interval,
+              min_doc_count: 0,
+              offset: statistical.min
+            }
+          }
+        },
+      })
+
+      let totalDocuments = _.get('hits.total', pricesResult)
+      histogram = _.map(entry => {
+        let percent = Math.round(totalDocuments ? (100 * (entry.doc_count / totalDocuments)) : 0)
+        return {
+          price: Math.round(entry.key),
+          count: entry.doc_count,
+          percent: (percent > 1 ? percent : 2)
+        }
+      }, _.get('aggregations.prices.buckets', pricesResult))
+    }
+
+    return {
+      results: {
+        interval,
+        statistical,
+        histogram
+      }
+    }
+  }
+}
