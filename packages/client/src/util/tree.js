6d9dabbeac11fd249299f8f46e2ce9ec74f5db47
Decrapifier <drodriguez@smartprocure.us>
2018-01-27 01:33:22 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-client/pull/9

--- packages/client/src/util/tree.js
+++ packages/client/src/util/tree.js
@@ -1,16 +1,15 @@
 import _ from 'lodash/fp'
 import * as F from 'futil-js'
 
-export let Tree = F.tree(_.get('children'), F.when(_.isString, key => ({key})))
+export let Tree = F.tree(
+  _.get('children'),
+  F.when(_.isString, key => ({ key }))
+)
 
-export let {encode: encodePath, decode: decodePath} = F.encoder('/')
+export let { encode: encodePath, decode: decodePath } = F.encoder('/')
 
 export let flattenTree = Tree.flatten(F.propTreePath('key'))
 export let flatLeaves = Tree.flatLeaves
 
 export let bubbleUp = (f, path, lookup) =>
-  _.flow(
-    F.prefixes,
-    _.reverse,
-    _.map(p => f(lookup(p), p))
-  )(path)
+  _.flow(F.prefixes, _.reverse, _.map(p => f(lookup(p), p)))(path)
