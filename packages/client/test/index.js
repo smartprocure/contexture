5324217aa9410ff68514dd91edbc48522932fde3
Samuel Greene <sgreene@smartprocure.us>
2018-01-30 22:46:59 +0100

    add onResult and dropping stale update tests
    https://github.com/smartprocure/contexture-client/pull/11

--- packages/client/test/index.js
+++ packages/client/test/index.js
@@ -1,71 +1,13 @@
+import _ from 'lodash/fp'
 import chai from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import * as lib from '../src'
+import Promise from 'bluebird'
 const expect = chai.expect
 chai.use(sinonChai)
 
 describe('lib', () => {
-  let tree = {
-    key: 'root',
-    join: 'and',
-    children: [
-      {
-        key: 'analysis',
-        join: 'and',
-        children: [
-          {
-            key: 'results',
-            type: 'results',
-          },
-        ],
-      },
-      {
-        key: 'criteria',
-        join: 'or',
-        children: [
-          {
-            key: 'agencies',
-            field: 'Organization.Name',
-            type: 'facet',
-            data: {
-              values: ['City of Deerfield'],
-            },
-            config: {
-              size: 24,
-            },
-          },
-          {
-            key: 'mainQuery',
-            type: 'query',
-            data: {
-              query: 'cable internet~',
-            },
-          },
-          {
-            key: 'noValue',
-            type: 'facet',
-            data: {
-              values: [],
-            },
-          },
-          {
-            key: 'uselessGroup',
-            join: 'and',
-            children: [
-              {
-                key: 'uselessChild',
-                type: 'facet',
-                data: {
-                  values: [],
-                },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  }
   describe('should generally work', () => {
     // TODO: make these generally self contained - some rely on previous test runs
     let tree = {
@@ -337,7 +279,66 @@ describe('lib', () => {
   // })
   it('should work', async () => {
     let service = sinon.spy(() => ({ data: {} }))
-    let Tree = lib.ContextTree(tree, service)
+    let Tree = lib.ContextTree({
+      key: 'root',
+      join: 'and',
+      children: [
+        {
+          key: 'analysis',
+          join: 'and',
+          children: [
+            {
+              key: 'results',
+              type: 'results',
+            },
+          ],
+        },
+        {
+          key: 'criteria',
+          join: 'or',
+          children: [
+            {
+              key: 'agencies',
+              field: 'Organization.Name',
+              type: 'facet',
+              data: {
+                values: ['City of Deerfield'],
+              },
+              config: {
+                size: 24,
+              },
+            },
+            {
+              key: 'mainQuery',
+              type: 'query',
+              data: {
+                query: 'cable internet~',
+              },
+            },
+            {
+              key: 'noValue',
+              type: 'facet',
+              data: {
+                values: [],
+              },
+            },
+            {
+              key: 'uselessGroup',
+              join: 'and',
+              children: [
+                {
+                  key: 'uselessChild',
+                  type: 'facet',
+                  data: {
+                    values: [],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }, service)
 
     await Tree.mutate(['root', 'criteria', 'mainQuery'], {
       data: {
@@ -390,5 +391,70 @@ describe('lib', () => {
         },
       ],
     })
+  })
+  it('should call onResult and drop stale updates', async () => {
+    let tree = {
+      key: 'root',
+      join: 'and',
+      children: [
+        {
+          key: 'filter',
+        },
+        {
+          key: 'results',
+        },
+      ],
+    }
+    let service = sinon.spy(async (dto, lastUpdateTime) => {
+      let testChange = dto.children[0].data.values[0]
+      // arbitrarily delay the first call to trigger a stale update 
+      await Promise.delay(testChange == 'a' ? 20 : 1)
+      return {
+        data: {
+          key: 'root',
+          children: [
+            {
+              key: 'results',
+              context: {
+                count: 1,
+                results: [
+                  {
+                    title: 'some result',
+                  },
+                ],
+              },
+              config: {
+                size: 20,
+              },
+              lastUpdateTime
+            },
+            {
+              key: 'filter',
+            },
+          ],
+        },
+      }
+    })
+    
+    let spy = sinon.spy()
+    // Just call the spy for `results`
+    let onResult = path => _.isEqual(path, ['root', 'results']) && spy()
+    let Tree = lib.ContextTree(tree, service, undefined, {
+      onResult
+    })
+    let step1 = Tree.mutate(['root', 'filter'], {
+      data: {
+        values: ['a'],
+      },
+    })
+    // Give it enough time for the core to trigger a search for step 1 (but not awaiting step1 because that would also wait for the service)
+    await Promise.delay(10)
+    let step2 = Tree.mutate(['root', 'filter'], {
+      data: {
+        values: ['b'],
+      },
+    })
+    await Promise.all([step1, step2])
+    expect(spy).to.have.callCount(1)
   })
 })
