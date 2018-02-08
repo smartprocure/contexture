22166699542f0c4a04281b423973523029d3b4ba
Daniel Rodríguez <sadasant@users.noreply.github.com>
2018-02-07 19:17:45 -0500

    Update text.js https://github.com/smartprocure/contexture-mongo/pull/21

--- packages/mongo/src/example-types/text.js
+++ packages/mongo/src/example-types/text.js
@@ -10,28 +10,25 @@ let joinmap = {
 let getValues = x => x.values || [x.value]
 
 module.exports = {
-  hasValue: F.cascade(['value', 'values.length']),
-  filter: context =>
-    _.isEmpty(getValues(context))
-      ? {}
-      : {
-          [joinmap[context.join || 'all']]: _.map(
-            val => ({
-              [context.field]: {
-                $regex: {
-                  containsWord: val,
-                  startsWith: `^${val}`,
-                  wordStartsWith: `\\b${val}`,
-                  endsWith: `${val}$`,
-                  wordEndsWith: `${val}\\b`,
-                  is: `^${val}$`,
-                  containsExact: `\\b${val}\\b`,
-                }[context.operator],
-                $options: 'i',
-              },
-            }),
-            context.values || [context.value]
-          ),
+  hasValue: x => F.cascade(['value', 'values.length'])(x),
+  filter: context => ({
+    [joinmap[context.join || 'all']]: _.map(
+      val => ({
+        [context.field]: {
+          $regex: {
+            containsWord: val,
+            startsWith: `^${val}`,
+            wordStartsWith: `\\b${val}`,
+            endsWith: `${val}$`,
+            wordEndsWith: `${val}\\b`,
+            is: `^${val}$`,
+            containsExact: `\\b${val}\\b`,
+          }[context.operator],
+          $options: 'i',
         },
+      }),
+      context.values || [context.value]
+    ),
+  }),
 }
 // query could be the same as text:containsExact
