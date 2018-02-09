e0b22729c045ef7f881c194c5c0aeb341d0b49ad
Daniel Rodríguez <sadasant@gmail.com>
2018-02-09 19:57:06 +0000

    Some example types
    https://github.com/smartprocure/contexture-client/pull/25

--- packages/client/src/exampleTypes.js
+++ packages/client/src/exampleTypes.js
@@ -6,43 +6,16 @@ export default {
     validate: defaultHasValue,
   },
   facet: {
-    // data: {
-    //   values: [],
-    //   mode: 'include',
-    // },
-    // config: {
-    //   size: 12,
-    //   filter: '',
-    // },
-    // context: {
-    //   total: 0,
-    //   options: [],
-    //   cardinality: 0,
-    // },
     reactors: {
       values: 'others',
       mode: 'others',
       size: 'self',
       filter: 'self',
     },
-    // validate: x => x.data.values,
-    // toString: ({data: {values, mode}}) =>
-    //   values.length
-    //     ? F.compactJoin(' ', [
-    //       'is',
-    //       mode === 'exclude' ? 'not' : '',
-    //       toSentence(', ', ` ${mode === 'exclude' ? 'nor' : 'or'} `, values)
-    //     ])
-    //     : 'is anything'
   },
   text: {
     reactors: {
       value: 'others',
-    },
-  },
-  facet: {
-    reactors: {
-      values: 'others',
     },
   },
   query: {
@@ -63,6 +36,12 @@ export default {
   results: {
     reactors: {
       page: 'self',
+    },
+  },
+  number: {
+    reactors: {
+      min: 'others',
+      max: 'others',
     },
   },
 }
