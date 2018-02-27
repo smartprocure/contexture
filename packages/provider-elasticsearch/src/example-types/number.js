1cd73a77a5ea47da084c6660d1179dde642cb293
Geovanny Fajardo <gfajardo@smartprocure.us>
2018-02-27 14:29:52 -0500

    Changes to number types.
    https://github.com/smartprocure/contexture-elasticsearch/pull/69

--- packages/provider-elasticsearch/src/example-types/number.js
+++ packages/provider-elasticsearch/src/example-types/number.js
@@ -1,5 +1,4 @@
 const _ = require('lodash/fp')
-
 /*
  1. An empty value as the upper boundary represents infinity.
  2. An empty value as the lower boundary represents negative infinity.
@@ -19,90 +18,100 @@ let rangeFilter = (field, min, max) => ({
   },
 })
 
-module.exports = {
-  hasValue: context => !_.isNil(context.min) || !_.isNil(context.max),
-  filter: ({ field, min, max }) => rangeFilter(field, min, max),
-  async result({ field, min, max, percentileInterval = 1 }, search) {
-    let statisticalResult = await search({
-      aggs: {
-        range_filter: {
-          filter: rangeFilter(field, min, max),
-          aggs: {
-            statistical: {
-              stats: {
-                field,
-                missing: 0,
-              },
+let searchResults = async (search, field, min, max, percentileInterval) => {
+  let statisticalResult = await search({
+    aggs: {
+      range_filter: {
+        filter: rangeFilter(field, min, max),
+        aggs: {
+          statistical: {
+            stats: {
+              field,
+              missing: 0,
             },
-            all_percentiles: {
-              percentiles: {
-                field,
-                percents: [
-                  0,
-                  percentileInterval,
-                  100 - percentileInterval,
-                  100,
-                ],
-              },
+          },
+          all_percentiles: {
+            percentiles: {
+              field,
+              percents: [
+                0,
+                percentileInterval,
+                100 - percentileInterval,
+                100,
+              ],
             },
           },
         },
       },
-    })
+    },
+  })
 
-    let percentiles = _.flow(_.mapKeys(Number), mappedResult => ({
-      rangeMin: mappedResult[0],
-      rangeMax: mappedResult[100],
-      intervalMin: mappedResult[percentileInterval],
-      intervalMax: mappedResult[100 - percentileInterval],
-    }))(
-      _.get(
-        'aggregations.range_filter.all_percentiles.values',
-        statisticalResult
-      )
-    )
-
-    let statistical = _.get(
-      'aggregations.range_filter.statistical',
+  let percentiles = _.flow(_.mapKeys(Number), mappedResult => ({
+    rangeMin: mappedResult[0],
+    rangeMax: mappedResult[100],
+    intervalMin: mappedResult[percentileInterval],
+    intervalMax: mappedResult[100 - percentileInterval],
+  }))(
+    _.get(
+      'aggregations.range_filter.all_percentiles.values',
       statisticalResult
     )
+  )
 
-    let interval =
-      Math.round(Math.abs(statistical.max - statistical.min) / 40) || 1
-    let histogram = []
+  let statistical = _.get(
+    'aggregations.range_filter.statistical',
+    statisticalResult
+  )
 
-    if (interval) {
-      let histogramResult = await search({
-        aggs: {
-          range_filter: {
-            filter: rangeFilter(field, min, max),
-            aggs: {
-              values: {
-                histogram: {
-                  field,
-                  interval,
-                  min_doc_count: 0,
-                },
-              },
-            },
-          },
-        },
+  return {
+    statistical,
+    percentiles,
+  }
+}
+
+module.exports = {
+  hasValue: context => !_.isNil(context.min) || !_.isNil(context.max),
+  filter: ({ field, min, max }) => rangeFilter(field, min, max),
+  async result({ field, min, max, percentileInterval = 1, rangeThreshold, findBestRange = false }, search) {
+    let results
+
+    if (findBestRange) {
+      let hasMaxOutlier = true
+      let hasMinOutlier = true
+      let minValue = min
+      let maxValue = max
+      let maxIteration = 10
+      let iterationCount = 1
+
+      while (hasMaxOutlier || hasMaxOutlier || iterationCount <= maxIteration) {
+        results = await searchResults(search, field, minValue, maxValue, percentileInterval)
+        let { statistical, percentiles } = results
+        let rangeMin = _.get('min', statistical)
+        let rangeMax = _.get('max', statistical)
+        hasMaxOutlier = percentiles &&
+          ((rangeMax - percentiles.intervalMax) / (rangeMax - rangeMin) > rangeThreshold)
+        hasMinOutlier = percentiles &&
+          ((percentiles.intervalMin - rangeMin) / (rangeMax - rangeMin) > rangeThreshold)
+        if (hasMaxOutlier) {
+          maxValue = percentiles.intervalMax
+        }
+
+        if (hasMinOutlier) {
+          minValue = percentiles.intervalMin
+        }
+        iterationCount++
+      }
+
+      results = _.extend(results, {
+        extremes: {
+          min: minValue,
+          max: maxValue
+        }
       })
-
-      histogram = _.map(
-        entry => ({
-          value: Math.round(entry.key),
-          count: entry.doc_count,
-        }),
-        _.get('aggregations.range_filter.values.buckets', histogramResult)
-      )
+    } else {
+      results = await searchResults(search, field, min, max, percentileInterval)
     }
 
-    return {
-      interval,
-      statistical,
-      histogram,
-      percentiles,
-    }
+    return results
   },
 }
