87d255473f1970707564a6d4b7e4436b8d9886cb
Geovanny Fajardo <gfajardo@smartprocure.us>
2018-03-02 11:24:14 -0500

    implemented unit tests
    https://github.com/smartprocure/contexture-elasticsearch/pull/69

--- packages/elasticsearch/src/example-types/numberRangeHistogram.js
+++ packages/elasticsearch/src/example-types/numberRangeHistogram.js
@@ -5,7 +5,7 @@ module.exports = {
   hasValue: context => !_.isNil(context.min) || !_.isNil(context.max),
   filter: ({ field, min, max }) => util.rangeFilter(field, min, max),
   async result({ field, min, max, percentileInterval = 1 }, search) {
-    let { statistical, percentiles } = await util.getStatisticalResults(
+    let { statistical = {min, max}, percentiles } = await util.getStatisticalResults(
       search,
       field,
       min,
