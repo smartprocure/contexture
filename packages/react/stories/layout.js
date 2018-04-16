4255be2a2f8d8f7950734a2ccc677b5cce89af65
Samuel Greene <sgreene@smartprocure.us>
2018-04-16 18:26:57 +0000

    add awaiter
    https://github.com/smartprocure/contexture-react/pull/28

--- packages/react/stories/layout.js
+++ packages/react/stories/layout.js
@@ -2,9 +2,11 @@ import React from 'react'
 import * as F from 'futil-js'
 import { storiesOf } from '@storybook/react'
 import { observer } from 'mobx-react'
+import { fromPromise } from 'mobx-utils'
 import { withStateLens } from '../src/utils/mobx-react-utils'
 import Popover from '../src/layout/Popover'
 import Modal from '../src/layout/Modal'
+import Awaiter from '../src/layout/Awaiter'
 
 let ModalDemo = withStateLens({ isOpen: false })(
   observer(
@@ -31,8 +33,25 @@ let PopoverDemo = withStateLens({ isOpen: false })(
     )
   )
 )
+
 export default () => {
   storiesOf('Layout', module)
     .add('Popover', () => <PopoverDemo />)
     .add('Modal', () => <ModalDemo />)
+    .add('Awaiter', () => {
+      let resolve, reject
+      let p = fromPromise(new Promise((_resolve, _reject) => {
+        resolve = _resolve
+        reject = _reject
+      }))
+      return <div>
+        <Awaiter promise={p}>
+          {x => (
+            <div>{x}</div>
+          )}
+        </Awaiter>
+        <button onClick={() => resolve('async value')}>Resolve</button>
+        <button onClick={() => reject('some error')}>Reject</button>
+      </div>
+    })
 }
