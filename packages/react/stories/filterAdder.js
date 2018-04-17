9a6f2c701bbcd1b418f40a66c271453e0bc7d7e5
Samuel Greene <sgreene@smartprocure.us>
2018-04-17 06:16:13 +0000

    add filter add story with FilteredPickerModal
    https://github.com/smartprocure/contexture-react/pull/28

--- packages/react/stories/filterAdder.js
+++ packages/react/stories/filterAdder.js
@@ -5,6 +5,9 @@ import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
 import FilterAdder from '../src/FilterAdder'
 import {applyDefaults} from '../src/utils/schema'
+import {partial} from '../src/utils/mobx-react-utils'
+import Modal from '../src/layout/Modal'
+import {ModalPicker, FilteredPicker} from '../src/layout/Pickers'
 
 let Select = ({options, onChange}) => (
   <select onChange={e => onChange(e.target.value)}>
@@ -19,11 +22,45 @@ let Select = ({options, onChange}) => (
   </select>
 )
 
+let Adder = partial(
+  {
+    Picker: partial(
+      {
+        Modal,
+        label: '+ Include Additional Filter',
+        Picker: FilteredPicker,
+      },
+      ModalPicker
+    ),
+  },
+  FilterAdder
+)
+
 export default () => {
-  storiesOf('FilterAdder', module).add('Example', () => (
-    <div>
-      <FilterAdder
-        Picker={Select}
+  storiesOf('FilterAdder', module)
+    .add('Example', () => (
+      <div>
+        <FilterAdder
+          Picker={Select}
+          tree={{
+            add: action('add'),
+            getNode: () => {},
+          }}
+          path={['path']}
+          fields={applyDefaults({
+            directors: {
+              typeDefault: 'facet',
+            },
+            runtimeMinutes: {
+              typeDefault: 'number',
+            },
+          })}
+        />
+        <div>Check action log to see adding being dispatched</div>
+      </div>
+    ))
+    .add('With FilteredPickerModal', () => (
+      <Adder
         tree={{
           add: action('add'),
           getNode: () => {},
@@ -38,7 +75,5 @@ export default () => {
           },
         })}
       />
-      <div>Check action log to see adding being dispatched</div>
-    </div>
-  ))
+    ))
 }
