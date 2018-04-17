a7ebfb5ceb90529b2757f6be2ba7daf44884190a
Samuel Greene <sgreene@smartprocure.us>
2018-04-17 17:38:47 +0000

    add ContextureProvider
    https://github.com/smartprocure/contexture-react/pull/28

---
+++ packages/react/src/ContextureProvider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Provider} from 'mobx-react'
+import ContextureMobx from './utils/contexture-mobx'
+
+export default ({types, service, children, nodeKey, ...props}) => (
+  <Provider
+    tree={ContextureMobx({types, service})({
+      key: nodeKey || 'root',
+      type: 'group',
+      children: [],
+      ...props
+    })}>
+    {children}
+  </Provider>
+)
