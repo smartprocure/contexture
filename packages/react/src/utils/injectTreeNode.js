2f5c53227d21e38867bc816f7cf61137f1cb6a1a
Samuel Greene <sgreene@smartprocure.us>
2018-04-17 06:20:00 +0000

    make inject tree node support adding nodes
    https://github.com/smartprocure/contexture-react/pull/28

--- packages/react/src/utils/injectTreeNode.js
+++ packages/react/src/utils/injectTreeNode.js
@@ -1,7 +1,20 @@
-import { inject } from 'mobx-react'
-export default inject(
-  ({ tree: t1, node: n1 }, { tree = t1, path, node: n2 }) => ({
-    tree,
-    node: n2 || n1 || tree.getNode(path),
-  })
-)
+import _ from 'lodash/fp'
+import {inject} from 'mobx-react'
+export default (render, {type, reactors, nodeProps = _.keys(reactors)} = {}) =>
+  inject(({tree: t, node: n}, {tree = t, path, node = n, ...props}) => {
+    node = node || tree.getNode(path)
+    if (!node && props.key) // Check if already added
+      node = tree.getNode([...path, props.key])
+    if (!node && props.group) {
+      let key = props.key || _.uniqueId(type)
+      tree.add(props.group, {
+        key,
+        type,
+        ...(props.field && {field: props.field}),
+        ..._.pick(nodeProps, props),
+      })
+      // add doesn't return the node
+      node = tree.getNode([...props.group, key])
+    }
+    return {tree, node}
+  })(render)
