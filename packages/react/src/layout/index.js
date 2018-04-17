5f11eb6b93fb08cc9fe27122b9db7827cb085918
Samuel Greene <sgreene@smartprocure.us>
2018-04-17 17:08:49 +0000

    add layout index for easier importing
    https://github.com/smartprocure/contexture-react/pull/28

---
+++ packages/react/src/layout/index.js
@@ -0,0 +1,18 @@
+import Awaiter from './Awaiter'
+import {Flex} from './Flex'
+import Modal from './Modal'
+import Popover from './Popover'
+import SpacedList from './SpacedList'
+import TextHighlight from './TextHighlight'
+import {FilteredPicker, ModalPicker} from './Pickers'
+
+export {
+  Awaiter,
+  Flex,
+  Modal,
+  Popover,
+  SpacedList,
+  TextHighlight,
+  FilteredPicker,
+  ModalPicker,
+}
