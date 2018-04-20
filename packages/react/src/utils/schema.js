122056a563e946071bd1a568c0c10eb41ef7b2df
Samuel Greene <sgreene@smartprocure.us>
2018-04-20 18:46:55 +0000

    add support for aliases and `esType` and `esIndex` on es mapped schemas
    https://github.com/smartprocure/contexture-react/pull/28

--- packages/react/src/utils/schema.js
+++ packages/react/src/utils/schema.js
@@ -1,5 +1,9 @@
 import _ from 'lodash/fp'
 import * as F from 'futil-js'
+// For futil
+let stampKey = _.curry((key, x) =>
+  F.mapValuesIndexed((val, k) => ({ ...val, [key]: k }), x)
+)
 
 export let flagFields = _.flow(F.invertByArray, _.mapValues(F.flags))
 export let applyDefaults = F.mapValuesIndexed((node, field) =>
@@ -17,33 +21,48 @@ let Tree = F.tree(x => x.properties)
 let flatten = _.flow(Tree.flatten(), _.omitBy(Tree.traverse))
 export let fromEsMapping = _.mapValues(
   _.flow(
-    _.flow(
-      _.get('mappings'),
-      // Always 1 type per index but sometimes there's a `_default_` type thing
-      _.omit(['_default_']),
-      _.values,
-      _.head,
-      flatten
-    ),
-    _.mapValues(({ type }) => {
-      let typeDefault = F.alias(type, {
-        string: 'query',
-        text: 'facet',
-        long: 'number',
-        float: 'number',
-        double: 'number',
-      })
-      return {
-        typeDefault,
-        // TODO: exists, bool, geo, text? //date auto
-        typeOptions: {
-          text: ['facet', 'query'],
-        }[type] || [typeDefault],
-      }
-    }),
-    applyDefaults,
-    fields => ({ fields })
+    _.get('mappings'),
+    // Always 1 type per index but sometimes there's a `_default_` type thing
+    _.omit(['_default_']),
+    _.toPairs,
+    // Capture esType
+    ([[esType, fields]]) => ({ fields, esType }),
+    _.update('fields', 
+      _.flow(
+        flatten,
+        _.mapValues(({ type }) => {
+          let typeDefault = F.alias(type, {
+            string: 'query',
+            text: 'facet',
+            long: 'number',
+            float: 'number',
+            double: 'number',
+          })
+          return {
+            typeDefault,
+            // TODO: exists, bool, geo, text? //date auto
+            typeOptions: {
+              text: ['facet', 'query'],
+            }[type] || [typeDefault],
+          }
+        }),
+        applyDefaults
+      )
+    )
   )
 )
-export let getESSchemas = client =>
-  client.indices.getMapping().then(fromEsMapping)
+export let getESSchemas = client => 
+  Promise.all([client.indices.getMapping(), client.indices.getAlias()]).then(
+    ([mappings, aliases ]) => {
+      let schemas = fromEsMapping(mappings)
+      return _.flow(
+        _.mapValues(x => _.keys(x.aliases)),
+        F.invertByArray,
+        _.mapValues(([x]) => schemas[x]),
+        _.merge(schemas),
+        stampKey('esIndex')
+      )(aliases)
+    }
+  )
+
+
