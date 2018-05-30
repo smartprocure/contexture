5b5c72d55cc56c9ed5246a8dbc64703dad5e61af
Samuel Greene <sgreene@smartprocure.us>
2018-05-30 20:17:39 +0000

    add getPropTypeOrError and add support for node specific overrides of typeProps
    https://github.com/smartprocure/contexture-client/pull/57

--- packages/client/src/types.js
+++ packages/client/src/types.js
@@ -2,8 +2,13 @@ import _ from 'lodash/fp'
 import * as F from 'futil-js'
 
 export let getTypeProp = _.curry((types, prop, node) =>
-  F.cascade([`${node.type}.${prop}`, `default.${prop}`], types)
+  node[prop] || F.cascade([`${node.type}.${prop}`, `default.${prop}`], types)
 )
 export let runTypeFunction = _.curry((types, prop, node, extend) =>
   (getTypeProp(types, prop, node) || _.stubTrue)(node, extend)
+)
+
+export let getTypePropOrError = _.curry((types, prop, node) =>
+  getTypeProp(types, prop, node) ||
+  F.throws(Error(`No '${prop}' found for ${node.type}`))
 )
