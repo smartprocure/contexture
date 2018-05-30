b282aa976e3b446139177b70194a641758388060
Samuel Greene <sgreene@smartprocure.us>
2018-05-30 20:44:00 +0000

    add some inline documentation
    https://github.com/smartprocure/contexture-client/pull/57

--- packages/client/src/types.js
+++ packages/client/src/types.js
@@ -1,14 +1,17 @@
 import _ from 'lodash/fp'
 import * as F from 'futil-js'
 
+// Gets type a specific property from any of the places it might be - on the type in `types`, on default in `types`, or already on the node itself
 export let getTypeProp = _.curry((types, prop, node) =>
   node[prop] || F.cascade([`${node.type}.${prop}`, `default.${prop}`], types)
 )
-export let runTypeFunction = _.curry((types, prop, node, extend) =>
-  (getTypeProp(types, prop, node) || _.stubTrue)(node, extend)
-)
 
+// Same as getTypeProp, but throws instead of returning undefined if it's missing
 export let getTypePropOrError = _.curry((types, prop, node) =>
   getTypeProp(types, prop, node) ||
   F.throws(Error(`No '${prop}' found for ${node.type}`))
+)
+
+export let runTypeFunction = _.curry((types, prop, node, extend) =>
+  (getTypeProp(types, prop, node) || _.stubTrue)(node, extend)
 )
