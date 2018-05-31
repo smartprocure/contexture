533c247ef8d1ef48e98883f745d04de06450b57c
David Sargeant <david@dsargeant.com>
2018-05-31 11:27:04 -0400

    Address PR feedback.
    https://github.com/smartprocure/contexture-elasticsearch/pull/82

--- packages/provider-elasticsearch/test/example-types/tagsQuery.js
+++ packages/provider-elasticsearch/test/example-types/tagsQuery.js
@@ -1,9 +1,8 @@
 let {
   wordPermutations,
   limitResultsToCertainTags,
-  quoteIf,
   quoteAndTilde,
-  escapeSpecialChars,
+  escapeReservedChars,
   joinTags,
   tagToQueryString,
   tagsToQueryString,
@@ -26,16 +25,9 @@ describe('wordPermutations', () => {
 })
 
 describe('limitResultsToCertainTags', () => {
-  it('should return true if found', () => {
-    expect(
-      limitResultsToCertainTags([{ onlyShowTheseResults: true }, {}])
-    ).to.deep.equal(true)
-  })
-})
-
-describe('quoteIf', () => {
-  it('should quote if true', () => {
-    expect(quoteIf('foo bar', true)).to.deep.equal(`"foo bar"`)
+  it('should return truthy if found', () => {
+    expect(limitResultsToCertainTags([{ onlyShowTheseResults: true }, {}])).to
+      .exist
   })
 })
 
@@ -55,23 +47,31 @@ describe('quoteAndTilde', () => {
   })
 })
 
-describe('escapeSpecialChars', () => {
-  it('should quote if is phrase', () => {
-    expect(escapeSpecialChars('foo: [bar] (baz) - 1 ^ 2')).to.deep.equal(
+describe('escapeReservedChars', () => {
+  it('should escape reserved characters', () => {
+    expect(escapeReservedChars('foo: [bar] (baz) - 1 ^ 2')).to.deep.equal(
       'foo\\: \\[bar\\] \\(baz\\) \\- 1 \\^ 2'
+    )
+  })
+  it('should not escape non-reserved characters', () => {
+    expect(escapeReservedChars(`I'm just a plain old sentence.`)).to.deep.equal(
+      `I'm just a plain old sentence.`
     )
   })
 })
 
 describe('joinTags', () => {
+  it('should return empty string if empty', () => {
+    expect(joinTags('all', [])).to.deep.equal('')
+  })
   it('should join with AND', () => {
-    expect(joinTags('all')(['foo', 'bar'])).to.deep.equal('foo AND bar')
+    expect(joinTags('all', ['foo', 'bar'])).to.deep.equal('foo AND bar')
   })
   it('should join with OR', () => {
-    expect(joinTags('any')(['foo', 'bar'])).to.deep.equal('foo OR bar')
+    expect(joinTags('any', ['foo', 'bar'])).to.deep.equal('foo OR bar')
   })
-  it('should join with NOT', () => {
-    expect(joinTags('none')(['foo', 'bar'])).to.deep.equal('NOT (foo OR bar)')
+  it('should join with OR and wrap with NOT', () => {
+    expect(joinTags('none', ['foo', 'bar'])).to.deep.equal('NOT (foo OR bar)')
   })
 })
 
@@ -84,26 +84,55 @@ describe('tagToQueryString', () => {
       tagToQueryString({ word: 'foo bar', distance: 'unlimited' })
     ).to.deep.equal('(foo AND bar)')
   })
+  it('should handle multiple words with unlimited distance and more than one space', () => {
+    expect(
+      tagToQueryString({ word: 'foo    bar    baz', distance: 'unlimited' })
+    ).to.deep.equal('(foo AND bar AND baz)')
+  })
   it('should handle multiple words with any order', () => {
-    expect(tagToQueryString({ word: 'foo bar', anyOrder: true })).to.deep.equal(
-      '(foo bar OR bar foo)'
-    )
+    expect(
+      tagToQueryString({ word: 'foo bar', anyOrder: true, isPhrase: true })
+    ).to.deep.equal(`("foo bar" OR "bar foo")`)
   })
 })
 
 describe('tagsToQueryString', () => {
-  it('should join multiple words', () => {
+  it('should join multiple tags', () => {
     expect(
       tagsToQueryString([{ word: 'foo' }, { word: 'bar' }], 'any')
     ).to.deep.equal('foo OR bar')
   })
-  it('should only include words if onlyShowTheseResults is enabled for a word', () => {
+  it('should join multiple tags with multi-word text', () => {
     expect(
       tagsToQueryString(
-        [{ word: 'foo', onlyShowTheseResults: true }, { word: 'bar' }],
+        [{ word: 'foo bar', anyOrder: true, isPhrase: true }, { word: 'baz' }],
+        'any'
+      )
+    ).to.deep.equal(`("foo bar" OR "bar foo") OR baz`)
+  })
+  it('should only include one word if onlyShowTheseResults is enabled for one tag', () => {
+    expect(
+      tagsToQueryString(
+        [
+          { word: 'foo', onlyShowTheseResults: true },
+          { word: 'bar' },
+          { word: 'baz' },
+        ],
         'any'
       )
     ).to.deep.equal('foo')
+  })
+  it('should only include two words if onlyShowTheseResults is enabled for two tags', () => {
+    expect(
+      tagsToQueryString(
+        [
+          { word: 'foo', onlyShowTheseResults: true },
+          { word: 'bar', onlyShowTheseResults: true },
+          { word: 'baz' },
+        ],
+        'any'
+      )
+    ).to.deep.equal('foo OR bar')
   })
 })
 
