d7e548b9195395103d007fdc6f944d5a8f58f564
Decrapifier <drodriguez@smartprocure.us>
2018-06-07 21:47:13 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/37

--- packages/react/src/utils/StripedLoader.js
+++ packages/react/src/utils/StripedLoader.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { bgStriped } from '../styles/generic'
 
-export default Component => observer(
-  props => 
-    props.loading
-      ? <div style={{ ...bgStriped, opacity: '0.5' }}>
+export default Component =>
+  observer(
+    props =>
+      props.loading ? (
+        <div style={{ ...bgStriped, opacity: '0.5' }}>
           <Component {...props} />
         </div>
-      : <Component {...props} />
-)
+      ) : (
+        <Component {...props} />
+      )
+  )
