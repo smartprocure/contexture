6f5f40394eb716381f0b1f2579b13f8dfd8c6c98
Samuel Greene <sgreene@smartprocure.us>
2018-06-07 21:36:43 +0000

    use loading in StripedLoader
    https://github.com/smartprocure/contexture-react/pull/37

--- packages/react/src/utils/StripedLoader.js
+++ packages/react/src/utils/StripedLoader.js
@@ -4,7 +4,7 @@ import { bgStriped } from '../styles/generic'
 
 export default Component => observer(
   props => 
-    (props.node.updating || props.node.markedForUpdate)
+    props.loading
       ? <div style={{ ...bgStriped, opacity: '0.5' }}>
           <Component {...props} />
         </div>
