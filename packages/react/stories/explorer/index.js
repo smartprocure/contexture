72cd9f7d495aae5a128c26847bb497876d10ea16
Samuel Greene <sgreene@smartprocure.us>
2018-06-08 19:48:45 +0000

    add basic search explorer story
    https://github.com/smartprocure/contexture-react/pull/39

--- packages/react/stories/explorer/index.js
+++ packages/react/stories/explorer/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill'
 import { storiesOf } from '@storybook/react'
 
 export default () =>
-  storiesOf('Index Explorer', module).addWithJSX(
-    'Advanced Search',
-    require('./advanced').default
-  )
+  storiesOf('Index Explorer', module)
+    .addWithJSX('Advanced Search', require('./advanced').default)
+    .addWithJSX('Basic Search', require('./basic').default)
