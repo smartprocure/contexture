e555b03904390f0b212d7a48ecb6f0948846e34a
Decrapifier <drodriguez@smartprocure.us>
2018-06-12 22:08:19 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/23

--- packages/provider-mongo/test/example-types/statistical.js
+++ packages/provider-mongo/test/example-types/statistical.js
@@ -4,27 +4,34 @@ let statistical = require('../../src/example-types/statistical')
 describe('statistical', () => {
   describe('result should output the expected auery', () => {
     let field = 'price'
-    expect(statistical.result({
-      key: 'test',
-      type: 'statistical',
-      field,
-    }, x => x)).eql([{
-      $group: {
-        _id: {},
-        count: { $sum: 1 },
-        max: {
-          $max: `$${field}`
+    expect(
+      statistical.result(
+        {
+          key: 'test',
+          type: 'statistical',
+          field,
         },
-        min: {
-          $min: `$${field}`
+        x => x
+      )
+    ).eql([
+      {
+        $group: {
+          _id: {},
+          count: { $sum: 1 },
+          max: {
+            $max: `$${field}`,
+          },
+          min: {
+            $min: `$${field}`,
+          },
+          avg: {
+            $avg: `$${field}`,
+          },
+          sum: {
+            $sum: `$${field}`,
+          },
         },
-        avg: {
-          $avg: `$${field}`
-        },
-        sum: {
-          $sum: `$${field}`
-        }
       },
-    }])
+    ])
   })
 })
