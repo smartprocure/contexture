0c57bb857a580e98b09cd58d658c0315c3487432
Decrapifier <drodriguez@smartprocure.us>
2018-06-12 22:08:19 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/23

--- packages/mongo/src/example-types/statistical.js
+++ packages/mongo/src/example-types/statistical.js
@@ -1,20 +1,23 @@
 module.exports = {
-  result: (context, search) => search([{
-    $group: {
-      _id: {},
-      count: { $sum: 1 },
-      max: {
-        $max: `$${context.field}`
+  result: (context, search) =>
+    search([
+      {
+        $group: {
+          _id: {},
+          count: { $sum: 1 },
+          max: {
+            $max: `$${context.field}`,
+          },
+          min: {
+            $min: `$${context.field}`,
+          },
+          avg: {
+            $avg: `$${context.field}`,
+          },
+          sum: {
+            $sum: `$${context.field}`,
+          },
+        },
       },
-      min: {
-        $min: `$${context.field}`
-      },
-      avg: {
-        $avg: `$${context.field}`
-      },
-      sum: {
-        $sum: `$${context.field}`
-      }
-    },
-  }]),
+    ]),
 }
