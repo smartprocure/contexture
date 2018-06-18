ad07d7b1588a65f2245fec59bcdfac4cfe1adf8a
Decrapifier <drodriguez@smartprocure.us>
2018-06-18 22:06:53 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/27

--- packages/provider-mongo/test/example-types/exists.js
+++ packages/provider-mongo/test/example-types/exists.js
@@ -5,15 +5,25 @@ let exists = require('../../src/example-types/exists')
 describe('exists', () => {
   describe('exists.filter', () => {
     it('If a value is provided, use $and', () => {
-      expect(_.get('$and.0.myField.$exists', exists.filter({
-        value: 'myValue',
-        field: 'myField',
-      }))).to.equal('myValue')
+      expect(
+        _.get(
+          '$and.0.myField.$exists',
+          exists.filter({
+            value: 'myValue',
+            field: 'myField',
+          })
+        )
+      ).to.equal('myValue')
     })
     it('If no value is provided, use $or', () => {
-      expect(_.get('$or.0.myField.$exists', exists.filter({
-        field: 'myField',
-      }))).to.equal(false)
+      expect(
+        _.get(
+          '$or.0.myField.$exists',
+          exists.filter({
+            field: 'myField',
+          })
+        )
+      ).to.equal(false)
     })
   })
 })
--- packages/provider-mongo/test/example-types/number.js
+++ packages/provider-mongo/test/example-types/number.js
@@ -7,40 +7,46 @@ describe('number', () => {
       expect(number.hasValue({ min: 1 })).to.equal(true)
       expect(number.hasValue({ max: 2 })).to.equal(true)
     }),
-    it('Allows 0 on min and max', () => {
-      expect(number.hasValue({ min: 0 })).to.equal(true)
-      expect(number.hasValue({ max: 0 })).to.equal(true)
-    })
+      it('Allows 0 on min and max', () => {
+        expect(number.hasValue({ min: 0 })).to.equal(true)
+        expect(number.hasValue({ max: 0 })).to.equal(true)
+      })
   })
   describe('number.filter', () => {
     it('Should convert both min and max to valid numbers', () => {
-      expect(number.filter({
-        min: '123',
-        max: '321',
-        field: 'myField',
-      })).to.deep.equal({
+      expect(
+        number.filter({
+          min: '123',
+          max: '321',
+          field: 'myField',
+        })
+      ).to.deep.equal({
         myField: {
           $gte: 123,
           $lte: 321,
-        }
+        },
       })
     })
     it('Should remove unused properties', () => {
-      expect(number.filter({
-        min: '123',
-        field: 'myField',
-      })).to.deep.equal({
+      expect(
+        number.filter({
+          min: '123',
+          field: 'myField',
+        })
+      ).to.deep.equal({
         myField: {
           $gte: 123,
-        }
+        },
       })
-      expect(number.filter({
-        max: '321',
-        field: 'myField',
-      })).to.deep.equal({
+      expect(
+        number.filter({
+          max: '321',
+          field: 'myField',
+        })
+      ).to.deep.equal({
         myField: {
           $lte: 321,
-        }
+        },
       })
     })
   })
