a28aade0499591653c4b05cc265b3cb19bfa5427
Samuel Greene <sgreene@smartprocure.us>
2018-07-10 18:56:12 +0000

    add basic ie grid support
    https://github.com/smartprocure/contexture-react/pull/45

--- packages/react/src/layout/Grid.js
+++ packages/react/src/layout/Grid.js
@@ -4,7 +4,10 @@ export let Grid = ({ style, columns, gap, ...x }) => (
   <div
     style={{
       display: 'grid',
-      ...(columns && { gridTemplateColumns: columns }),
+      ...(columns && {
+        gridTemplateColumns: columns,
+        msGridTemplateColumns: columns
+      }),
       ...(gap && { gridGap: gap }),
       ...style,
     }}
