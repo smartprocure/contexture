374b4cff18eb4ec25d2f4e999fd7df4af1645aab
Samuel Greene <sgreene@smartprocure.us>
2018-07-14 20:09:26 +0000

    make imdb advanced work with filter adder
    https://github.com/smartprocure/contexture-react/pull/47

--- packages/react/stories/imdb/stories/advanced.js
+++ packages/react/stories/imdb/stories/advanced.js
@@ -3,12 +3,11 @@ import React from 'react'
 import { Provider } from 'mobx-react'
 import { fromPromise } from 'mobx-utils'
 import QueryBuilder from '../../../src/queryBuilder/'
-import ExampleTypes from '../../../src/exampleTypes/'
 import { Awaiter } from '../../../src/layout/'
 import Contexture, { esClient } from '../utils/contexture'
 import { getESSchemas } from '../../../src/utils/schema'
-import { Input } from '../../DemoControls'
-let { ResultCount, ResultTable, TypeMap } = ExampleTypes({ Input })
+import { ExampleTypes } from '../../DemoControls'
+let { ResultCount, ResultTable, TypeMap } = ExampleTypes
 
 let tree = Contexture({
   key: 'root',
@@ -57,22 +56,23 @@ let schemas = fromPromise(
 export default () => (
   <Awaiter promise={schemas}>
     {schemas => (
-      <div>
-        <Provider tree={tree}>
-          <div>
-            <QueryBuilder
-              types={TypeMap}
-              fields={schemas.movies.fields}
-              path={['root', 'searchRoot']}
-            />
-            <h1>
-              <ResultCount path={['root', 'results']} />
-            </h1>
-            <ResultTable path={['root', 'results']} infer />
-          </div>
-        </Provider>
-        <pre>{JSON.stringify(tree, null, 2)}</pre>
-      </div>
+      <Provider tree={tree}>
+        <div>
+          <QueryBuilder
+            types={TypeMap}
+            fields={schemas.movies.fields}
+            path={['root', 'searchRoot']}
+          />
+          <h1>
+            <ResultCount path={['root', 'results']} />
+          </h1>
+          <ResultTable
+            path={['root', 'results']}
+            fields={schemas.movies.fields}
+          />
+          <pre>{JSON.stringify(tree, null, 2)}</pre>
+        </div>
+      </Provider>
     )}
   </Awaiter>
 )
