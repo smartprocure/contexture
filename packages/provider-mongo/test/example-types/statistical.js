244573ffa1963615db923476101aca4dfe8c6a0a
Samuel Greene <sgreene@smartprocure.us>
2018-07-18 23:20:08 +0000

    handle statistical case where there are no results
    https://github.com/smartprocure/contexture-mongo/pull/34

--- packages/provider-mongo/test/example-types/statistical.js
+++ packages/provider-mongo/test/example-types/statistical.js
@@ -33,5 +33,24 @@ describe('statistical', () => {
         },
       })
     })
+    it('result should handle no results', async () => {
+      let field = 'price'
+      expect(
+        await statistical.result(
+          {
+            key: 'test',
+            type: 'statistical',
+            field,
+          },
+          x => []
+        )
+      ).eql({
+        count: 0,
+        avg: 0,
+        max: 0,
+        min: 0,
+        sum: 0,
+      })
+    })
   })
 })
