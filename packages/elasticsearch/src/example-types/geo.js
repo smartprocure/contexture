f9092ac0eb3874de65cfce8c7cb5b7c2850c85fd
Decrapifier <decrapifier@govspend.us>
2018-08-15 20:31:30 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-elasticsearch/pull/92

--- packages/elasticsearch/src/example-types/geo.js
+++ packages/elasticsearch/src/example-types/geo.js
@@ -7,20 +7,24 @@ let geo = ({
   },
 } = {}) => ({
   hasValue: context =>
-    !!((context.location || (context.latitude && context.longitude)) && context.radius && context.operator),
+    !!(
+      (context.location || (context.latitude && context.longitude)) &&
+      context.radius &&
+      context.operator
+    ),
   filter: context =>
     Promise.resolve(context)
       .then(context => {
         if (context.latitude && context.longitude) {
           return {
             Latitude: context.latitude,
-            Longitude: context.longitude
+            Longitude: context.longitude,
           }
         } else {
           return geocodeLocation(context.location)
         }
-      }).then(response => {
-
+      })
+      .then(response => {
         context._meta.preprocessorResult = response
 
         let result = {
@@ -42,10 +46,14 @@ let geo = ({
         console.error('An error occured within the geo provider: ', err)
       ),
   validContext: context =>
-    !!((context.location || (context.latitude && context.longitude)) && context.radius && context.operator),
+    !!(
+      (context.location || (context.latitude && context.longitude)) &&
+      context.radius &&
+      context.operator
+    ),
   result: context => ({
     Latitude: _.get('_meta.preprocessorResult.latitude', context),
-    Longitude: _.get('_meta.preprocessorResult.longitude', context)
+    Longitude: _.get('_meta.preprocessorResult.longitude', context),
   }),
 })
 
