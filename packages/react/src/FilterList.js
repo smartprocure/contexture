ff0acc6524f1f8ea535675e4dc26d11872d6d2e5
Dimitar Shishkov <dshishkov@smartprocure.us>
2018-08-28 14:40:05 -0400

    use provider for tree inject on Label comp
    https://github.com/smartprocure/contexture-react/pull/64

--- packages/react/src/FilterList.js
+++ packages/react/src/FilterList.js
@@ -1,44 +1,39 @@
 import React from 'react'
 import _ from 'lodash/fp'
-import { observer } from 'mobx-react'
+import { observer, inject } from 'mobx-react'
 import { Dynamic, SpacedList } from './layout'
 import InjectTreeNode from './utils/injectTreeNode'
 
-export let Label = observer(x => (
-  <div style={{ margin: '10px 0' }}>
-    <b {...x} />
-    {x.tree && (
-      <span
-        style={{ float: 'right', marginRight: '5px', cursor: 'pointer' }}
-        onClick={() => x.tree.mutate(x.node.path, { paused: !x.node.paused })}
-      >
-        {x.node.paused ? '◀' : '▼'}
-      </span>
-    )}
-  </div>
-))
+export let Label = inject(_.identity)(
+  observer(({ tree, node, ...x }) => (
+    <div style={{ margin: '10px 0' }}>
+      <b {...x} />
+      {tree &&
+        node && (
+          <span
+            style={{ float: 'right', marginRight: '5px', cursor: 'pointer' }}
+            onClick={() => tree.mutate(node.path, { paused: !node.paused })}
+          >
+            {node.paused ? '◀' : '▼'}
+          </span>
+        )}
+    </div>
+  ))
+)
 
 export let FieldLabel = InjectTreeNode(
-  observer(({ tree, node, node: { field } = {}, fields }) => (
-    <Label node={node} tree={tree}>
-      {_.get([field, 'label'], fields)}
-    </Label>
+  observer(({ node, node: { field } = {}, fields }) => (
+    <Label node={node}>{_.get([field, 'label'], fields)}</Label>
   ))
 )
 
 export let FilterList = InjectTreeNode(
   observer(
-    ({
-      tree,
-      node,
-      typeComponents: types,
-      fields,
-      mapNodeToProps = _.noop,
-    }) => (
+    ({ node, typeComponents: types, fields, mapNodeToProps = _.noop }) => (
       <SpacedList>
         {node.children.map(child => (
           <div key={child.path}>
-            <FieldLabel tree={tree} node={child} fields={fields} />
+            <FieldLabel node={child} fields={fields} />
             {!child.paused && (
               <Dynamic
                 component={types[child.type]}
