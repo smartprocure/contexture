ffba5fff070598ac860f8bdab07f53d2c09e2153
David Sargeant <david@dsargeant.com>
2018-08-29 15:08:50 -0400

    Handle edge cases around number type.
    https://github.com/smartprocure/contexture-mongo/pull/38

--- packages/mongo/src/example-types/number.js
+++ packages/mongo/src/example-types/number.js
@@ -1,15 +1,17 @@
 var _ = require('lodash/fp')
 
+let predicate = x =>
+  (_.isString(x) || _.isNumber(x)) && x !== '' && _.isFinite(_.toNumber(x))
+
 let cleanFilter = _.flow(
-  _.pickBy(_.negate(_.isNil)),
+  _.pickBy(predicate),
   _.mapValues(_.toNumber)
 )
 
 module.exports = {
   hasValue: _.flow(
     _.pick(['min', 'max']),
-    _.mapValues(_.toNumber),
-    _.some(x => !_.isNaN(x))
+    _.some(predicate)
   ),
   filter: ({ field, min, max }) => ({
     [field]: cleanFilter({
