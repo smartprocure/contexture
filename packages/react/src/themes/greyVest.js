a9da1b9f3f6def9fa1b7fb040cd16795a5450e86
Samuel Greene <sgreene@smartprocure.us>
2018-10-05 20:54:18 +0000

    add Fonts to greyVest, including font awesome
    https://github.com/smartprocure/contexture-react/pull/79

--- packages/react/src/themes/greyVest.js
+++ packages/react/src/themes/greyVest.js
@@ -44,6 +44,15 @@ export let Checkbox = ({ checked, onChange, style = {} }) => (
   </label>
 )
 
+export let Fonts = () =>
+  <div>
+    <link
+      href="https://fonts.googleapis.com/css?family=Lato:400,600,700,900"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
+  </div>
+
 export let GVStyle = () => (
   <style>
     {`
