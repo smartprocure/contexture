570730bcf4b136c73ff619970b5053733664d6f6
David Sargeant <david@dsargeant.com>
2018-10-10 14:56:02 -0400

    Best range fix. Formatting!
    https://github.com/smartprocure/contexture-elasticsearch/pull/98

--- packages/provider-elasticsearch/src/schema.js
+++ packages/provider-elasticsearch/src/schema.js
@@ -16,10 +16,7 @@ let firstValue = _.curry((field, data) => _.get(field, _.find(field, data)))
 
 let Tree = F.tree(x => x.properties)
 // flatLeaves should auto detect reject vs omit (or just more general obj vs arr method)
-let flatten = _.flow(
-  Tree.flatten(),
-  _.omitBy(Tree.traverse)
-)
+let flatten = _.flow(Tree.flatten(), _.omitBy(Tree.traverse))
 
 let fromEsIndexMapping = _.mapValues(
   _.flow(
