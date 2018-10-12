351f23a57a844abdb9fc10666eae26e0c4bb81ed
Dimitar Shishkov <dshishkov@smartprocure.us>
2018-10-12 10:17:05 -0400

    split ExpandableTable into some sub-components
    https://github.com/smartprocure/contexture-react/pull/78

--- packages/react/src/layout/ExpandableTable.js
+++ packages/react/src/layout/ExpandableTable.js
@@ -7,7 +7,24 @@ import { observer, inject } from 'mobx-react'
 export let Column = _.identity
 Column.displayName = 'Column'
 
-let State = (stores, props) => {
+let ExpandedSection = observer(({ recordKey, columnCount, expanded }) =>
+  _.flow(
+    key => expanded.get(key),
+    expandedRow =>
+      _.getOr(null, 'details.Component', expandedRow) && (
+        <tr align="center">
+          <td colSpan={columnCount}>
+            {expandedRow.details.Component(
+              _.get(expandedRow.field, expandedRow.record),
+              expandedRow.record
+            )}
+          </td>
+        </tr>
+      )
+  )(recordKey)
+)
+
+let TableBodyState = () => {
   let state = {
     expanded: observable(new Map()),
     onClick(field, keyField, record, index, details) {
@@ -26,110 +43,92 @@ let State = (stores, props) => {
         state.expanded.delete(key)
       }
     },
-    columns: _.map(
-      ({ props }) => ({
-        ..._.pick(['field', 'label', 'display'], props),
-        details: F.compactObject({
-          ..._.pick(['expand', 'collapse'], props),
-          Component: props.children,
-        }),
-      }),
-      _.castArray(props.children)
-    ),
   }
+
   return state
 }
 
-let ExpandedSection = observer(({ recordKey, columnCount, expanded }) =>
-  _.flow(
-    key => expanded.get(key),
-    expandedRow =>
-      _.getOr(null, 'details.Component', expandedRow) && (
-        <tr align="center">
-          <td colSpan={columnCount}>
-            {expandedRow.details.Component(
-              _.get(expandedRow.field, expandedRow.record),
-              expandedRow.record
-            )}
-          </td>
-        </tr>
-      )
-  )(recordKey)
+let TableBody = inject(TableBodyState)(
+  observer(({ data, columns, recordKey, expanded, onClick }) => (
+    <tbody>
+      {_.map(
+        x => (
+          <React.Fragment key={x[recordKey]}>
+            <tr {...x.rowAttrs} key={x[recordKey]}>
+              {F.mapIndexed(
+                ({ field, display = x => x, details = {} }, i) => (
+                  <td
+                    key={`${field}${i}`}
+                    {...!_.isEmpty(details) && {
+                      style: {
+                        cursor: !_.isEmpty(details) ? 'pointer' : 'auto',
+                      },
+                      onClick: () =>
+                        onClick(field, recordKey, x, i, details, expanded),
+                    }}
+                  >
+                    {_.getOr(
+                      display,
+                      `${
+                        _.isEqual(
+                          _.get('indexedField', expanded.get(x[recordKey])),
+                          `${field}${i}`
+                        )
+                          ? 'collapse'
+                          : 'expand'
+                      }.display`,
+                      details
+                    )(_.get(field, x), x)}
+                  </td>
+                ),
+                columns
+              )}
+            </tr>
+            {/* See if there is a details component to render for the column value when row expanded */}
+            <ExpandedSection
+              recordKey={x[recordKey]}
+              expanded={expanded}
+              columnCount={columns.length}
+            />
+          </React.Fragment>
+        ),
+        data
+      )}
+    </tbody>
+  ))
 )
 
-let TableBody = observer(({ data, columns, recordKey, expanded, onClick }) => (
-  <tbody>
-    {_.map(
-      x => (
-        <React.Fragment key={x[recordKey]}>
-          <tr {...x.rowAttrs} key={x[recordKey]}>
-            {F.mapIndexed(
-              ({ field, display = x => x, details = {} }, i) => (
-                <td
-                  key={`${field}${i}`}
-                  {...!_.isEmpty(details) && {
-                    style: {
-                      cursor: !_.isEmpty(details) ? 'pointer' : 'auto',
-                    },
-                    onClick: () => onClick(field, recordKey, x, i, details),
-                  }}
-                >
-                  {_.getOr(
-                    display,
-                    `${
-                      _.isEqual(
-                        _.get('indexedField', expanded.get(x[recordKey])),
-                        `${field}${i}`
-                      )
-                        ? 'collapse'
-                        : 'expand'
-                    }.display`,
-                    details
-                  )(_.get(field, x), x)}
-                </td>
-              ),
-              columns
-            )}
-          </tr>
-          {/* See if there is a details component to render for the column value when row expanded */}
-          <ExpandedSection
-            recordKey={x[recordKey]}
-            expanded={expanded}
-            columnCount={columns.length}
-          />
-        </React.Fragment>
-      ),
-      data
-    )}
-  </tbody>
-))
+let TableState = (stores, props) => ({
+  columns: _.map(
+    ({ props }) => ({
+      ..._.pick(['field', 'label', 'display'], props),
+      details: F.compactObject({
+        ..._.pick(['expand', 'collapse'], props),
+        Component: props.children,
+      }),
+    }),
+    _.castArray(props.children)
+  ),
+})
 
-let ExpandableTable = inject(State)(
-  observer(
-    ({ data, columns, recordKey = 'key', expanded, onClick, ...props }) => (
-      <table {...props.tableAttrs}>
-        <thead>
-          <tr>
-            {F.mapIndexed(
-              (c, i) => (
-                <th key={`${c.field}${i}`}>
-                  {F.callOrReturn(_.getOr(F.autoLabel(c.field), 'label', c))}
-                </th>
-              ),
-              columns
-            )}
-          </tr>
-        </thead>
-        <TableBody
-          columns={columns}
-          data={data}
-          recordKey={recordKey}
-          expanded={expanded}
-          onClick={onClick}
-        />
-      </table>
-    )
-  )
+let ExpandableTable = inject(TableState)(
+  observer(({ data, columns, recordKey = 'key', ...props }) => (
+    <table {...props.tableAttrs}>
+      <thead>
+        <tr>
+          {F.mapIndexed(
+            (c, i) => (
+              <th key={`${c.field}${i}`}>
+                {F.callOrReturn(_.getOr(F.autoLabel(c.field), 'label', c))}
+              </th>
+            ),
+            columns
+          )}
+        </tr>
+      </thead>
+      <TableBody columns={columns} data={data} recordKey={recordKey} />
+    </table>
+  ))
 )
 
 ExpandableTable.displayName = 'ExpandableTable'
