0909c4b87365980bb42932c422dc2b182b648d7f
Daniel Rodríguez <sadasant@gmail.com>
2018-10-12 16:45:35 +0000

    lint fix
    https://github.com/smartprocure/contexture-export/pull/1

--- packages/export/.eslintrc.js
+++ packages/export/.eslintrc.js
@@ -2,6 +2,15 @@ module.exports = {
   extends: 'smartprocure',
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaVersion: 2017
+  },
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  },
+  rules: {
+    "no-extra-semi": 0
   }
 }
