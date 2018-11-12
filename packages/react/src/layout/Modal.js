649075522aaed9a0681746a3bf61be62f9761744
Samuel Greene <sgreene@smartprocure.us>
2018-11-12 23:32:38 +0000

    make modal fit-content
    https://github.com/smartprocure/contexture-react/pull/97

--- packages/react/src/layout/Modal.js
+++ packages/react/src/layout/Modal.js
@@ -24,7 +24,7 @@ let Modal = observer(
           style={{
             backgroundColor: '#fff',
             borderRadius: 5,
-            maxWidth: 500,
+            maxWidth: 'fit-content',
             // minHeight: 300,
             margin: '0 auto',
             padding: 30,
