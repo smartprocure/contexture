a306db947095bb8ef321561a8ed0e241cecdcba3
Decrapifier <decrapifier@govspend.us>
2018-12-20 03:54:12 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-client/pull/87

--- packages/client/src/exampleTypes.js
+++ packages/client/src/exampleTypes.js
@@ -56,7 +56,7 @@ export default stampKey('type', {
     defaults: {
       field: null,
       value: '',
-      operator: 'containsWord'
+      operator: 'containsWord',
     },
   },
   tagsText: {
