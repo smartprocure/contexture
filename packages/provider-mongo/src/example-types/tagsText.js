2961232916b525e70995c8071978e0b20f31a8a4
Samuel Greene <sgreene@smartprocure.us>
2019-01-02 18:27:43 -0500

    add tagsText example type
    https://github.com/smartprocure/contexture-mongo/pull/42

---
+++ packages/provider-mongo/src/example-types/tagsText.js
@@ -0,0 +1,8 @@
+let _ = require('lodash/fp')
+let text = require('./text')
+
+// `tagsText` is just text with only values and not value
+module.exports = {
+  hasValue: _.get('values.length'),
+  filter: text.fiter,
+}
