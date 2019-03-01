6268200e136cadca4ec0f855b24c62d802467dd8
Alejandro Hernandez <ahernandez@govspend.com>
2019-03-01 15:10:09 -0500

    Add clear action to reset a node's value to its defaults
    https://github.com/smartprocure/contexture-client/pull/96

--- packages/client/src/actions.js
+++ packages/client/src/actions.js
@@ -1,6 +1,7 @@
 import _ from 'lodash/fp'
 import { pullOn } from 'futil-js'
 import { encode } from './util/tree'
+import { getTypeProp } from './types'
 
 export default ({
   getNode,
@@ -10,36 +11,46 @@ export default ({
   types,
   extend,
   initNode,
-}) => ({
-  async add(parentPath, node) {
-    let target = getNode(parentPath)
-    let path = [...parentPath, node.key]
-    // TODO: Does not currently call init on child nodes
-    initNode(node, path, extend, types)
-    target.children.push(node)
-    // Need this nonsense to support the case where push actually mutates, e.g. a mobx observable tree
-    flat[encode(path)] = target.children[target.children.length - 1]
-    return dispatch({ type: 'add', path, node })
-  },
-  async remove(path) {
-    let previous = getNode(path)
-    let parent = getNode(_.dropRight(1, path))
-    pullOn(previous, parent.children)
-    delete flat[encode(path)]
-    return dispatch({ type: 'remove', path, previous })
-  },
-  mutate: _.curry(async (path, value) => {
-    let target = getNode(path)
-    let previous = snapshot(_.omit('children', target))
-    extend(target, value)
-    return dispatch({
-      type: 'mutate',
+}) => {
+  let actions = {
+    async add(parentPath, node) {
+      let target = getNode(parentPath)
+      let path = [...parentPath, node.key]
+      // TODO: Does not currently call init on child nodes
+      initNode(node, path, extend, types)
+      target.children.push(node)
+      // Need this nonsense to support the case where push actually mutates, e.g. a mobx observable tree
+      flat[encode(path)] = target.children[target.children.length - 1]
+      return dispatch({ type: 'add', path, node })
+    },
+    async remove(path) {
+      let previous = getNode(path)
+      let parent = getNode(_.dropRight(1, path))
+      pullOn(previous, parent.children)
+      delete flat[encode(path)]
+      return dispatch({ type: 'remove', path, previous })
+    },
+    mutate: _.curry(async (path, value) => {
+      let target = getNode(path)
+      let previous = snapshot(_.omit('children', target))
+      extend(target, value)
+      return dispatch({
+        type: 'mutate',
+        path,
+        previous,
+        value,
+        node: target,
+      })
+    }),
+    refresh: path => dispatch({ type: 'refresh', path }),
+    triggerUpdate: () => dispatch({ type: 'none', path: [], autoUpdate: true }),
+  }
+
+  actions.clear = path =>
+    actions.mutate(
       path,
-      previous,
-      value,
-      node: target,
-    })
-  }),
-  refresh: path => dispatch({ type: 'refresh', path }),
-  triggerUpdate: () => dispatch({ type: 'none', path: [], autoUpdate: true }),
-})
+      _.omit(['field'], getTypeProp(types, 'defaults', getNode(path)))
+    )
+
+  return actions
+}
