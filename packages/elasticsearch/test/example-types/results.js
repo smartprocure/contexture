9c12d2f6f6ee26586a0464d9f0ed802bf87b2346
Decrapifier <decrapifier@govspend.us>
2019-03-06 01:17:14 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-elasticsearch/pull/118

--- packages/elasticsearch/test/example-types/results.js
+++ packages/elasticsearch/test/example-types/results.js
@@ -21,7 +21,7 @@ describe('results', () => {
             {
               _id: 'test-id',
               field: 'test field',
-            }
+            },
           ],
         },
       },
@@ -143,7 +143,7 @@ describe('results', () => {
   it('should sort on sortField config', async () => {
     let sortField = 'test.field'
     F.extendOn(context, { sortField })
-    await  resultsTest(context, [
+    await resultsTest(context, [
       _.extend(expectedCalls[0], {
         sort: {
           [context.sortField]: 'desc',
