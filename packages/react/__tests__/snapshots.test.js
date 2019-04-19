a6c5e3c40c9e4c36dffa7f9b5676a5060abfe8f8
Samuel Greene <sgreene@smartprocure.us>
2019-04-18 23:08:54 -0400

    fix jest snapshot tests
    https://github.com/smartprocure/contexture-react/pull/210

---
+++ packages/react/__tests__/snapshots.test.js
@@ -0,0 +1,5 @@
+import initStoryshots from '@storybook/addon-storyshots'
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+
+registerRequireContextHook()
+initStoryshots()
