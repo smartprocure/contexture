49da86f0907315311b5925b4ab09639baf5c7d5d
Decrapifier <decrapifier@govspend.us>
2019-04-24 07:35:22 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-client/pull/99

--- packages/client/src/actions.js
+++ packages/client/src/actions.js
@@ -3,7 +3,7 @@ import { pullOn } from 'futil-js'
 import { encode, Tree } from './util/tree'
 import { getTypeProp } from './types'
 
-let pushOrSpliceOn = (array, item, index)  => {
+let pushOrSpliceOn = (array, item, index) => {
   if (index === undefined) array.push(item)
   else array.splice(index, 0, item)
   return array
@@ -24,14 +24,14 @@ export default ({
       initNode(node, path, extend, types)
       flat[encode(path)] = node
     })(node)
-    
+
     let target = getNode(parentPath)
-    
+
     // consider moving this in the tree walk? it could work for al children too but would be exgra work for chilren
     pushOrSpliceOn(target.children, node, index)
     // Need this nonsense to support the case where push actually mutates, e.g. a mobx observable tree
     // flat[encode(path)] = target.children[index]
-    
+
     return dispatch({ type: 'add', path: _.toArray(node.path), node })
   }
 
@@ -40,9 +40,9 @@ export default ({
     let parentPath = _.dropRight(1, path)
     let parent = getNode(parentPath)
     pullOn(previous, parent.children)
-    
+
     Tree.walk((node, index, [parent = {}]) => {
-      let path = [...parent.path || parentPath, node.key]
+      let path = [...(parent.path || parentPath), node.key]
       delete flat[encode(path)]
     })(previous)
 
@@ -72,7 +72,7 @@ export default ({
       path,
       _.omit(['field'], getTypeProp(types, 'defaults', getNode(path)))
     )
-  
+
   let replace = (path, node) => {
     let parentPath = _.dropRight(1, path)
     let index = _.findIndex(getNode(path), getNode(parentPath).children)
@@ -82,7 +82,7 @@ export default ({
 
   let shallowCloneNode = node => ({
     ...node,
-    children: [...node.children]
+    children: [...node.children],
   })
 
   // indent in place should make node key be the new key and put root _inside_ the thing
@@ -93,24 +93,24 @@ export default ({
 
     // Remove all children (they'll be readded at the end when we add the shallow clone)
     await Promise.all(_.map(child => remove(child.path), node.children))
-    
+
     // Mutate existing root into new root
     await mutate(path, {
       ...newNode,
-      path: [newNode.key]
+      path: [newNode.key],
     })
-    
+
     // Replace flat tree references to root
     flat[encode([newNode.key])] = flat[encode(path)]
     delete flat[encode(path)]
-    
+
     // Add original root as a child of the new root
     await add([newNode.key], node)
   }
   let indentReplace = async (path, newNode) =>
     replace(path, {
       ...newNode,
-      children: [getNode(path)]
+      children: [getNode(path)],
     })
 
   let indent = async (path, newNode) =>
@@ -121,17 +121,16 @@ export default ({
   let move = (path, { path: targetPath, index: targetIndex } = {}) => {
     let parentPath = _.dropRight(1, path)
     targetPath = targetPath || parentPath
-    
+
     let node = getNode(path)
     if (_.isEqual(parentPath, targetPath)) {
       // Same group, no dispatch or updating of paths needed - just rearrange children
       pullOn(node, getNode(parentPath).children)
       pushOrSpliceOn(getNode(targetPath).children, node, targetIndex)
-    }
-    else {
+    } else {
       return Promise.all([
         remove(path),
-        add(targetPath, node, { index: targetIndex })
+        add(targetPath, node, { index: targetIndex }),
       ])
     }
   }
@@ -139,9 +138,7 @@ export default ({
   let mutateNested = (path, payload) =>
     _.flow(
       getNode,
-      Tree.toArrayBy(
-        node => mutate(node.path.slice(), payload)
-      ),
+      Tree.toArrayBy(node => mutate(node.path.slice(), payload)),
       x => Promise.all(x)
     )(path)
 
