30b4e893468be245f8aa32d036fc4edc025844f8
Samuel Greene <sgreene@smartprocure.us>
2019-04-25 00:01:18 -0400

    remove paging info from ResultCount
    https://github.com/smartprocure/contexture-react/pull/211

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -4,15 +4,11 @@ import injectTreeNode from '../utils/injectTreeNode'
 import { exampleTypes } from 'contexture-client'
 
 let ResultCount = injectTreeNode(
-  observer(({ node }) => (
-    <span>
-      {node.context.response.results.length
-        ? `${node.context.response.startRecord} - ${
-            node.context.response.endRecord
-          } out of ${node.context.response.totalRecords}`
-        : 'No Results'}
-    </span>
-  )),
+  observer(({ node, display = x => x }) => 
+    node.context.response.results.length
+      ? display(node.context.response.totalRecords)
+      : 'No Results'
+  ),
   {
     ...exampleTypes.results,
     style: { display: 'inline-block' },
