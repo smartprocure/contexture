7670ee2900ed16c48f0b7dd93a94ed53e72098d3
Samuel Greene <sgreene@smartprocure.us>
2019-05-07 02:46:02 -0400

    improve storybook jsx panel rendering
    https://github.com/smartprocure/contexture-react/pull/219

--- packages/react/stories/greyVest/linkButton.stories.js
+++ packages/react/stories/greyVest/linkButton.stories.js
@@ -4,8 +4,10 @@ import { action } from '@storybook/addon-actions'
 import decorator from './decorator'
 import { LinkButton } from './../../src/themes/greyVest'
 
+let click = action('clicked')
+
 storiesOf('Non Search Components|Grey Vest', module)
   .addDecorator(decorator)
   .addWithJSX('LinkButton', () =>
-    <LinkButton onClick={() => action('clicked')()}>Click</LinkButton>
+    <LinkButton onClick={() => click()}>Click</LinkButton>
   )
