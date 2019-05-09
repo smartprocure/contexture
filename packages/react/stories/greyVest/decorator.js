aa7de80ce9782fab5b38e3becc07ca126a499da9
Samuel Greene <sgreene@smartprocure.us>
2019-05-09 00:58:12 -0400

    clean up decorator
    https://github.com/smartprocure/contexture-react/pull/219

--- packages/react/stories/greyVest/decorator.js
+++ packages/react/stories/greyVest/decorator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Fonts, GVStyle } from './../../src/themes/greyVest'
 
 export default storyFn =>
-  <div>
+  <div className="gv-body">
     <Fonts />
     <GVStyle />
     {storyFn()}
