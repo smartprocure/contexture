a8ec0b1051cae3b139ab51be6b3eec96449d6886
Samuel Greene <sgreene@smartprocure.us>
2019-05-10 16:11:15 -0400

    refactor SearchFilters into smaller components (same public API)
    https://github.com/smartprocure/contexture-react/pull/220

--- packages/react/src/themes/greyVest/SearchFilters.js
+++ packages/react/src/themes/greyVest/SearchFilters.js
@@ -6,43 +6,49 @@ import LinkButton from './LinkButton'
 import TreePauseButton from './TreePauseButton'
 import ToggleFiltersButton from './ToggleFiltersButton'
 
+let LabelledList = ({ list, Component }) =>
+  F.mapIndexed(
+    ({ label, ...props }, i) => (
+      <React.Fragment key={i}>
+        {label && <h3>{label}</h3>}
+        <Component {...props} />
+      </React.Fragment>
+    ),
+    list
+  )
+
+let BasicSearchFilters = ({ setMode, trees, children, FiltersBox }) => (
+  <div>
+    <Flex style={{ alignItems: 'center' }}>
+      <h1>Filters</h1>
+      <TreePauseButton children={children} />
+      <ToggleFiltersButton onClick={() => setMode('resultsOnly')} />
+    </Flex>
+    <LabelledList list={trees} Component={FiltersBox} />
+    <LinkButton onClick={() => setMode('builder')} style={{ marginTop: 15 }}>
+      Switch to Advanced Search Builder
+    </LinkButton>
+  </div>
+)
+let BuilderSearchFilters = ({ setMode, trees, QueryBuilder }) => (
+  <div>
+    <Flex style={{ alignItems: 'center' }}>
+      <h1>Filters</h1>
+      <LinkButton onClick={() => setMode('basic')}>
+        Back to Regular Search
+      </LinkButton>
+    </Flex>
+    <LabelledList list={trees} Component={QueryBuilder} />
+  </div>
+)
+
 let SearchFilters = ({ mode, setMode, children, QueryBuilder, FiltersBox }) => {
   let trees = React.Children.map(children, x => x.props)
-  let SearchGroupComponent = mode === 'builder' ? QueryBuilder : FiltersBox
-  return mode === 'resultsOnly' ? null : (
-    <div>
-      <Flex style={{ alignItems: 'center' }}>
-        <h1>Filters</h1>
-        {mode === 'basic' ? (
-          <>
-            <TreePauseButton children={children} />
-            <ToggleFiltersButton onClick={() => setMode('resultsOnly')} />
-          </>
-        ) : (
-          <LinkButton onClick={() => setMode('basic')}>
-            Back to Regular Search
-          </LinkButton>
-        )}
-      </Flex>
-      {F.mapIndexed(
-        ({ label, ...props }, i) => (
-          <React.Fragment key={i}>
-            {label && <h3>{label}</h3>}
-            <SearchGroupComponent {...props} />
-          </React.Fragment>
-        ),
-        trees
-      )}
-      {mode === 'basic' && (
-        <LinkButton
-          onClick={() => setMode('builder')}
-          style={{ marginTop: 15 }}
-        >
-          Switch to Advanced Search Builder
-        </LinkButton>
-      )}
-    </div>
-  )
+  if (mode === 'resultsOnly') return null
+  if (mode === 'basic')
+    return <BasicSearchFilters {...{ trees, setMode, children, FiltersBox }} />
+  if (mode === 'builder')
+    return <BuilderSearchFilters {...{ trees, setMode, QueryBuilder }} />
 }
 
 export default observer(SearchFilters)
