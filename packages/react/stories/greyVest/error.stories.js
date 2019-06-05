9e4fdb462d9511376c60d33c3919ee2363cdb2f5
Michelle Saad <git@michsa.me>
2019-06-05 15:13:47 -0400

    Add 'form demo' error story
    https://github.com/smartprocure/contexture-react/pull/241

--- packages/react/stories/greyVest/error.stories.js
+++ packages/react/stories/greyVest/error.stories.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import decorator from './decorator'
-import { ErrorList } from './../../src/themes/greyVest'
+import { Flex } from './../../src/layout/Flex'
+import { Box, ErrorList, Input } from './../../src/themes/greyVest'
 
 storiesOf('Components (Grey Vest)|Error', module)
   .addDecorator(decorator)
@@ -10,4 +11,19 @@ storiesOf('Components (Grey Vest)|Error', module)
     <ErrorList block>
       {['Error 1', 'Error 2', ['Error 3A', 'Error 3B']]}
     </ErrorList>
+  ))
+  .addWithJSX('Form Demo', () => (
+    <Box>
+      <h1 style={{ margin: '15px 0' }}>Header</h1>
+      <ErrorList block>Block error</ErrorList>
+      <Flex column style={{ marginBottom: 25 }}>
+        <Flex as="label" column style={{ flex: 1 }}>
+          <div className="filter-field-label" style={{ marginBottom: 14 }}>
+            Label
+          </div>
+          <Input style={{ borderColor: '#D75050' }} />
+        </Flex>
+        <ErrorList>Text error</ErrorList>
+      </Flex>
+    </Box>
   ))
