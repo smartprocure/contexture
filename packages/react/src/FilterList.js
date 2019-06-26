dca16ed50ecf8ed03dfecc8747471fad2d457aa9
Michelle Saad <git@michsa.me>
2019-06-26 15:18:06 -0400

    Move FilterOptions back into FilterList and rename to FilterActions
    https://github.com/smartprocure/contexture-react/pull/253

--- packages/react/src/FilterList.js
+++ packages/react/src/FilterList.js
@@ -7,7 +7,36 @@ import { withStateLens } from './utils/mobx-react-utils'
 import InjectTreeNode from './utils/injectTreeNode'
 import DefaultIcon from './DefaultIcon'
 import { bdJoin } from './styles/generic'
-import FilterOptions from './FilterOptions'
+import { newNodeFromType } from './utils/search'
+
+export let FilterActions = ({ node, tree, fields, Item }) => (
+  <>
+    <Item className="filter-options-selected-type">
+      {F.autoLabel(node.type)}
+    </Item>
+    {_.map(
+      x => (
+        <Item
+          key={x.value}
+          onClick={() =>
+            tree.replace(node.path, newNodeFromType(x.value, fields, node))
+          }
+        >
+          —Change to {x.label}
+        </Item>
+      ),
+      F.autoLabelOptions(
+        _.without([node.type], _.get([node.field, 'typeOptions'], fields)) || []
+      )
+    )}
+    <div className="filter-options-separator" />
+    {/* If only contexture-client diffed the tree before sending a request... */}
+    {(node.hasValue || false) && (
+      <Item onClick={() => tree.clear(node.path)}>Clear Filter</Item>
+    )}
+    <Item onClick={() => tree.remove(node.path)}>Delete Filter</Item>
+  </>
+)
 
 export let Label = inject(_.pick('tree'))(
   withStateLens({ popover: false })(
@@ -36,7 +65,7 @@ export let Label = inject(_.pick('tree'))(
             >
               <Icon icon="TableColumnMenu" />
               <Popover isOpen={popover} className="filter-options-popover">
-                <FilterOptions
+                <FilterActions
                   node={node}
                   tree={tree}
                   fields={fields}
