cac30cc5e965e9df8d120dbf38e8116d95c39a83
Decrapifier <decrapifier@govspend.us>
2019-07-26 11:52:06 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/272

--- packages/react/src/FilterButtonList.js
+++ packages/react/src/FilterButtonList.js
@@ -13,53 +13,55 @@ import DefaultMissingTypeComponent from './DefaultMissingTypeComponent'
 import { withNode, withLoader } from './utils/injectTreeNode'
 import styles from './styles'
 
-let FilterButtonItem = withLoader(({
-  node,
-  tree,
-  fields,
-  mapNodeToProps,
-  Button,
-  CheckButton,
-  MissingTypeComponent,
-  Modal,
-}) => {
-  let mappedProps = mapNodeToProps(node, fields)
-  let modal = F.stateLens(React.useState(false))
-  let title = // we really need a title, so here's every possible fallback
-    _.get('label', mappedProps) ||
-    _.get([node.field, 'label'], fields) ||
-    node.field ||
-    node.key
-  let description = _.get('description', mappedProps)
-  return (
-    <>
-      <CheckButton checked={node.hasValue} onClick={F.on(modal)}>
-        {title}
-      </CheckButton>
-      <Modal isOpen={modal}>
-        <div className="filter-button-modal">
-          <h1>{title}</h1>
-          {description && (
-            <div className="filter-description">{description}</div>
-          )}
-          <div className="filter-component">
-            <Dynamic
-              Component={MissingTypeComponent}
-              tree={tree}
-              node={node}
-              path={_.toArray(node.path)}
-              {...mappedProps}
-            />
+let FilterButtonItem = withLoader(
+  ({
+    node,
+    tree,
+    fields,
+    mapNodeToProps,
+    Button,
+    CheckButton,
+    MissingTypeComponent,
+    Modal,
+  }) => {
+    let mappedProps = mapNodeToProps(node, fields)
+    let modal = F.stateLens(React.useState(false))
+    let title = // we really need a title, so here's every possible fallback
+      _.get('label', mappedProps) ||
+      _.get([node.field, 'label'], fields) ||
+      node.field ||
+      node.key
+    let description = _.get('description', mappedProps)
+    return (
+      <>
+        <CheckButton checked={node.hasValue} onClick={F.on(modal)}>
+          {title}
+        </CheckButton>
+        <Modal isOpen={modal}>
+          <div className="filter-button-modal">
+            <h1>{title}</h1>
+            {description && (
+              <div className="filter-description">{description}</div>
+            )}
+            <div className="filter-component">
+              <Dynamic
+                Component={MissingTypeComponent}
+                tree={tree}
+                node={node}
+                path={_.toArray(node.path)}
+                {...mappedProps}
+              />
+            </div>
+            <Button onClick={() => tree.clear(node.path)}>Clear</Button>
+            <Button primary onClick={F.off(modal)}>
+              Done
+            </Button>
           </div>
-          <Button onClick={() => tree.clear(node.path)}>Clear</Button>
-          <Button primary onClick={F.off(modal)}>
-            Done
-          </Button>
-        </div>
-      </Modal>
-    </>
-  )
-})
+        </Modal>
+      </>
+    )
+  }
+)
 
 let GroupBox = ({ nodeJoinColor, children, nested, className }) => (
   <Flex
--- packages/react/src/utils/StripedLoader.js
+++ packages/react/src/utils/StripedLoader.js
@@ -3,9 +3,7 @@ import { observer } from 'mobx-react'
 import { loading as loadingStyle } from '../styles/generic'
 
 let StripedLoader = observer(({ loading, style = {}, children }) => (
-  <div style={{ ...style, ...(loading && loadingStyle) }}>
-    {children}
-  </div>
+  <div style={{ ...style, ...(loading && loadingStyle) }}>{children}</div>
 ))
 StripedLoader.displayName = 'StripedLoader'
 
