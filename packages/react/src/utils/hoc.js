6de0cc0b87da877926a44f60f9cd5456e0e84103
Michelle Saad <git@michsa.me>
2019-07-31 00:52:52 -0400

    Feedback
    https://github.com/smartprocure/contexture-react/pull/272

--- packages/react/src/utils/hoc.js
+++ packages/react/src/utils/hoc.js
@@ -3,18 +3,14 @@ import { observer } from 'mobx-react'
 import _ from 'lodash/fp'
 import StripedLoader from './StripedLoader'
 
-export let withNode = (Component, { allowEmptyNode = false } = {}) => ({
-  tree,
-  node,
-  path,
-  ...props
-}) => {
+export let withNode = Component => props => {
+  let { tree, node, path } = props
   node = node || (tree && path && tree.getNode(path))
 
-  if (!node && !allowEmptyNode)
+  if (!node)
     throw Error(`Node not provided, and couldn't find node at ${path}`)
 
-  return <Component {...{ tree, node, path, ...props }} />
+  return <Component {...props} node={node} />
 }
 
 export let withLoader = (Component, loaderProps) =>
@@ -24,19 +20,14 @@ export let withLoader = (Component, loaderProps) =>
     </Loader>
   ))
 
-export let contexturify = (Component, config, loaderProps) =>
+export let contexturify = (Component, loaderProps) =>
   _.flow(
     observer,
-    c => withNode(c, config),
+    withNode,
     c => withLoader(c, loaderProps)
   )(Component)
 
 // this is used for the text components
-export let withTreeLens = Component => ({
-  tree,
-  node,
-  prop = 'value',
-  ...props
-}) => (
-  <Component {...{ tree, node, lens: tree.lens(node.path, prop), ...props }} />
+export let withTreeLens = Component => ({ prop = 'value', ...props }) => (
+  <Component {...{ lens: props.tree.lens(props.node.path, prop), ...props }} />
 )
