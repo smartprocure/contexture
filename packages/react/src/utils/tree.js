7c7e2e04e334caa318920b772c94a034311be63d
Michelle Saad <git@michsa.me>
2019-08-08 01:05:04 -0400

    Delete tree util
    https://github.com/smartprocure/contexture-react/pull/283

--- packages/react/src/utils/tree.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash/fp'
-import * as F from 'futil-js'
-
-export let traverse = x => x && x.children && _.toArray(x.children) // mobx needs slice
-export let keyPath = key => (_.isString(key) ? { key } : key)
-export default F.tree(traverse, keyPath)
