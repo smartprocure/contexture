f5f58f51a021b735f29b29218aa86920f0b16402
Michelle Saad <git@michsa.me>
2019-08-22 01:45:09 -0400

    Kill ModalFilterAdder
    https://github.com/smartprocure/contexture-react/pull/299

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -3,10 +3,10 @@ import QueryBuilder from './queryBuilder/'
 export * from './FilterList'
 export * from './layout'
 export * from './utils/schema'
+export * from './utils/react'
 import FilterAdder from './FilterAdder'
-import ModalFilterAdder from './ModalFilterAdder'
 import FilterButtonList from './FilterButtonList'
 export { default as SearchFilters } from './SearchFilters'
 export { default as SearchLayout } from './SearchLayout'
 
-export { QueryBuilder, FilterAdder, ModalFilterAdder, FilterButtonList }
+export { QueryBuilder, FilterAdder, FilterButtonList }
