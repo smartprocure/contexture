0c9d891372f01bef4755d228f9a5306fc8a625c1
Michelle Saad <git@michsa.me>
2019-08-29 23:58:47 -0400

    feedback: remove withTheme from TagsInput
    https://github.com/smartprocure/contexture-react/pull/299

--- packages/react/src/purgatory/PopoverTagsInput.js
+++ packages/react/src/purgatory/PopoverTagsInput.js
@@ -1,44 +1,40 @@
 import React from 'react'
 import _ from 'lodash/fp'
 import F from 'futil-js'
-import { withState } from 'recompose'
-import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 import { withTheme } from '../utils/theme'
+import { useLens } from '../utils/react'
 
 // We're only using withState to preserve the state between renders, since
 // inject doesn't do that for us.
 let PopoverTagsInput = ({
   PopoverContents,
   onTagClick,
-  state,
   theme: { Popover, TagsInput },
   ...props
-}) => (
-  <>
-    <TagsInput
-      onTagClick={tag => {
-        state.popoverOpen = true
-        state.selectedTag = tag
-        onTagClick(tag)
-      }}
-      {...props}
-    />
-    {PopoverContents && (
-      <Popover isOpen={F.lensProp('popoverOpen', state)}>
-        <PopoverContents tag={state.selectedTag} />
-      </Popover>
-    )}
-  </>
-)
+}) => {
+  let isOpen = useLens(false)
+  let [selectedTag, setSelectedTag] = React.useState(null)
+  return (
+    <>
+      <TagsInput
+        onTagClick={tag => {
+          F.on(isOpen)()
+          setSelectedTag(tag)
+          onTagClick(tag)
+        }}
+        {...props}
+      />
+      {PopoverContents && (
+        <Popover isOpen={isOpen}>
+          <PopoverContents tag={selectedTag} />
+        </Popover>
+      )}
+    </>
+  )
+}
 
 export default _.flow(
   observer,
-  withTheme,
-  withState('state', 'setState', () =>
-    observable({
-      selectedTag: null,
-      popoverOpen: false,
-    })
-  )
+  withTheme
 )(PopoverTagsInput)
