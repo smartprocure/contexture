9844e5bfcd0217bf0f3808ab8bb6bcbaa0fe77a7
Michelle Saad <git@michsa.me>
2019-09-03 20:23:09 -0400

    add theme switcher!
    https://github.com/smartprocure/contexture-react/pull/299

--- packages/react/stories/greyVest/decorator.js
+++ packages/react/stories/greyVest/decorator.js
@@ -1,9 +1,50 @@
 import React from 'react'
-import theme from '../../src/themes/greyVest'
-import { ThemeProvider } from '../../src/utils/theme'
+import F from 'futil-js'
+import greyVest from '../../src/themes/greyVest'
+import blueberry from '../../src/themes/blueberry'
+import base from '../../src/themes/base'
+import { ThemeProvider, ThemeConsumer } from '../../src/utils/theme'
+import { Flex } from '../../src/greyVest'
+import { useLens } from '../../src/utils/react'
+
+let options = [
+  { label: 'Unthemed', value: 'base' },
+  { label: 'Grey Vest', value: 'greyVest' },
+  { label: 'Blueberry', value: 'blueberry' },
+]
+let themes = { greyVest, blueberry, base }
+
+let ThemeSwitcher = ({ children }) => {
+  let theme = useLens('greyVest')
+  return (
+    <ThemeProvider theme={themes[F.view(theme)]}>
+      <ThemeConsumer>
+        {({ Box, Select }) => (
+          <Box style={{ marginBottom: 20 }}>
+            <Flex alignItems="center">
+              <p
+                className="filter-field-label"
+                style={{ margin: 0, marginRight: 8 }}
+              >
+                Current theme:{' '}
+              </p>
+              <Select
+                options={options}
+                {...F.domLens.value(theme)}
+                placeholder={false}
+                style={{ width: 'auto', minWidth: 200 }}
+              />
+            </Flex>
+          </Box>
+        )}
+      </ThemeConsumer>
+      {children}
+    </ThemeProvider>
+  )
+}
 
 export default Story => (
-  <ThemeProvider theme={theme}>
+  <ThemeSwitcher>
     <Story />
-  </ThemeProvider>
+  </ThemeSwitcher>
 )
