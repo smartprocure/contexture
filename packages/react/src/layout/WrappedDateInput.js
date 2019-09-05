57a41a3330674b20aac9795ef184e99fce02fcac
Alejandro Hernandez <ahernandez@govspend.com>
2019-09-05 13:12:28 -0400

    WrappedDateInput: forward props
    https://github.com/smartprocure/contexture-react/pull/311

--- packages/react/src/layout/WrappedDateInput.js
+++ packages/react/src/layout/WrappedDateInput.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import moment from 'moment'
 
-let WrappedDateInput = ({ value, onChange }) => (
+let WrappedDateInput = ({ value, onChange, ...props }) => (
   <input
     type="date"
     value={value ? moment(value).format('YYYY-MM-DD') : ''}
     onChange={e => onChange(new Date(e.target.value))}
+    {...props}
   />
 )
 
