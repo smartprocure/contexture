830a3e0443ef464ad350ad6af4e71dcfa8c29dde
Decrapifier <decrapifier@govspend.us>
2019-09-10 21:01:31 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/323

--- packages/react/src/queryBuilder/OperatorMenu.stories.js
+++ packages/react/src/queryBuilder/OperatorMenu.stories.js
@@ -3,9 +3,9 @@ import { storiesOf } from '@storybook/react'
 import { parent, root } from './stories/util'
 import OperatorMenu from './OperatorMenu'
 
-  storiesOf(
-    'Components|Search Components/QueryBuilder/Internals',
-    module
-  ).addWithJSX('OperatorMenu', () => (
-    <OperatorMenu {...{ node: { join: 'and' }, parent, root }} />
-  ))
+storiesOf(
+  'Components|Search Components/QueryBuilder/Internals',
+  module
+).addWithJSX('OperatorMenu', () => (
+  <OperatorMenu {...{ node: { join: 'and' }, parent, root }} />
+))
--- packages/react/src/queryBuilder/preview/AddPreview.stories.js
+++ packages/react/src/queryBuilder/preview/AddPreview.stories.js
@@ -3,10 +3,10 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import AddPreview from './AddPreview'
 
-  storiesOf(
-    'Components|Search Components/QueryBuilder/Internals/AddPreview',
-    module
-  )
-    .addWithJSX('and', () => <AddPreview onClick={action('join')} join="and" />)
-    .addWithJSX('or', () => <AddPreview onClick={action('join')} join="or" />)
-    .addWithJSX('not', () => <AddPreview onClick={action('join')} join="not" />)
+storiesOf(
+  'Components|Search Components/QueryBuilder/Internals/AddPreview',
+  module
+)
+  .addWithJSX('and', () => <AddPreview onClick={action('join')} join="and" />)
+  .addWithJSX('or', () => <AddPreview onClick={action('join')} join="or" />)
+  .addWithJSX('not', () => <AddPreview onClick={action('join')} join="not" />)
