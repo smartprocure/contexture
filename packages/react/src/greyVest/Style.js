324faa3dd5c5515806535c82a626390813657598
Michelle Saad <git@michsa.me>
2019-09-12 14:42:54 -0400

    add min and max height/width styles to gv-checkbox (should fix it randomly shrinking)
    https://github.com/smartprocure/contexture-react/pull/325

--- packages/react/src/greyVest/Style.js
+++ packages/react/src/greyVest/Style.js
@@ -139,6 +139,10 @@ export default () => (
       .gv-checkbox {
         height: 20px;
         width: 20px;
+        min-height: 20px;
+        min-width: 20px;
+        max-height: 20px;
+        max-width: 20px;
         border-radius: 3px;
         display: flex;
         justify-content: center;
