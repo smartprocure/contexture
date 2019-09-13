7a57e91edadc9e354e733090b0195b62ecb67418
Michelle Saad <git@michsa.me>
2019-09-13 06:13:42 -0400

    move greyVest Tabs docs to a real markdown file
    https://github.com/smartprocure/contexture-react/pull/325

--- packages/react/src/greyVest/Tabs.stories.js
+++ packages/react/src/greyVest/Tabs.stories.js
@@ -6,59 +6,7 @@ import { observable } from 'mobx'
 import { Observer } from 'mobx-react'
 import { Tabs, Tab, TabLabel, TabContent, ButtonRadio, Button } from '.'
 import decorator from './stories/decorator'
-
-let tabDocs = `
-# Overview
-
-A tab control which switches out content dynamically. \`Tabs\` is used with one or more \`Tab\` components (or \`TabLabel\`/\`TabContent\` pairs) as children.
-
-~~~js
-<Tabs>
-  <Tab label="Tab One">
-    Tab One Contents
-  </Tab>
-  <Tab label="Tab Two">
-    Tab Two Contents
-  </Tab>
-</Tabs>
-~~~
-
-# Component API
-
-## \`Tabs\`
-
-| Prop Name | Type | Description |
-| --------- | ---- | ----------- |
-| \`value\` | string/number | Sets tab value. Makes it controlled. |
-| \`defaultValue\` | string/number | Default value if uncontrolled. |
-| \`TabsList\` | Component | Component to use to render the list of tab options |
-| \`TabPanel\` | Component | Component to use for the tab panel which wraps active tab content |
-| \`onChange\` | (to, from) => {} | Function to handle tab change. Takes (to, from) and works on both controlled and uncontrolled Tab |
-
-## \`Tab\`
-
-| Prop Name | Type | Description |
-| --------- | ---- | ----------- |
-| \`value\` | string/number | Tab value, used to represent the underlying value. Defaults to the index in the list of children |
-| \`label\` | Fragment | Label used for the tab option |
-| \`children\` | Fragment | Tab contents shown if the tab is active |
-
-## \`TabLabel\`
-Used in conjunction with TabContent as an alternative to a combined Tab.
-
-| Prop Name | Type | Description |
-| --------- | ---- | ----------- |
-| \`value\` | string/number | Tab value, used to represent the underlying value. Defaults to the index in the list of children |
-| \`children\` | Fragment | Label used for the tab option |
-
-## \`TabContent\`
-Used in conjunction with TabLabel as an alternative to a combined Tab.
-
-| Prop Name | Type | Description |
-| --------- | ---- | ----------- |
-| \`value\` | string/number | Tab value, used to represent the underlying value. Defaults to the index in the list of children |
-| \`children\` | Fragment | Tab contents shown if the tab is active |
-`
+import tabDocs from './docs/tabs.md'
 
 let state = observable({ tab: 'results' })
 
