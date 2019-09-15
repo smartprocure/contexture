c289ca1562b944c9ba0695527d14b29c4d3f03c9
Michelle Saad <git@michsa.me>
2019-09-15 17:04:49 -0400

    kill ContextureProvider
    https://github.com/smartprocure/contexture-react/pull/327

--- packages/react/src/ContextureProvider.js
+++
@@ -1,9 +0,0 @@
-import ContextureMobx from './utils/contexture-mobx'
-
-export default ({ types, service, nodeKey, ...args }) =>
-  ContextureMobx({ types, service })({
-    key: nodeKey || 'root',
-    type: 'group',
-    children: [],
-    ...args,
-  })
