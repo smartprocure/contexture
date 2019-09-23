b0dd39b96ab0d7ee5ffc8c6d82ee1430854e9864
Decrapifier <decrapifier@govspend.us>
2019-09-23 22:19:57 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/52

--- packages/mongo/src/example-types/results.js
+++ packages/mongo/src/example-types/results.js
@@ -117,7 +117,7 @@ let result = async (context, search, schema, { getSchema }) => {
       startRecord: startRecord + 1,
       endRecord: startRecord + results.length,
       results,
-    }
+    },
   }
 }
 
