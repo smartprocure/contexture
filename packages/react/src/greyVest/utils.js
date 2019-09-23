a36d3e50a8950cf38ab1324416d03b7a5a442a1a
Michelle Saad <git@michsa.me>
2019-09-23 16:05:09 -0400

    make expandProp ignore undefined props
    https://github.com/smartprocure/contexture-react/pull/331

--- packages/react/src/greyVest/utils.js
+++ packages/react/src/greyVest/utils.js
@@ -1,7 +1,6 @@
 import F from 'futil-js'
 
-export let openBinding = lens =>
-  lens && {
-    isOpen: F.view(lens),
-    onClose: F.off(lens),
-  }
+export let openBinding = (...lens) => ({
+  isOpen: F.view(...lens),
+  onClose: F.off(...lens),
+})
