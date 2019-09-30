4ef982e782f10be31ed635082697309c9ffd74a0
Michelle Saad <git@michsa.me>
2019-09-30 16:59:28 -0400

    add FacetSelect component
    https://github.com/smartprocure/contexture-react/pull/335

---
+++ packages/react/src/exampleTypes/FacetSelect.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import F from 'futil-js'
+import _ from 'lodash/fp'
+import { contexturify } from '../utils/hoc'
+import Async from 'react-select/lib/Async'
+
+let getOptions = node =>
+  _.map(({ name }) => F.autoLabelOption(name), node.context.options)
+
+let FacetSelect = ({ tree, node }) => (
+  <Async
+    isMulti
+    cacheOptions
+    defaultOptions={getOptions(node)}
+    loadOptions={async val => {
+      await tree.mutate(node.path, { optionsFilter: val })
+      return getOptions(node)
+    }}
+    onChange={x => tree.mutate(node.path, { values: _.map(_.get('value'), x) })}
+  />
+)
+
+export default contexturify(FacetSelect)
