1c3639dfd721b1cbc78ea70ca77984efac4d1e0d
Michelle Saad <git@michsa.me>
2019-10-11 07:39:41 -0400

    FacetSelect: show count, add display function props
    https://github.com/smartprocure/contexture-react/pull/335

--- packages/react/src/exampleTypes/FacetSelect.js
+++ packages/react/src/exampleTypes/FacetSelect.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import F from 'futil-js'
 import _ from 'lodash/fp'
+import { Flex } from '../greyVest'
 import { contexturify } from '../utils/hoc'
 import Async from 'react-select/lib/Async'
 
 let getOptions = node =>
-  _.map(({ name }) => F.autoLabelOption(name), node.context.options)
+  _.map(
+    ({ name, count }) => ({ ...F.autoLabelOption(name), count }),
+    _.get('context.options', node)
+  )
 
-let FacetSelect = ({ tree, node, isMulti = true }) => (
+let FacetSelect = ({
+  tree,
+  node,
+  isMulti = true,
+  display = x => x,
+  formatCount = x => x,
+  displayBlank = () => <i>Not Specified</i>,
+}) => (
   <Async
+    placeholder="Search..."
     isMulti={isMulti}
     cacheOptions
     defaultOptions={getOptions(node)}
@@ -16,6 +28,18 @@ let FacetSelect = ({ tree, node, isMulti = true }) => (
       await tree.mutate(node.path, { optionsFilter: val })
       return getOptions(node)
     }}
+    formatOptionLabel={({ label, count }, { context }) =>
+      context === 'menu' ? (
+        <Flex justifyContent="space-between">
+          {display(label) || displayBlank()}
+          <span>{formatCount(count)}</span>
+        </Flex>
+      ) : (
+        <span>
+          {display(label) || displayBlank()} ({formatCount(count)})
+        </span>
+      )
+    }
     onChange={x => tree.mutate(node.path, { values: _.map('value', x) })}
   />
 )
