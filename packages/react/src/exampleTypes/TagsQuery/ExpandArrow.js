e95fbaeda07bae7a35f0cd46a9439eae4a42ff1c
Michelle Saad <git@michsa.me>
2019-10-28 14:37:21 -0400

    move ExpandArrow to its own file, add icon
    https://github.com/smartprocure/contexture-react/pull/346

---
+++ packages/react/src/exampleTypes/TagsQuery/ExpandArrow.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import F from 'futil-js'
+import _ from 'lodash/fp'
+import { observer } from 'mobx-react'
+import { Flex } from '../../greyVest'
+import { withTheme } from '../../utils/theme'
+
+let ExpandArrow = ({ collapse, tagsLength, style, theme: { Icon } }) =>
+  !!(F.view(collapse) && tagsLength) && (
+    <div className="expand-arrow" onClick={F.off(collapse)} style={style}>
+      <div
+        style={{
+          height: 0,
+          cursor: 'pointer',
+          textAlign: 'center',
+        }}
+        title="Expand to see all keywords"
+      >
+        <Flex
+          style={{
+            display: 'inline-flex',
+            backgroundColor: 'white',
+            borderRadius: 4,
+            padding: '5px 12px',
+            boxShadow: '0 1px 4px 0 rgba(39, 44, 65, 0.1)',
+            color: '#9b9b9b',
+            fontWeight: 400,
+          }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          View all {tagsLength} tags
+          <Icon icon="Expand" style={{ fontSize: 16, marginLeft: 6 }} />
+        </Flex>
+      </div>
+    </div>
+  )
+
+export default _.flow(
+  observer,
+  withTheme
+)(ExpandArrow)
--- packages/react/src/exampleTypes/TagsQuery/index.js
+++ packages/react/src/exampleTypes/TagsQuery/index.js
@@ -1,56 +1,15 @@
 import React from 'react'
 import _ from 'lodash/fp'
 import F from 'futil-js'
-import { setDisplayName } from 'recompose'
-import { observer } from 'mobx-react'
 import { Flex } from '../../greyVest'
 import { contexturify } from '../../utils/hoc'
 import { useLens } from '../../utils/react'
 import { getTagStyle } from './utils'
 import TagActionsMenu from './TagActionsMenu'
 import ActionsMenu from './ActionsMenu'
+import ExpandArrow from './ExpandArrow'
 
 const field = 'word'
-
-let ExpandArrow = _.flow(
-  setDisplayName('ExpandArrow'),
-  observer
-)(
-  ({ collapse, tagsLength, style }) =>
-    !!(F.view(collapse) && tagsLength) && (
-      <div
-        className="down-arrow-shape-container"
-        onClick={F.off(collapse)}
-        style={style}
-      >
-        <div
-          style={{
-            height: 0,
-            cursor: 'pointer',
-            textAlign: 'center',
-            transform: 'translateY(-50%)',
-          }}
-          title="Expand to see all keywords"
-        >
-          <Flex
-            style={{
-              display: 'inline-flex',
-              backgroundColor: 'white',
-              borderRadius: 4,
-              padding: '5px 12px',
-              boxShadow: '0 1px 4px 0 rgba(39, 44, 65, 0.1)',
-              color: '#9b9b9b',
-              fontWeight: 400,
-            }}
-            alignItems="center"
-            justifyContent="center"
-          >
-            View all {tagsLength} tags
-          </Flex>
-        </div>
-      </div>
-    )
-)
 
 let TagsQuery = ({
   tree,
