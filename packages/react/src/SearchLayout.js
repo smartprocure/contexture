40db28850550839742de6d6971c9d073d7ef1d87
Michelle Saad <git@michsa.me>
2019-10-31 17:01:11 -0400

    set SearchLayout gutter & margins to 24px
    https://github.com/smartprocure/contexture-react/pull/352

--- packages/react/src/SearchLayout.js
+++ packages/react/src/SearchLayout.js
@@ -2,11 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 
+let margin = 24
+
 let styles = mode => ({
   display: 'grid',
-  gridGap: 40,
-  margin: '0 40px',
-  marginBottom: 50,
+  gridGap: margin,
+  margin: `0 ${margin}px`,
+  marginBottom: margin,
   gridTemplateColumns:
     mode === 'basic' ? 'minmax(250px, 400px) minmax(0, 1fr)' : 'minmax(0, 1fr)',
 })
