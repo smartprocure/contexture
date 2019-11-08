1218dc08066a96350370430b6fc722fa8ecf3ee3
Samuel Greene <sam@samuelgreene.info>
2019-11-07 23:19:32 -0500

    add commment https://github.com/smartprocure/contexture-mongo/pull/56

--- packages/mongo/src/example-types/text.js
+++ packages/mongo/src/example-types/text.js
@@ -10,6 +10,8 @@ let joinmap = {
 // Convert to an array, strip empty strings, and determine if there are any values
 let hasValue = _.flow(
   // NOTE: Don't change the below, otherwise things will explode!
+  // Cascade can take a third arg which causes this to behave weird (since hasValue is actually called with extra args like the schema, etc)
+  // See https://github.com/smartprocure/futil-js/issues/218
   x => F.cascade(['value', 'values'], x),
   _.defaultTo([]),
   _.castArray,
