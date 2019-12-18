f0172ffdfdf65fb5d4ddfabcb8ce08fef27a7b81
Ilian Alexiev <ialexiev@smartprocure.us>
2019-12-17 19:32:10 -0800

    More changes to highlighting results
    https://github.com/smartprocure/contexture-elasticsearch/pull/132

--- packages/elasticsearch/src/example-types/results.js
+++ packages/elasticsearch/src/example-types/results.js
@@ -13,7 +13,14 @@ module.exports = {
         getField(schema, context.sortField, context.sortMode || 'word')
       : '_score'
     let sortDir = context.sortDir || 'desc'
-    let result = {
+    // The default schema highlighting settings w/o the fields
+    let defaultHighlightingConfig = {
+      pre_tags: ['<b class="search-highlight">'],
+      post_tags: ['</b>'],
+      require_field_match: false,
+      number_of_fragments: 0,
+    }
+    let searchObj = {
       from: startRecord,
       size: pageSize,
       sort: {
@@ -24,43 +31,58 @@ module.exports = {
 
     if (context.forceExclude && _.isArray(schema.forceExclude))
       context.exclude = _.union(schema.forceExclude, context.exclude)
+    if (context.include || context.exclude) searchObj._source = {}
+    if (context.include) searchObj._source.includes = context.include
+    if (context.exclude) searchObj._source.excludes = context.exclude
 
-    if (context.include || context.exclude) result._source = {}
-    if (context.include) result._source.includes = context.include
-    if (context.exclude) result._source.excludes = context.exclude
+    // Global schema highlight configuration
+    let schemaHighlight = _.getOr(true, 'highlight', context) && _.cloneDeep(schema.elasticsearch.highlight)
+    // Specific search highlight override
+    let searchHighlight = _.isPlainObject(context.highlight) ? context.highlight : {}
+    let resultColumns = context.include
 
-    let highlight =
-      _.getOr(true, 'highlight', context) && schema.elasticsearch.highlight
-    let inlineAliases = _.getOr({}, 'inlineAliases', highlight)
-    let showOtherMatches = _.getOr(false, 'showOtherMatches', context)
+    // Highlighting starts with defaults in the schema first
+    if (schemaHighlight) {
+      let showOtherMatches = _.getOr(false, 'showOtherMatches', context)
+      let schemaInline = _.getOr([], 'inline', schemaHighlight)
+      let schemaInlineAliases = _.getOr({}, 'inlineAliases', schemaHighlight)
 
-    if (highlight) {
+      // Concat the search specific override fields with the schema `inline` so we have them as targets for highlight replacement
+      F.setOn('inline', _.concat(schemaInline, _.keys(searchHighlight.fields)), schemaHighlight)
       // Convert the highlight fields from array to an object map
       let fields = _.flow(
         _.pick(['inline', 'additionalFields']), // Get the highlight fields we will be working with
         _.values,
         _.flatten,
-        _.concat(_.values(inlineAliases)), // Include the provided field aliases if any
-        _.uniq(),
+        _.concat(_.values(schemaInlineAliases)), // Include the provided field aliases if any
+        _.uniq,
         arrayToHighlightsFieldMap, // Convert the array to object map so we can simply _.pick again
         filtered =>
-          showOtherMatches
+        showOtherMatches
             ? filtered // Highlight on all fields specified in the initial _.pick above
-            : _.pick(context.include, filtered) // Only highlight on the fields listed in the context include section
-      )(highlight)
+            : _.pick(context.include, filtered), // Only highlight on the fields listed in the context include section
+      )(schemaHighlight)
 
-      F.extendOn(result, {
-        highlight: {
-          pre_tags: ['<b class="search-highlight">'],
-          post_tags: ['</b>'],
-          require_field_match: false,
-          number_of_fragments: 0,
-          fields,
-        },
-      })
+      // Setup the DEFAULT highlight config object with the calculated fields above
+      // and merge with the search specific config
+      F.extendOn(searchObj,
+        _.merge({
+          highlight: {...defaultHighlightingConfig, fields } }, {
+          highlight: searchHighlight })
+      )
+
+      // Make sure the search specific overrides are part of the context include.
+      // This way they do not have to be added manually. All that is needed is the highlight config
+      resultColumns = _.flow(
+        _.concat(_.keys(searchHighlight.fields)),
+        _.uniq
+      )(context.include)
+
+      // Make sure search returns the resultColumns we want by setting the _.source.includes
+      F.setOn('_source.includes', resultColumns, searchObj)
     }
 
-    return search(result).then(results => ({
+    return search(searchObj).then(results => ({
       scrollId: results._scroll_id,
       response: {
         totalRecords: results.hits.total,
@@ -69,27 +91,27 @@ module.exports = {
         results: _.map(hit => {
           let highlightObject
           let additionalFields
-          if (highlight) {
+          if (schemaHighlight) {
             highlightObject = highlightResults(
-              schema.elasticsearch.highlight,
-              hit,
+              schemaHighlight,  // The highlight configuration
+              hit,              // The ES result
               schema.elasticsearch.nestedPath,
-              context.include
+              resultColumns     // The columns to return
             )
             additionalFields = highlightObject.additionalFields
           }
 
-          // TODO - If nested path, iterate properties on nested path, filtering out nested path results unless mainHighlighted or relevant nested fields have b tags in them
+          // TODO - If nested path, iterate properties on nested path, filtering out nested path results unless mainHighlighted or relevant nested fields have "<b></b>" tags in them
           return _.extendAll([
             context.verbose || _.size(context.include) > 0 ? { hit } : {},
             {
               _id: hit._id,
-              additionalFields: highlight ? additionalFields : [],
+              additionalFields: schemaHighlight ? additionalFields : [],
             },
             _.getOr(_.identity, 'elasticsearch.summaryView', schema)(hit),
           ])
         }, results.hits.hits),
       },
     }))
-  },
+  }
 }
