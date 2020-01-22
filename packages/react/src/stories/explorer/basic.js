431cc4eadc0bd23c43d53553812786e9fa2b0255
Caden Albaugh <cadenalbaugh@gmail.com>
2020-01-22 16:24:31 -0600

    fix: missed one
    https://github.com/smartprocure/contexture-react/pull/376

--- packages/react/src/stories/explorer/basic.js
+++ packages/react/src/stories/explorer/basic.js
@@ -167,6 +167,7 @@ let Story = observer(() => {
                         tree={tree}
                         path={['root', 'criteria']}
                         fields={schemas[tree.tree.schema].fields}
+                        uniqueFields
                       />
                     </div>
                     <div style={{ flex: 4, maxWidth: '80%', ...whiteBox }}>
