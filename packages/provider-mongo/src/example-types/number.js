48087eb22342650994ccd18ee84559b7a65f9a48
Decrapifier <decrapifier@govspend.us>
2020-02-06 03:28:55 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/59

--- packages/provider-mongo/src/example-types/number.js
+++ packages/provider-mongo/src/example-types/number.js
@@ -3,16 +3,10 @@ let _ = require('lodash/fp')
 let predicate = x =>
   (_.isString(x) || _.isNumber(x)) && x !== '' && _.isFinite(_.toNumber(x))
 
-let cleanFilter = _.flow(
-  _.pickBy(predicate),
-  _.mapValues(_.toNumber)
-)
+let cleanFilter = _.flow(_.pickBy(predicate), _.mapValues(_.toNumber))
 
 module.exports = {
-  hasValue: _.flow(
-    _.pick(['min', 'max']),
-    _.some(predicate)
-  ),
+  hasValue: _.flow(_.pick(['min', 'max']), _.some(predicate)),
   filter: ({ field, min, max }) => ({
     [field]: cleanFilter({
       $gte: min,
