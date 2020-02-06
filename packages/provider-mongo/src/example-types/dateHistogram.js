b3d570550118098b18c7c185307090957c1ae8e5
Samuel Greene <sam@samuelgreene.info>
2020-02-06 03:02:32 -0500

    first pass at dateHistogram
    https://github.com/smartprocure/contexture-mongo/pull/59

---
+++ packages/provider-mongo/src/example-types/dateHistogram.js
@@ -0,0 +1,64 @@
+let _ = require('lodash/fp')
+let F = require('futil')
+let { statsAgg } = require('./statistical')
+
+let keysToObject = F.arrayToObject(x => x)
+
+module.exports = {
+  result: async (
+    {
+      key_field,
+      value_field,
+      interval,
+      include = ['min', 'max', 'avg', 'sum'],
+      timezone,
+    },
+    search
+  ) => {
+    let stats = {
+      ..._.omit(['_id'], statsAgg(value_field).$group),
+      cardinality: { $addToSet: `$${value_field}` },
+    }
+    let timeAgg = timezone
+      ? { date: `$${key_field}`, timezone }
+      : `$${key_field}`
+    return {
+      entries: _.map(
+        x => ({
+          ...x,
+          key:
+            new Date(`${x.year}-${x.month || 1}-${x.day || 1}`).getTime() /
+            1000,
+        }),
+        await search([
+          {
+            $group: {
+              _id: {
+                ...(interval === 'day' && { day: { $dayOfMonth: timeAgg } }),
+                ...(_.includes(interval, ['day', 'month']) && {
+                  month: { $month: timeAgg },
+                }),
+                year: { $year: timeAgg },
+              },
+              ..._.pick(include, stats),
+            },
+          },
+          // Mongo ignore missing fields during project :)
+          {
+            $project: {
+              day: '$_id.day',
+              month: '$_id.month',
+              year: '$_id.year',
+              _id: 0,
+              ...keysToObject(
+                x => (x === 'cardinality' ? { $size: '$cardinality' } : 1),
+                include
+              ),
+            },
+          },
+          { $sort: { year: 1, month: 1, day: 1 } },
+        ])
+      ),
+    }
+  },
+}
---
+++ packages/provider-mongo/test/example-types/dateHistogram.js
@@ -0,0 +1,134 @@
+let _ = require('lodash/fp')
+let { expect } = require('chai')
+let mingo = require('mingo')
+let dateHistogram = require('../../src/example-types/dateHistogram')
+
+let aggregate = sampleData => aggs => new mingo.Aggregator(aggs).run(sampleData)
+
+let sampleData = _.times(
+  i => ({
+    createdAt: new Date(`2020-02-0${(i % 5) + 1}`),//T1${i % 10}:00:00.000Z`),
+    metrics: { usersCount: i * 100 },
+  }),
+  50
+)
+let simulateAggregation = aggregate(sampleData)
+
+describe('dateHistogram', () => {
+  describe('dateHistogram.result', () => {
+    it('result should output the expected query and results', async () => {
+      let search = _.flow(
+        _.tap(x => (query = x)),
+        simulateAggregation
+      )
+
+      let result = await dateHistogram.result(
+        {
+          key: 'test',
+          type: 'dateHistogram',
+          key_field: 'createdAt',
+          value_field: 'metrics.usersCount',
+          include: ['min', 'max', 'avg', 'sum', 'count', 'cardinality'],
+          interval: 'day',
+        },
+        search
+      )
+      expect(query).eql([
+        {
+          $group: {
+            _id: {
+              day: { $dayOfMonth: '$createdAt' },
+              month: { $month: '$createdAt' },
+              year: { $year: '$createdAt' },
+            },
+            avg: { $avg: '$metrics.usersCount' },
+            max: { $max: '$metrics.usersCount' },
+            min: { $min: '$metrics.usersCount' },
+            sum: { $sum: '$metrics.usersCount' },
+            count: { $sum: 1 },
+            cardinality: { $addToSet: '$metrics.usersCount' },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            day: '$_id.day',
+            month: '$_id.month',
+            year: '$_id.year',
+            avg: 1,
+            max: 1,
+            min: 1,
+            sum: 1,
+            count: 1,
+            cardinality: { $size: '$cardinality' },
+          },
+        },
+        { $sort: { year: 1, month: 1, day: 1 } },
+      ])
+      expect(result).eql({
+        entries: [
+          {
+            key: 1580446800,
+            year: 2020,
+            month: 1,
+            day: 31,
+            count: 10,
+            max: 4500,
+            min: 0,
+            avg: 2250,
+            sum: 22500,
+            cardinality: 10,
+          },
+          {
+            key: 1580533200,
+            year: 2020,
+            month: 2,
+            day: 1,
+            count: 10,
+            max: 4600,
+            min: 100,
+            avg: 2350,
+            sum: 23500,
+            cardinality: 10,
+          },
+          {
+            key: 1580619600,
+            year: 2020,
+            month: 2,
+            day: 2,
+            count: 10,
+            max: 4700,
+            min: 200,
+            avg: 2450,
+            sum: 24500,
+            cardinality: 10,
+          },
+          {
+            key: 1580706000,
+            year: 2020,
+            month: 2,
+            day: 3,
+            count: 10,
+            max: 4800,
+            min: 300,
+            avg: 2550,
+            sum: 25500,
+            cardinality: 10,
+          },
+          {
+            key: 1580792400,
+            year: 2020,
+            month: 2,
+            day: 4,
+            count: 10,
+            max: 4900,
+            min: 400,
+            avg: 2650,
+            sum: 26500,
+            cardinality: 10,
+          },
+        ],
+      })
+    })
+  })
+})
