8370cc1648b586d014d1110a9a5b23b91f78541e
Michelle Saad <git@michsa.me>
2020-02-19 22:30:21 -0500

    put ResultPager back the way it was
    https://github.com/smartprocure/contexture-react/pull/384

--- packages/react/src/exampleTypes/ResultPager.js
+++ packages/react/src/exampleTypes/ResultPager.js
@@ -1,22 +1,21 @@
 import F from 'futil'
-import _ from 'lodash/fp'
 import React from 'react'
 import { contexturifyWithoutLoader } from '../utils/hoc'
-import { Pager, Flex } from '../greyVest'
+import { Pager } from '../greyVest'
 
-let ResultPager = ({ node, tree, theme: { PagerItem, Icon }, ...props }) => {
-  let pages = Math.ceil((totalRecords.current || 1) / (node.pageSize || 1))
+let ResultPager = ({ node, tree, theme: { PagerItem, Icon } }) => {
+  let pages = Math.ceil(
+    F.cascade(['response.totalRecords', 'totalRecords'], node.context, 1) /
+      node.pageSize
+  )
   let page = node.page || 1
   return (
-    <Flex alignItems="center" {...props}>
-      <Pager
-        value={page}
-        pageCount={pages}
-        onChange={page => tree.mutate(node.path, { page })}
-        {...{ PagerItem, Icon }}
-      />
-      
-    </Flex>
+    <Pager
+      value={page}
+      pageCount={pages}
+      onChange={page => tree.mutate(node.path, { page })}
+      {...{ PagerItem, Icon }}
+    />
   )
 }
 
