bdf6f0f5b5003d5e00a836e73e60b3ecf0c23919
Michelle Saad <git@michsa.me>
2020-02-20 23:06:32 -0500

    kill totalRecords state
    https://github.com/smartprocure/contexture-react/pull/384

--- packages/react/src/exampleTypes/ResultTable/ResultTableFooter.js
+++ packages/react/src/exampleTypes/ResultTable/ResultTableFooter.js
@@ -7,15 +7,12 @@ import { contexturifyWithoutLoader } from '../../utils/hoc'
 let ResultTableFooter = ({ tree, node, pageSizeOptions }) => {
   let getFromContext = key =>
     F.cascade([`context.response.${key}`, `context.${key}`], node)
-  // We keep track of a running totalRecords in case the results node doesn't
-  // contain a count (eg. for very expensive queries using contexture-mongo,
-  // where the count may be disabled for performance).
-  let totalRecords = React.useRef(0)
-  totalRecords.current = _.max([
-    totalRecords.current,
-    getFromContext('totalRecords'),
-    getFromContext('endRecord'),
-  ])
+  // We use endRecord as a cheap fallback for totalRecords in case the results node
+  // doesn't contain a count (eg. for very expensive queries using contexture-mongo,
+  // where the count may be disabled for performance). This lets us use the normal
+  // pagination component to navigate back through previous pages.
+  let totalRecords =
+    getFromContext('totalRecords') || getFromContext('endRecord') || 0
   return (
     <TableFooter
       page={node.page || 1}
@@ -24,11 +21,11 @@ let ResultTableFooter = ({ tree, node, pageSizeOptions }) => {
       onChangePageSize={pageSize => {
         tree.mutate(node.path, {
           pageSize,
-          page: _.min([node.page, _.ceil(totalRecords.current / pageSize)]),
+          page: _.min([node.page, _.ceil(totalRecords / pageSize)]),
         })
       }}
       pageSizeOptions={pageSizeOptions}
-      totalRecords={totalRecords.current}
+      totalRecords={totalRecords}
       hasMore={getFromContext('hasMore')}
     />
   )
