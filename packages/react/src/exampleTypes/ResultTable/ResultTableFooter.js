35b0f12ca0ff6fae7132a6454e9f7a4b8dca3e45
Michelle Saad <git@michsa.me>
2020-02-25 18:52:53 -0500

    ResultTableFooter: fix page resize logic
    https://github.com/smartprocure/contexture-react/pull/384

--- packages/react/src/exampleTypes/ResultTable/ResultTableFooter.js
+++ packages/react/src/exampleTypes/ResultTable/ResultTableFooter.js
@@ -7,12 +7,6 @@ import { contexturifyWithoutLoader } from '../../utils/hoc'
 let ResultTableFooter = ({ tree, node, pageSizeOptions }) => {
   let getFromContext = key =>
     F.cascade([`context.response.${key}`, `context.${key}`], node)
-  // We use endRecord as a cheap fallback for totalRecords in case the results node
-  // doesn't contain a count (eg. for very expensive queries using contexture-mongo,
-  // where the count may be disabled for performance). This lets us use the normal
-  // pagination component to navigate back through previous pages.
-  let totalRecords =
-    getFromContext('totalRecords') || getFromContext('endRecord') || 0
   return (
     <TableFooter
       page={node.page || 1}
@@ -21,7 +15,7 @@ let ResultTableFooter = ({ tree, node, pageSizeOptions }) => {
       onChangePageSize={pageSize => {
         tree.mutate(node.path, {
           pageSize,
-          page: _.min([node.page, _.ceil(totalRecords / pageSize)]),
+          page: _.ceil((getFromContext('startRecord') || 0) / pageSize) || 1,
         })
       }}
       pageSizeOptions={pageSizeOptions}
