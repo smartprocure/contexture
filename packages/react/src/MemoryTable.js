16392b67a15860eaf80e9989b71428db1be54d70
Michelle Saad <git@michsa.me>
2020-03-01 22:09:26 -0500

    add MemoryTable component
    https://github.com/smartprocure/contexture-react/pull/387

---
+++ packages/react/src/MemoryTable.js
@@ -0,0 +1,27 @@
+import Contexture from 'contexture'
+import memory from 'contexture/src/provider-memory'
+import types from 'contexture/src/provider-memory/exampleTypes'
+import { observer } from 'mobx-react'
+import React from 'react'
+import ContextureMobx from './utils/contexture-mobx'
+import { ResultTable } from './exampleTypes'
+
+let MemoryTable = ({ data, fields }) => {
+  let tree = {
+    key: 'root',
+    schema: 'schema',
+    children: [{ key: 'results', type: 'results', pageSize: 5 }],
+  }
+  let service = Contexture({
+    debug: true,
+    schemas: { schema: { memory: { records: data } } },
+    providers: { memory: { ...memory, types: types() } },
+  })
+  let search = ContextureMobx({ service })(tree)
+  search.refresh(['root'])
+  return (
+    <ResultTable fields={fields} tree={search} path={['root', 'results']} />
+  )
+}
+
+export default observer(MemoryTable)
