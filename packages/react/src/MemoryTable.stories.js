b6e4c231be31f099dcfbb2125cc12efd0b2578a0
Michelle Saad <git@michsa.me>
2020-03-01 23:04:27 -0500

    MemoryTable: add emoji-data story
    https://github.com/smartprocure/contexture-react/pull/387

--- packages/react/src/MemoryTable.stories.js
+++ packages/react/src/MemoryTable.stories.js
@@ -2,6 +2,8 @@ import _ from 'lodash/fp'
 import React from 'react'
 import ThemePicker from './stories/themePicker'
 import MemoryTable from './MemoryTable'
+import emoji from 'emoji-datasource'
+import sheet from 'emoji-datasource/img/twitter/sheets/32.png'
 
 export default {
   title: 'MemoryTable',
@@ -14,6 +16,34 @@ export let story = () => {
   let data = _.times(x => ({ _id: x, value: _.random(0, 20000) }), 221)
   return <MemoryTable {...{ data, fields }} />
 }
+
+export let withInfer = () => {
+  let data = _.times(x => ({ _id: x, value: _.random(0, 20000) }), 221)
+  return <MemoryTable infer {...{ data }} />
+}
+
+export let emojis = () => (
+  <MemoryTable
+    infer
+    data={emoji}
+    fields={{
+      image: {
+        order: 1,
+        display: (x, { sheet_x, sheet_y }) => (
+          <div
+            style={{
+              backgroundImage: `url(${sheet})`,
+              backgroundPosition: `-${sheet_x * 34}px -${sheet_y * 34}px`,
+              width: 33,
+              height: 33,
+              transform: 'scale(0.75)',
+            }}
+          />
+        ),
+      },
+    }}
+  />
+)
 
 export let resultTableProps = () => {
   let fields = { _id: { label: 'id' }, value: { label: 'val' } }
