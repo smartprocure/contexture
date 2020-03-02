16ff000e946bc214cd7ea0522fe8a4cf66f6db4c
Michelle Saad <git@michsa.me>
2020-03-01 22:17:43 -0500

    MemoryTable: add story for ResultTable props
    https://github.com/smartprocure/contexture-react/pull/387

--- packages/react/src/MemoryTable.stories.js
+++ packages/react/src/MemoryTable.stories.js
@@ -14,3 +14,15 @@ export let story = () => {
   let data = _.times(x => ({ _id: x, value: _.random(0, 20000) }), 221)
   return <MemoryTable {...{ data, fields }} />
 }
+
+export let resultTableProps = () => {
+  let fields = { _id: { label: 'id' }, value: { label: 'val' } }
+  let data = _.times(x => ({ _id: x, value: _.random(0, 20000) }), 221)
+  return (
+    <MemoryTable
+      {...{ data, fields }}
+      pageSize={12}
+      pageSizeOptions={[12, 24, 48, 96]}
+    />
+  )
+}
