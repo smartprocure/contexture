7a7cdacd16e7ab9366d5025997dbdc330b034a6a
Michelle Saad <git@michsa.me>
2020-03-03 18:01:20 -0500

    update emoji data schemas
    https://github.com/smartprocure/contexture-react/pull/387

--- packages/react/src/exampleTypes/stories/emojiData.js
+++ packages/react/src/exampleTypes/stories/emojiData.js
@@ -4,46 +4,55 @@ import F from 'futil'
 import MemoryTable from '../../MemoryTable'
 import EmojiIcon from '../../stories/EmojiIcon'
 
-let typeDefaults = {
-  name: 'text',
-  unified: 'text',
-  non_qualified: 'text',
-  docomo: null,
-  au: 'text',
-  softbank: 'text',
-  google: 'text',
-  image: 'text',
-  sheet_x: 'number',
-  sheet_y: 'number',
-  short_name: 'text',
-  short_names: 'text',
-  text: null,
-  texts: null,
-  category: 'facet',
-  sort_order: 'number',
-  added_in: 'facet',
-  has_img_apple: 'bool',
-  has_img_google: 'bool',
-  has_img_twitter: 'bool',
-  has_img_facebook: 'bool',
-  skin_variations: 'facet',
-  obsoletes: 'facet',
-  obsoleted_by: 'facet',
+// types
+let number = { typeDefault: number }
+let bool = { typeDefault: 'bool', display: x => (x ? 'Yes' : 'No') }
+let codePoint = {
+  typeDefault: 'text',
+  display: F.whenExists(
+    _.flow(
+      _.split('-'),
+      _.map(x => parseInt(x, 16)),
+      _.spread(String.fromCodePoint)
+    )
+  ),
 }
 
-let overrides = {
-  skin_variations: {
-    display: x => x && <MemoryTable infer data={F.unkeyBy('foo', x)} />,
-  },
+let schema = {
   image: {
-    order: 1,
     display: (x, record) => <EmojiIcon set="facebook" record={record} />,
   },
+  name: { typeDefault: 'text' },
+  unified: codePoint,
+  non_qualified: codePoint,
+  sheet_x: { typeDefault: 'number' },
+  sheet_y: { typeDefault: 'number' },
+  short_name: { typeDefault: 'facet' },
+  short_names: { typeDefault: 'facet' },
+  category: { typeDefault: 'facet' },
+  sort_order: { typeDefault: 'number' },
+  added_in: { typeDefault: 'facet' },
+  has_img_apple: bool,
+  has_img_google: bool,
+  has_img_twitter: bool,
+  has_img_facebook: bool,
+  text: { typeDefault: 'facet' },
+  texts: { typeDefault: 'facet' },
+  obsoletes: codePoint,
+  obsoleted_by: codePoint,
 }
-
-let schema = _.flow(
-  _.mapValues(typeDefault => ({ typeDefault })),
-  _.merge(overrides)
-)(typeDefaults)
+schema.skin_variations = {
+  typeDefault: 'facet',
+  display: x =>
+    x && (
+      <MemoryTable
+        data={F.unkeyBy('skin_tone', x)}
+        fields={{
+          skin_tone: codePoint,
+          ..._.pick(['image', 'unified'], schema),
+        }}
+      />
+    ),
+}
 
 export default schema
