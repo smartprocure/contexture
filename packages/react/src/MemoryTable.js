510f4fcf857ea6f577b3930f96f56ae5afb02adf
Michelle Saad <git@michsa.me>
2020-03-03 13:37:51 -0500

    name the schema in MemoryTable
    https://github.com/smartprocure/contexture-react/pull/387

--- packages/react/src/MemoryTable.js
+++ packages/react/src/MemoryTable.js
@@ -7,19 +7,20 @@ import ContextureMobx from './utils/contexture-mobx'
 import { componentForType } from './utils/schema'
 import { ResultTable, TypeMap } from './exampleTypes'
 
-export let memoryService = (records, { debug } = {}) =>
+export let memoryService = (records, { schema, debug } = {}) =>
   Contexture({
     debug,
     // Hack to effectively set a default schema: if our tree root doesn't have
     // a `schema` property, it will get the schema at key `undefined`.
-    schemas: { undefined: { memory: { records } } },
+    schemas: { [schema]: { memory: { records } } },
     providers: { memory: { ...memory, types: types() } },
   })
 
 let MemoryTable = ({ data, fields, pageSize = 10, debug, ...props }) => {
-  let service = memoryService(data, { debug })
+  let service = memoryService(data, { schema: 'data', debug })
   let tree = ContextureMobx({ service })({
     key: 'root',
+    schema: 'data',
     children: [
       { key: 'results', type: 'results', pageSize },
       { key: 'criteria', type: 'group' },
