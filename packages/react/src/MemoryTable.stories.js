d87caac8165431f4b27aa0f8e8d700934b0babff
Michelle Saad <git@michsa.me>
2020-03-03 13:38:10 -0500

    add default type schemas for emoji data
    https://github.com/smartprocure/contexture-react/pull/387

--- packages/react/src/MemoryTable.stories.js
+++ packages/react/src/MemoryTable.stories.js
@@ -3,19 +3,21 @@ import React from 'react'
 import EmojiIcon from './stories/EmojiIcon'
 import ThemePicker from './stories/themePicker'
 import MemoryTable from './MemoryTable'
+import { schema } from './exampleTypes/stories/emojiData'
 
 export default {
   title: 'MemoryTable',
   component: MemoryTable,
   decorators: [ThemePicker('greyVest')],
   parameters: {
-    componentSubtitle: "A ResultTable from arbitrary data using contexture's memory provider",
+    componentSubtitle:
+      "A ResultTable from arbitrary data using contexture's memory provider",
     info: `
 MemoryTable is built on top of ResultTable and supports several of the same props: most notably \`fields\`, which takes a schema object that specifies which fields from the data are visible in the table and how they are ordered, and \`infer\`, which enables MemoryTable to infer field information from the given data without having to explicitly specify it in \`fields\`.
 
 However, in place of ResultTable's contexture-relevant \`tree\`/\`node\`/\`path\` props, MemoryTable simply accepts a \`data\` prop, which should be an array of obects. This is fed into a contexture instance running on the \`memory\` provider, which allows contexture to work against data in the form of plain Javascript objects (in contrast to, for example, a MongoDB database). The result is a dynamically-generated table with built-in support for sorting and filtering operations on the given data.
-`
-  }
+`,
+  },
 }
 
 export let story = () => {
@@ -46,6 +48,7 @@ export let emojiDataset = () => (
     infer
     data={require('emoji-datasource')}
     fields={{
+      ...schema,
       image: {
         order: 1,
         display: (x, record) => <EmojiIcon set="facebook" record={record} />,
---
+++ packages/react/src/exampleTypes/stories/emojiData.js
@@ -0,0 +1,30 @@
+import _ from 'lodash/fp'
+
+let defaultTypes = {
+  name: 'text',
+  unified: 'text',
+  non_qualified: 'text',
+  docomo: null,
+  au: 'text',
+  softbank: 'text',
+  google: 'text',
+  image: 'text',
+  sheet_x: 'number',
+  sheet_y: 'number',
+  short_name: 'text',
+  short_names: 'text',
+  text: null,
+  texts: null,
+  category: 'facet',
+  sort_order: 'number',
+  added_in: 'text',
+  has_img_apple: 'bool',
+  has_img_google: 'bool',
+  has_img_twitter: 'bool',
+  has_img_facebook: 'bool',
+  skin_variations: 'facet',
+  obsoletes: 'facet',
+  obsoleted_by: 'facet',
+}
+
+export let schema = _.mapValues(defaultType => ({ defaultType }), defaultTypes)
