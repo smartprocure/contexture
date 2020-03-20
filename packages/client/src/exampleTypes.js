b95e43fdc9730b13356bb72a311208f1ad8189d0
Caden Albaugh <cadenalbaugh@gmail.com>
2020-03-20 17:30:43 -0500

    refactor: remove local stampKey in favor of identical Futil method
    https://github.com/smartprocure/contexture-client/pull/128

--- packages/client/src/exampleTypes.js
+++ packages/client/src/exampleTypes.js
@@ -1,11 +1,6 @@
 import _ from 'lodash/fp'
 import * as F from 'futil-js'
 
-// For futil
-export let stampKey = _.curry((key, x) =>
-  F.mapValuesIndexed((val, k) => ({ ...val, [key]: k }), x)
-)
-
 let validateValues = ({ value, values = [] }) => value || values.length
 
 let twoLevelMatch = {
@@ -22,7 +17,7 @@ let twoLevelMatch = {
   },
 }
 
-export default stampKey('type', {
+export default F.stampKey('type', {
   facet: {
     label: 'List',
     validate: validateValues,
