bbd968db53a1e2def8a5e1b1fa202a73f706590d
Ilian Alexiev <ialexiev@smartprocure.us>
2020-04-28 13:59:40 -0700

    Change hasValue for bool/exists add tests
    https://github.com/smartprocure/contexture-elasticsearch/pull/136

--- packages/elasticsearch/test/example-types/exists.js
+++ packages/elasticsearch/test/example-types/exists.js
@@ -2,6 +2,19 @@ var exists = require('../../src/example-types/exists')
 let { expect } = require('chai')
 
 describe('exists', () => {
+  describe('hasValue', () => {
+    let node = {
+      type: 'bool',
+      field: 'test'
+    }
+    it('should detect a boolean value only', () => {
+      expect(exists.hasValue({ ...node, value: true })).to.be.true
+      expect(exists.hasValue({ ...node, value: false })).to.be.true
+      expect(exists.hasValue({ ...node, value: null })).to.be.false
+      expect(exists.hasValue({ ...node, value: undefined })).to.be.false
+      expect(exists.hasValue(node)).to.be.false
+    })
+  })
   it('should filter properly', () => {
     expect(
       exists.filter({
