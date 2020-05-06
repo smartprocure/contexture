a0fa3a7a83818f1c3686f6d77600ba9470fa5879
Ilian Alexiev <ialexiev@smartprocure.us>
2020-05-06 14:15:53 -0700

    Fix lint
    https://github.com/smartprocure/contexture-react/pull/401

--- packages/react/src/purgatory/BooleanType.js
+++ packages/react/src/purgatory/BooleanType.js
@@ -10,24 +10,22 @@ let BooleanType = ({
   display = value => _.isNil(value) ? 'Either' : value ? 'Yes' : 'No',
   className = 'contexture-bool',
   theme: { RadioList }
-}) => {
-  return (
-    <div className={className}>
-      <RadioList
-        value={getValue(node.value)}
-        onChange={value => {
-          tree.mutate(node.path, {
-            value: getValue(value)
-          })
-        }}
-        options={[
-          { label: display(true), value: true },
-          { label: display(false), value: false },
-          { label: display(null), value: null }
-        ]}
-      />
-    </div>
-  )
-}
+}) => (
+  <div className={className}>
+    <RadioList
+      value={getValue(node.value)}
+      onChange={value => {
+        tree.mutate(node.path, {
+          value: getValue(value)
+        })
+      }}
+      options={[
+        { label: display(true), value: true },
+        { label: display(false), value: false },
+        { label: display(null), value: null }
+      ]}
+    />
+  </div>
+)
 
 export default contexturify(BooleanType)
