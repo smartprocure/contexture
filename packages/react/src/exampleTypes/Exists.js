a09f171f88a76e7dac17ce6ea7155ff80f5a45ad
Decrapifier <decrapifier@govspend.us>
2020-05-06 21:11:54 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/401

--- packages/react/src/exampleTypes/Exists.js
+++ packages/react/src/exampleTypes/Exists.js
@@ -3,6 +3,7 @@ import _ from 'lodash/fp'
 import BooleanType from '../purgatory/BooleanType'
 
 export default ({
-  display = value => _.isNil(value) ? 'Either' : value ? 'Exists' : 'Does Not Exist',
+  display = value =>
+    _.isNil(value) ? 'Either' : value ? 'Exists' : 'Does Not Exist',
   ...props
-  }) => <BooleanType className='contexture-exists' display={display} {...props} />
+}) => <BooleanType className="contexture-exists" display={display} {...props} />
--- packages/react/src/purgatory/BooleanType.js
+++ packages/react/src/purgatory/BooleanType.js
@@ -2,32 +2,30 @@ import React from 'react'
 import _ from 'lodash/fp'
 import { contexturify } from '../utils/hoc'
 
-let getValue = value => _.isNil(value)  ? null : !!value
+let getValue = value => (_.isNil(value) ? null : !!value)
 
 let BooleanType = ({
   tree,
   node,
-  display = value => _.isNil(value) ? 'Either' : value ? 'Yes' : 'No',
+  display = value => (_.isNil(value) ? 'Either' : value ? 'Yes' : 'No'),
   className = 'contexture-bool',
-  theme: { RadioList }
-}) => {
-  return (
-    <div className={className}>
-      <RadioList
-        value={getValue(node.value)}
-        onChange={value => {
-          tree.mutate(node.path, {
-            value: getValue(value)
-          })
-        }}
-        options={[
-          { label: display(true), value: true },
-          { label: display(false), value: false },
-          { label: display(null), value: null }
-        ]}
-      />
-    </div>
-  )
-}
+  theme: { RadioList },
+}) => (
+  <div className={className}>
+    <RadioList
+      value={getValue(node.value)}
+      onChange={value => {
+        tree.mutate(node.path, {
+          value: getValue(value),
+        })
+      }}
+      options={[
+        { label: display(true), value: true },
+        { label: display(false), value: false },
+        { label: display(null), value: null },
+      ]}
+    />
+  </div>
+)
 
 export default contexturify(BooleanType)
