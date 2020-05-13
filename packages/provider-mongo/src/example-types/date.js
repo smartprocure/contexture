8d3fd51422202666f3232de909be26a51f752e17
David Sargeant <david@dsargeant.com>
2020-05-13 16:59:19 -0400

    Move rolling date expressions to the server.
    https://github.com/smartprocure/contexture-mongo/pull/80

--- packages/provider-mongo/src/example-types/date.js
+++ packages/provider-mongo/src/example-types/date.js
@@ -1,3 +1,4 @@
+let _ = require('lodash/fp')
 let F = require('futil')
 let moment = require('moment-timezone')
 let datemath = require('@elastic/datemath')
@@ -18,37 +19,64 @@ let getEndOfQuarter = date =>
     .add(1, 'Q')
     .subtract(1, 'ms')
 
+let quarterToOffset = { thisQuarter: 0, lastQuarter: -1, nextQuarter: 1 }
+
+// https://www.elastic.co/guide/en/elasticsearch/reference/7.x/common-options.html#date-math
+let rangeToDatemath = {
+  last3Days: { from: 'now-3d', to: 'now' },
+  last7Days: { from: 'now-7d', to: 'now' },
+  last30Days: { from: 'now-30d', to: 'now' },
+  last90Days: { from: 'now-90d', to: 'now' },
+  last180Days: { from: 'now-180d', to: 'now' },
+  last12Months: { from: 'now/d-12M', to: 'now' },
+  last15Months: { from: 'now/d-15M', to: 'now' },
+  last18Months: { from: 'now/d-18M', to: 'now' },
+  last24Months: { from: 'now/d-24M', to: 'now' },
+  last36Months: { from: 'now/d-36M', to: 'now' },
+  last48Months: { from: 'now/d-48M', to: 'now' },
+  last60Months: { from: 'now/d-60M', to: 'now' },
+  lastCalendarMonth: { from: 'now-1M/M', to: 'now/M-1ms' },
+  lastCalendarYear: { from: 'now-1y/y', to: 'now/y-1ms' },
+  thisCalendarMonth: { from: 'now/M', to: 'now+1M/M-1ms' },
+  thisCalendarYear: { from: 'now/y', to: 'now' },
+  nextCalendarMonth: { from: 'now+1M/M', to: 'now+2M/M-1ms' },
+  nextCalendarYear: { from: 'now+1y/y', to: 'now+2y/y-1ms' },
+  next30Days: { from: 'now/d', to: 'now/d+30d-1ms' },
+  next60Days: { from: 'now/d', to: 'now/d+60d-1ms' },
+  next90Days: { from: 'now/d', to: 'now/d+90d-1ms' },
+  next6Months: { from: 'now/d', to: 'now/d+6M-1ms' },
+  next12Months: { from: 'now/d', to: 'now/d+12M-1ms' },
+  next24Months: { from: 'now/d', to: 'now/d+24M-1ms' },
+  next36Months: { from: 'now/d', to: 'now/d+36M-1ms' },
+}
+
+let rollingRangeToDates = (range, timezone) => {
+  if (/quarter/.test(range)) {
+    let from = getStartOfQuarter(quarterToOffset[range], timezone)
+    let to = getEndOfQuarter(from)
+    return { from, to }
+  } else {
+    let expressions = rangeToDatemath[range]
+    let from = moment.tz(datemath.parse(expressions.from), timezone)
+    let to = moment.tz(datemath.parse(expressions.to), timezone)
+    return { from, to }
+  }
+}
+
+let dateTypeToFormatFn = {
+  date: x => x && moment.utc(x).toDate(),
+  unix: x => x && moment.utc(x).unix(),
+  timestamp: x => x && new Date(x).getTime(),
+}
+
 module.exports = {
   hasValue: node => node.from || node.to,
-  filter({
-    from,
-    to,
-    field,
-    useDateMath,
-    dateType = 'date',
-    timezone = 'UTC',
-  }) {
-    if (useDateMath) {
-      if (from === 'thisQuarter') {
-        from = getStartOfQuarter(0, timezone)
-        to = getEndOfQuarter(from)
-      } else if (from === 'lastQuarter') {
-        from = getStartOfQuarter(-1, timezone)
-        to = getEndOfQuarter(from)
-      } else if (from === 'nextQuarter') {
-        from = getStartOfQuarter(1, timezone)
-        to = getEndOfQuarter(from)
-      } else {
-        from = moment.tz(datemath.parse(from), timezone)
-        to = moment.tz(datemath.parse(to), timezone)
-      }
-    }
+  filter({ field, range, dateType = 'date', timezone = 'UTC', ...context }) {
+    let { from, to } = !_.includes(range, ['exact', 'all'])
+      ? rollingRangeToDates(range, timezone)
+      : context
 
-    let format = {
-      date: x => x && moment.utc(x).toDate(),
-      unix: x => x && moment.utc(x).unix(),
-      timestamp: x => x && new Date(x).getTime(),
-    }[dateType]
+    let format = dateTypeToFormatFn[dateType]
 
     return {
       [field]: F.compactObject({
