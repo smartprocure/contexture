447d2126f5b64809370496647d54175db4ff8dcc
David Sargeant <david@dsargeant.com>
2020-05-15 18:04:09 -0400

    Fix tests.
    https://github.com/smartprocure/contexture-elasticsearch/pull/137

--- packages/elasticsearch/test/example-types/date.js
+++ packages/elasticsearch/test/example-types/date.js
@@ -9,12 +9,13 @@ describe('date/filter', () => {
       date.filter({
         type: 'date',
         field: 'test',
+        range: 'exact',
         from: '2016-04-25',
       })
     ).to.deep.equal({
       range: {
         test: {
-          gte: '2016-04-25',
+          gte: '2016-04-25T00:00:00.000Z',
           format: 'dateOptionalTime',
         },
       },
@@ -25,12 +26,13 @@ describe('date/filter', () => {
       date.filter({
         type: 'date',
         field: 'test',
-        to: '2016-04-25',
+        range: 'exact',
+        to: '2016-04-25T00:00:00.000Z',
       })
     ).to.deep.equal({
       range: {
         test: {
-          lte: '2016-04-25',
+          lte: '2016-04-25T00:00:00.000Z',
           format: 'dateOptionalTime',
         },
       },
@@ -41,123 +43,15 @@ describe('date/filter', () => {
       date.filter({
         type: 'date',
         field: 'test',
+        range: 'exact',
         from: '2015-04-25',
         to: '2016-04-25',
       })
     ).to.deep.equal({
       range: {
         test: {
-          lte: '2016-04-25',
-          gte: '2015-04-25',
-          format: 'dateOptionalTime',
-        },
-      },
-    })
-  })
-  it('should handle custom dateMath', () => {
-    expect(
-      date.filter({
-        type: 'date',
-        field: 'test',
-        from: '2015-04-25',
-        to: 'now+1M',
-        useDateMath: true,
-      })
-    ).to.deep.equal({
-      range: {
-        test: {
-          lte: moment.utc(datemath.parse('now+1M')).format('YYYY-MM-DD'),
-          gte: '2015-04-25',
-          format: 'dateOptionalTime',
-        },
-      },
-    })
-  })
-  it('should handle dateMath thisQuarter', () => {
-    expect(
-      date.filter({
-        type: 'date',
-        field: 'test',
-        from: 'thisQuarter',
-        useDateMath: true,
-      })
-    ).to.deep.equal({
-      range: {
-        test: {
-          gte: moment()
-            .quarter(moment().quarter())
-            .startOf('quarter')
-            .format('YYYY-MM-DD'),
-          lte: moment
-            .utc(
-              datemath.parse(
-                `${moment()
-                  .quarter(moment().quarter())
-                  .startOf('quarter')
-                  .format('YYYY-MM-DD')}||+3M-1d/d`
-              )
-            )
-            .format('YYYY-MM-DD'),
-          format: 'dateOptionalTime',
-        },
-      },
-    })
-  })
-  it('should handle dateMath lastQuarter', () => {
-    expect(
-      date.filter({
-        type: 'date',
-        field: 'test',
-        from: 'lastQuarter',
-        useDateMath: true,
-      })
-    ).to.deep.equal({
-      range: {
-        test: {
-          gte: moment()
-            .quarter(moment().quarter() - 1)
-            .startOf('quarter')
-            .format('YYYY-MM-DD'),
-          lte: moment
-            .utc(
-              datemath.parse(
-                `${moment()
-                  .quarter(moment().quarter() - 1)
-                  .startOf('quarter')
-                  .format('YYYY-MM-DD')}||+3M-1d/d`
-              )
-            )
-            .format('YYYY-MM-DD'),
-          format: 'dateOptionalTime',
-        },
-      },
-    })
-  })
-  it('should handle dateMath nextQuarter', () => {
-    expect(
-      date.filter({
-        type: 'date',
-        field: 'test',
-        from: 'nextQuarter',
-        useDateMath: true,
-      })
-    ).to.deep.equal({
-      range: {
-        test: {
-          gte: moment()
-            .quarter(moment().quarter() + 1)
-            .startOf('quarter')
-            .format('YYYY-MM-DD'),
-          lte: moment
-            .utc(
-              datemath.parse(
-                `${moment()
-                  .quarter(moment().quarter() + 1)
-                  .startOf('quarter')
-                  .format('YYYY-MM-DD')}||+3M-1d/d`
-              )
-            )
-            .format('YYYY-MM-DD'),
+          lte: '2016-04-25T00:00:00.000Z',
+          gte: '2015-04-25T00:00:00.000Z',
           format: 'dateOptionalTime',
         },
       },
@@ -168,6 +62,7 @@ describe('date/filter', () => {
       date.filter({
         type: 'date',
         field: 'test',
+        range: 'exact',
         from: 'a very specific date',
         to: 'another very specific date',
         isDateTime: true,
