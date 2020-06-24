0e0e11899a67b526c66f94db9ec98bb205b206c5
Decrapifier <decrapifier@govspend.us>
2020-06-24 22:42:48 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/412

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -5,9 +5,7 @@ import { toNumber } from '../utils/format'
 
 let ResultCount = ({ node, display = toNumber }) => {
   let count = _.get('context.response.results.length', node)
-  return count
-    ? display(node.context.response.totalRecords)
-    : 'No Results'
+  return count ? display(node.context.response.totalRecords) : 'No Results'
 }
 
 export default _.flow(
