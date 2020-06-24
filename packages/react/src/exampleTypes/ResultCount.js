b8e95046d28dee4e12bc9bbde147fcf82a513e71
Ilian Alexiev <akrion@gmail.com>
2020-06-24 15:41:03 -0700

    Feedback changes
    https://github.com/smartprocure/contexture-react/pull/412

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -3,10 +3,10 @@ import { observer } from 'mobx-react'
 import { withNode, withInlineLoader } from '../utils/hoc'
 import { toNumber } from '../utils/format'
 
-let ResultCount = ({ node, display = x => x }) => {
+let ResultCount = ({ node, display = toNumber }) => {
   let count = _.get('context.response.results.length', node)
   return count
-    ? display(toNumber(node.context.response.totalRecords))
+    ? display(node.context.response.totalRecords)
     : 'No Results'
 }
 
