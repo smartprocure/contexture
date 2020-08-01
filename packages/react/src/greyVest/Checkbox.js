a0246912c4d21c8d5af87baf664e080c8913a728
Decrapifier <decrapifier@govspend.us>
2020-08-01 06:29:48 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/423

--- packages/react/src/greyVest/Checkbox.js
+++ packages/react/src/greyVest/Checkbox.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
 // Low effort custom checkbox
-let Checkbox = React.forwardRef(({ checked, onChange = () => {}, style = {} },ref) => (
-  <label
-    className={`gv-input gv-checkbox ${checked ? 'checked' : ''}`}
-    style={style}
-  >
-    <input
-      type="checkbox"
-      style={{ display: 'none' }}
-      {...{ checked, onChange }}
-      ref={ref}
-    />
-    {checked ? (
-      <i className="material-icons">check</i>
-    ) : (
-      String.fromCharCode(160) // non-breaking space
-    )}
-  </label>
-))
+let Checkbox = React.forwardRef(
+  ({ checked, onChange = () => {}, style = {} }, ref) => (
+    <label
+      className={`gv-input gv-checkbox ${checked ? 'checked' : ''}`}
+      style={style}
+    >
+      <input
+        type="checkbox"
+        style={{ display: 'none' }}
+        {...{ checked, onChange }}
+        ref={ref}
+      />
+      {checked ? (
+        <i className="material-icons">check</i>
+      ) : (
+        String.fromCharCode(160) // non-breaking space
+      )}
+    </label>
+  )
+)
 export default Checkbox
