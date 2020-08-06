abd10b407e32f1421050213fd7338ef6c625ece7
Samuel Greene <sam@samuelgreene.info>
2020-08-05 23:34:45 -0400

    modern js
    https://github.com/smartprocure/contexture-elasticsearch/pull/148

--- packages/provider-elasticsearch/src/aggUtils.js
+++ packages/provider-elasticsearch/src/aggUtils.js
@@ -10,16 +10,17 @@ let metrics = [
   'median_absolute_deviation',
 ]
 module.exports = {
-  buildAgg: agg => ({
-    [agg.key || agg.type]: {
-      [agg.type]: _.extend(agg.data, {
-        field: agg.field,
-      }),
+  buildAgg: ({ key, type, data, field }) => ({
+    [key || type]: {
+      [type]: {
+        ...data,
+        field,
+      },
     },
   }),
-  buildFilter: agg => ({
-    [agg.type]: {
-      [agg.field]: agg.data,
+  buildFilter: ({ type, field, data }) => ({
+    [type]: {
+      [field]: data,
     },
   }),
   metrics,
