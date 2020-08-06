3f30ee64a58d197fd6deff56dd7b75e3200d3b37
chris110408 <lei110408@gmail.com>
2020-08-06 00:18:09 -0400

    commit add ShowFiltersButton
    https://github.com/smartprocure/contexture-react/pull/426

--- packages/react/src/ToggleFiltersHeader.js
+++ packages/react/src/ToggleFiltersHeader.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ToggleFiltersButton from './purgatory/ToggleFiltersButton'
+import ShowFiltersButton from './purgatory/ShowFiltersButton'
 import { Flex } from './greyVest'
 
 let ToggleFiltersHeader = ({ mode, setMode, children }) => (
   <Flex style={{ alignItems: 'center' }}>
     {mode === 'resultsOnly' && (
       <span style={{ marginRight: 5 }}>
-        <ToggleFiltersButton onClick={() => setMode('basic')} />
+        <ShowFiltersButton onClick={() => setMode('basic')} />
       </span>
     )}
     <h1>{children}</h1>
