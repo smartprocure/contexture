b24fb058787f5a25914222c541686b3cf09692fd
Samuel Greene <sam@samuelgreene.info>
2020-08-06 00:07:56 -0400

    refactoring
    https://github.com/smartprocure/contexture-elasticsearch/pull/148

--- packages/elasticsearch/src/example-types/facet.js
+++ packages/elasticsearch/src/example-types/facet.js
@@ -2,30 +2,24 @@ let _ = require('lodash/fp')
 let F = require('futil')
 let { buildRegexQueryForWords, buildRegexForWords } = require('../regex')
 let { getField } = require('../fields')
+let { negate } = require('../elasticDSL')
 
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html#number
 const elasticsearchIntegerMax = 2 ** 31 - 1
 
 module.exports = {
-  hasValue: context => _.get('values.length', context),
-  filter(context, schema = {}) {
-    let field = getField(schema, context.field, context.fieldMode)
+  hasValue: _.get('values.length'),
+  filter(node, schema = {}) {
+    let field = getField(schema, node.field, node.fieldMode)
     let result = {
       terms: {
-        [field]: context.values,
+        [field]: node.values,
       },
     }
-
-    if (context.mode === 'exclude') {
-      result = {
-        bool: {
-          must_not: result,
-        },
-      }
-    }
+    if (node.mode === 'exclude') result = negate(result)
 
     // trying to prevent 'Too Many Clauses' exception ... http://george-stathis.com/2013/10/18/setting-the-booleanquery-maxclausecount-in-elasticsearch/
-    if (context.values.length > 4095) {
+    if (node.values.length > 4095) {
       // 4096 is our actual limit
       result = {
         bool: {
@@ -37,45 +31,40 @@ module.exports = {
     return result
   },
   async result(context, search, schema) {
+    let { values, size, cardinality } = context
     let field = getField(schema, context.field, context.fieldMode)
-    let values = _.get('values', context)
+    let order = {
+      term: { _key: 'asc' },
+      count: { _count: 'desc' },
+    }[context.sort || 'count']
 
     let resultRequest = {
       aggs: {
         facetOptions: {
-          terms: _.extendAll([
-            {
-              field,
-              // Size 0 no longer supported natively by ES: https://github.com/elastic/elasticsearch/issues/18838
-              size:
-                context.size ||
-                (context.size === 0 ? elasticsearchIntegerMax : 10),
-              order: {
-                term: { _key: 'asc' },
-                count: { _count: 'desc' },
-              }[context.sort || 'count'],
-            },
-            context.includeZeroes && { min_doc_count: 0 },
-            context.optionsFilter && {
+          terms: {
+            field,
+            // Size 0 no longer supported natively by ES: https://github.com/elastic/elasticsearch/issues/18838
+            size: size || (size === 0 ? elasticsearchIntegerMax : 10),
+            order,
+            ...context.includeZeroes && { min_doc_count: 0 },
+            ...context.optionsFilter && {
               include: buildRegexForWords(
                 context.caseSensitive,
                 context.anyOrder, // Scary
                 context.maxWords
               )(context.optionsFilter),
             },
-          ]),
+          },
         },
         facetCardinality: {
           cardinality: {
             field,
-            precision_threshold: _.isNumber(context.cardinality)
-              ? context.cardinality
-              : 5000, // setting default precision to reasonable default (40000 is max),
+            // setting default precision to reasonable default (40000 is max),
+            precision_threshold: _.isNumber(cardinality) ? cardinality : 5000,
           },
         },
       },
     }
-
     if (context.optionsFilter) {
       resultRequest.aggs = {
         topLevelFilter: {
@@ -132,10 +121,7 @@ module.exports = {
               terms: {
                 field,
                 size: missing.length,
-                order: {
-                  term: { _key: 'asc' },
-                  count: { _count: 'desc' },
-                }[context.sort || 'count'],
+                order,
               },
             },
           },
