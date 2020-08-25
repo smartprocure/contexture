cb49cbd56ef1a8569d2464d5c0fee1c4376cba3d
Samuel Greene <sam@samuelgreene.info>
2020-08-25 01:02:20 -0400

    * `groupedByValuePartition` - replaces `matchStats` and `matchCardinality`
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

---
+++ packages/provider-elasticsearch/src/example-types/metricGroups/groupedByValuePartition.js
@@ -0,0 +1,26 @@
+let { statsAggs, simplifyBuckets } = require('./utils')
+
+let buildQuery = ({ statsField, stats, groupField, matchValue }) => ({
+  aggs: {
+    groups: {
+      filters: {
+        other_bucket_key: 'fail',
+        filters: { pass: { term: { [groupField]: matchValue } } },
+      },
+      ...statsAggs(statsField, stats)
+    }
+  },
+})
+
+module.exports = {
+  buildQuery,
+  validContext: node => node.groupField && node.statsField,
+  result: async (node, search) => {
+    let response = await search(buildQuery(node))
+    return {
+      results: simplifyBuckets(
+        F.unkeyBy('key', response.aggregations.groups.buckets)
+      )
+    }
+  },
+}
---
+++ packages/provider-elasticsearch/test/example-types/metricGroups/groupedByValuePartition.js
@@ -0,0 +1,55 @@
+let { buildQuery } = require('../../../src/example-types/metricGroups/groupedByValuePartition')
+let { expect } = require('chai')
+
+describe('groupedByValuePartition', () => {
+  it('should buildQuery', () => {
+    expect(
+      buildQuery({
+        key: 'test',
+        type: 'groupedByValuePartition',
+        groupField: 'Vendor.City.untouched',
+        statsField: 'LineItem.TotalPrice',
+        matchValue: 'Washington'
+      },)
+    ).to.eql({
+      aggs: {
+        groups: {
+          filters: {
+            other_bucket_key: 'fail',
+            filters: { pass: { term: { 'Vendor.City.untouched': 'Washington' } } },
+          },
+          aggs: {
+            min: { min: { field: 'LineItem.TotalPrice' } },
+            max: { max: { field: 'LineItem.TotalPrice' } },
+            avg: { avg: { field: 'LineItem.TotalPrice' } },
+            sum: { sum: { field: 'LineItem.TotalPrice' } },
+          },
+        },
+      },
+    })
+  })
+  it('should buildQuery for cardinality', () => {
+    expect(
+      buildQuery({
+        key: 'test',
+        type: 'groupedByValuePartition',
+        groupField: 'Vendor.City.untouched',
+        statsField: 'LineItem.TotalPrice',
+        matchValue: 'Washington',
+        stats: ['cardinality']
+      },)
+    ).to.eql({
+      aggs: {
+        groups: {
+          filters: {
+            other_bucket_key: 'fail',
+            filters: { pass: { term: { 'Vendor.City.untouched': 'Washington' } } },
+          },
+          aggs: {
+            cardinality: { cardinality: { field: 'LineItem.TotalPrice' } },
+          },
+        },
+      },
+    })
+  })
+})
