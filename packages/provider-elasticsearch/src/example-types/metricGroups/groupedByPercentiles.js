159a9c2df595f582333b952606320f3ffe384988
Samuel Greene <sam@samuelgreene.info>
2020-08-25 01:49:29 -0400

    functional fun
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

--- packages/provider-elasticsearch/src/example-types/metricGroups/groupedByPercentiles.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/groupedByPercentiles.js
@@ -4,35 +4,19 @@ let { statsAggs, simplifyBuckets } = require('./utils')
 let percentiles = require('../percentiles')
 let getPercentiles = search => field => percentiles.result({ field }, search)
 
-let buildQuery = async (node, getPercentiles) => {
-  let { statsField, stats, groupField: field, percents } = node
-  let percentilesResult = await getPercentiles({ field, ...node})
-  let percentilesArray = _.get('percentiles.values', percentilesResult)
-  
-  let ranges = []
-  _.each(range => {
-    let index = _.indexOf(range, percents)
-    let percentileObj = _.find({ key: range }, percentilesArray)
-    if (index === 0) return ranges.push({ to: percentileObj.value })
-    else {
-      ranges.push({
-        from: _.last(ranges).to,
-        to: percentileObj.value,
-      })
-      if (index === percents.length - 1)
-        ranges.push({ from: percentileObj.value })
-      else
-        ranges.push({
-          from: percentileObj.value,
-          to: percentilesArray[index + 1].value,
-        })
-    }
-  }, percents)
+// [1, 2, 3] -> [{to: 1}, {from: 1, to: 2}, {from: 2, to: 3}, {from: 3}] 
+let boundariesToRanges = _.flow(
+  F.mapIndexed((to, i, list) => F.compactObject({ from: list[i-1], to })),
+  arr => F.push({ from: _.last(arr).to }, arr)
+)
 
+let buildQuery = async (node, getPercentiles) => {
+  let { statsField, stats, groupField: field } = node
+  let { percentiles: { values } } = await getPercentiles({ field, ...node }) 
   return {
     aggs: {
       groups: {
-        range: { field, ranges },
+        range: { field, ranges: boundariesToRanges(_.map('value', values)) },
         ...statsAggs(statsField, stats)
       },
     },
