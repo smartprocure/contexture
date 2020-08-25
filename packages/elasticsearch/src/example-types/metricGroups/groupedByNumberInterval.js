7391aed58e5b65f0e73ede3ccba541a7a3783324
Samuel Greene <sam@samuelgreene.info>
2020-08-25 01:01:07 -0400

    groupedByNumberInterval
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

---
+++ packages/elasticsearch/src/example-types/metricGroups/groupedByNumberInterval.js
@@ -0,0 +1,30 @@
+let { statsAggs, simplifyBuckets } = require('./utils')
+let { calcSmartInterval } = require('../../smartInterval')
+
+let buildQuery = async (
+  { statsField, stats, groupField: field, interval = 'smart' },
+  getStats
+) => {
+  if (interval == 'smart') {
+    let { min, max } = await getStats(field)
+    interval = calcSmartInterval(min, max)
+  }
+
+  return {
+    aggs: {
+      groups: {
+        histogram: { field, interval, min_doc_count: 0 },
+        ...statsAggs(statsField, stats),
+      },
+    },
+  }
+}
+
+module.exports = {
+  buildQuery,
+  validContext: (node) => node.groupField && node.statsField,
+  result: async (node, search) => {
+    let response = await search(buildQuery(node))
+    return { results: simplifyBuckets(response.aggregations.groups.buckets) }
+  },
+}
