2f17c0e00ca69665296bb3d73c0f8fdd3de1fa9d
Samuel Greene <sam@samuelgreene.info>
2020-08-25 00:52:12 -0400

    add valuesDelta
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

---
+++ packages/provider-elasticsearch/src/example-types/metricGroups/valuesDelta.js
@@ -0,0 +1,34 @@
+let _ = require('lodash/fp')
+let { getField } = require('../../fields')
+let { buildFilter, elasticsearchIntegerMax } = require('../../elasticDSL')
+
+let buildQuery = (
+  { groupField, background, foreground, size = elasticsearchIntegerMax },
+  schema
+) => ({
+  aggs: {
+    results: {
+      filters: {
+        filters: _.mapValues(buildFilter, { background, foreground }),
+      },
+      aggs: {
+        field: { terms: { field: getField(schema, groupField), size } },
+      },
+    },
+  },
+})
+
+module.exports = {
+  buildQuery,
+  validContext: () => true,
+  async result(node, search, schema) {
+    console.log('goo"')
+    let response = await search(buildQuery(node, schema))
+    
+    let { foreground, background } = _.mapValues(
+      ground => _.map('key', ground.field.buckets),
+      response.aggregations.results.buckets
+    )
+    return { results: _.difference(foreground, background) }
+  },
+}
