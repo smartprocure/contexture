7a4a860b14b9a699afdb77c86a69d5979b0110bb
Samuel Greene <sam@samuelgreene.info>
2020-08-25 01:01:37 -0400

    groupedByNumberRanges
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

---
+++ packages/provider-elasticsearch/src/example-types/metricGroups/groupedByNumberRanges.js
@@ -0,0 +1,19 @@
+let { statsAggs, simplifyBuckets } = require('./utils')
+
+let buildQuery = ({ groupField: field, statsField, stats, ranges }) => ({
+  aggs: {
+    groups: {
+      range: { field, ranges },
+      ...statsAggs(statsField, stats)
+    }
+  }
+})
+
+module.exports = {
+  buildQuery,
+  validContext: node => node.groupField && node.statsField && node.ranges,
+  result: async (node, search) => {
+    let response = await search(buildQuery(node))
+    return { results: simplifyBuckets(response.aggregations.groups.buckets) }
+  },
+}
