fb59ad719caaf5d41d6a82b97684c6a9c01651b4
Samuel Greene <sam@samuelgreene.info>
2020-08-25 00:59:22 -0400

    add metric group utils
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

---
+++ packages/provider-elasticsearch/src/example-types/metricGroups/utils.js
@@ -0,0 +1,25 @@
+let _ = require('lodash/fp')
+let F = require('futil')
+
+let objectify = F.arrayToObject(_.identity)
+let buildMetrics = (field, metrics = ['min', 'max', 'avg', 'sum']) =>
+  objectify(metric => ({ [metric]: { field } }), metrics)
+
+let statsAggs = (field, stats) =>
+  field ? { aggs: buildMetrics(field, stats) } : {}
+
+let simplifyBuckets = _.flow(
+  F.when(_.isPlainObject, F.unkeyBy('key')),
+  _.map(
+    _.flow(
+      F.renameProperty('doc_count', 'count'),
+      _.mapValues(F.getOrReturn('value'))
+    )
+  )
+)
+
+module.exports = {
+  statsAggs,
+  buildMetrics,
+  simplifyBuckets
+}
---
+++ packages/provider-elasticsearch/test/example-types/metricGroups/utils.js
@@ -0,0 +1,133 @@
+let {
+  statsAggs,
+  buildMetrics,
+  simplifyBuckets,
+} = require('../../../src/example-types/metricGroups/utils')
+let { expect } = require('chai')
+
+describe('metricGroups utils', () => {
+  describe('buildMetrics', () => {
+    it('should work', () => {
+      expect(buildMetrics('price')).to.eql({
+        min: { min: { field: 'price' } },
+        max: { max: { field: 'price' } },
+        avg: { avg: { field: 'price' } },
+        sum: { sum: { field: 'price' } },
+      })
+    })
+    it('should work with explicit stats', () => {
+      expect(buildMetrics('price', ['max', 'sum', 'cardinality'])).to.eql({
+        max: { max: { field: 'price' } },
+        sum: { sum: { field: 'price' } },
+        cardinality: { cardinality: { field: 'price' } },
+      })
+    })
+  })
+  describe('statsAggs', () => {
+    it('should work', () => {
+      expect(statsAggs('price')).to.eql({
+        aggs: {
+          min: { min: { field: 'price' } },
+          max: { max: { field: 'price' } },
+          avg: { avg: { field: 'price' } },
+          sum: { sum: { field: 'price' } },
+        }
+      })
+    })
+    it('should work with explicit stats', () => {
+      expect(statsAggs('price', ['max', 'sum', 'cardinality'])).to.eql({
+        aggs:{
+          max: { max: { field: 'price' } },
+          sum: { sum: { field: 'price' } },
+          cardinality: { cardinality: { field: 'price' } },
+        }
+      })
+    })
+    it('should add no stats without field', () => {
+      expect(statsAggs(null, ['max', 'sum', 'cardinality'])).to.eql({})
+      expect(statsAggs()).to.eql({})
+    })
+  })
+  describe('simplifyBuckets', () => {
+    it('should work', () => {
+      expect(
+        simplifyBuckets([
+          { key: 'test', doc_count: 12, min: { value: 1 }, max: { value: 6 } },
+          { key: 'testy', doc_count: 12, min: { value: 1 }, max: { value: 6 } },
+        ])
+      ).to.eql([
+        { key: 'test', count: 12, min: 1, max: 6 },
+        { key: 'testy', count: 12, min: 1, max: 6 },
+      ])
+    })
+    it('should work for match object', () => {
+      expect(
+        simplifyBuckets({
+          pass: { doc_count: 12, min: { value: 1 }, max: { value: 6 } },
+          fail: { doc_count: 12, min: { value: 1 }, max: { value: 6 } },
+        })
+      ).to.eql([
+        { key: 'pass', count: 12, min: 1, max: 6 },
+        { key: 'fail', count: 12, min: 1, max: 6 },
+      ])
+    })
+    it('should work for range stats', () => {
+      expect(
+        simplifyBuckets([
+          {
+            key: '0.0-500.0',
+            from: 0,
+            from_as_string: '0.0',
+            to: 500,
+            to_as_string: '500.0',
+            doc_count: 476899106,
+              min: {value: -500000000 },
+              max: {value: 937998784 },
+              avg: {value: 973.7296742278231 },
+              sum: {value: 464370811124.9201 },
+            
+          },
+          {
+            key: '500.0-10000.0',
+            from: 500,
+            from_as_string: '500.0',
+            to: 10000,
+            to_as_string: '10000.0',
+            doc_count: 110489302,
+              min: {value: -999299968 },
+              max: {value: 2100000000 },
+              avg: {value: 3038.799582495458 },
+              sum: {value: 335754844787.8146 },
+            
+          },
+        ]
+        )
+      ).to.eql([
+        {
+          key: '0.0-500.0',
+          from: 0,
+          from_as_string: '0.0',
+          to: 500,
+          to_as_string: '500.0',
+          count: 476899106,
+          min:  -500000000,
+          max:  937998784,
+          avg:  973.7296742278231,
+          sum:  464370811124.9201,
+        },
+        {
+          key: '500.0-10000.0',
+          from: 500,
+          from_as_string: '500.0',
+          to: 10000,
+          to_as_string: '10000.0',
+          count: 110489302,
+          min:  -999299968,
+          max:  2100000000,
+          avg:  3038.799582495458,
+          sum:  335754844787.8146,
+        },
+      ])
+    })
+  })
+})
