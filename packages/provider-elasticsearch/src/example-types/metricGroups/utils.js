f6e111a9a121f86c8cc9019b70b17f56841d1e13
Samuel Greene <sam@samuelgreene.info>
2020-08-26 16:50:30 -0400

    add cardinality stats test case and fix bug
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

--- packages/provider-elasticsearch/src/example-types/metricGroups/utils.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/utils.js
@@ -3,12 +3,18 @@ let F = require('futil')
 let objectify = F.arrayToObject(_.identity)
 let emptyObjectify = objectify(() => ({}))
 
+// Rename metricProps keys?   include -> _.source.includes, sort -> order, etc
 let buildMetrics = (field, metrics = ['min', 'max', 'avg', 'sum']) =>
-  F.mapValuesIndexed((metricProps, metric) => {
-    if (metric === 'topHits') field = null
-    // Rename metricProps keys?   include -> _.source.includes, sort -> order, etc
-    return { [_.snakeCase(metric)]: F.omitNil({ field, ...metricProps }) }
-  }, F.when(_.isArray, emptyObjectify, metrics))
+  F.mapValuesIndexed(
+    (metricProps, metric) => ({
+      [_.snakeCase(metric)]: F.omitNil({
+        field: metric === 'topHits' ? null : field,
+        ...metricProps,
+      }),
+    }),
+    F.when(_.isArray, emptyObjectify, metrics)
+  )
+
 
 let statsAggs = (field, stats) =>
   field ? { aggs: buildMetrics(field, stats) } : {}
