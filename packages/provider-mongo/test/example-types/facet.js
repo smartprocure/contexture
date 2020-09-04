ed52e554adaae269a25845b1bdaead0c24a7cff4
Decrapifier <decrapifier@govspend.us>
2020-09-04 18:15:53 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/86

--- packages/provider-mongo/test/example-types/facet.js
+++ packages/provider-mongo/test/example-types/facet.js
@@ -437,8 +437,6 @@ describe('facet', () => {
     })
 
     describe('should always include checked values', () => {
-
-
       let Data = [
         { _id: 1, name: '1' },
         { _id: 2, name: '2' },
@@ -451,50 +449,42 @@ describe('facet', () => {
         { _id: '5d1ca49436e1d20038f8c84f', name: 'Customer Experience' },
         { _id: '5ce30b403aa154002d01b9ed', name: 'Government Division' },
       ]
-     let node = {
+      let node = {
         key: 'id',
         field: '_id',
         type: 'facet',
-       label:{
-         collection:null,
-         foreignField: '_id',
-         fields: ['name'],
-       },
+        label: {
+          collection: null,
+          foreignField: '_id',
+          fields: ['name'],
+        },
         mode: 'include',
         optionsFilter: '',
         size: 2,
       }
-      it('when there are missed values', async() => {
+      it('when there are missed values', async () => {
+        ;(node.label.collection = Data), (node.values = [4])
 
-        node.label.collection= Data,
-
-        node.values = [4]
-
-        let result = await facet.result(node, agg =>
-          mingo.aggregate(Data, agg)
-        )
+        let result = await facet.result(node, agg => mingo.aggregate(Data, agg))
         let ids = _.map(({ name }) => _.toString(name), result.options)
         expect(result.options.length).to.equal(2)
         expect(_.includes('4', ids)).to.be.true
       })
-      it('when there is no missed value', async() => {
-        node.label.collection= Data,
-        node.values = [1]
-        let result = await facet.result(node, agg =>
-          mingo.aggregate(Data, agg)
-        )
+      it('when there is no missed value', async () => {
+        ;(node.label.collection = Data), (node.values = [1])
+        let result = await facet.result(node, agg => mingo.aggregate(Data, agg))
         let ids = _.map(({ name }) => _.toString(name), result.options)
         expect(result.options.length).to.equal(2)
         expect(_.includes('1', ids)).to.be.true
       })
-      it('when there are missed values and isMongoId: true', async() => {
+      it('when there are missed values and isMongoId: true', async () => {
         let collection = _.map(
           ({ _id, name }) => ({ _id: ObjectID(_id), name }),
           mongoIdData
         )
-        node.isMongoId= true
-        node.label.collection= collection,
-        node.values = ['5ce30b403aa154002d01b9ed']
+        node.isMongoId = true
+        ;(node.label.collection = collection),
+          (node.values = ['5ce30b403aa154002d01b9ed'])
 
         let result = await facet.result(node, agg =>
           mingo.aggregate(collection, agg)
@@ -504,14 +494,14 @@ describe('facet', () => {
         expect(result.options.length).to.equal(2)
         expect(_.includes('5ce30b403aa154002d01b9ed', ids)).to.be.true
       })
-      it('when there is no missed value and isMongoId: true', async() => {
+      it('when there is no missed value and isMongoId: true', async () => {
         let collection = _.map(
           ({ _id, name }) => ({ _id: ObjectID(_id), name }),
           mongoIdData
         )
-        node.isMongoId= true
-        node.label.collection= collection,
-        node.values = ['5e9dbd76e991760021124966']
+        node.isMongoId = true
+        ;(node.label.collection = collection),
+          (node.values = ['5e9dbd76e991760021124966'])
 
         let result = await facet.result(node, agg =>
           mingo.aggregate(collection, agg)
@@ -521,9 +511,6 @@ describe('facet', () => {
         expect(result.options.length).to.equal(2)
         expect(_.includes('5e9dbd76e991760021124966', ids)).to.be.true
       })
-
-
     })
-
   })
 })
