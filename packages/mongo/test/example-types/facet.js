62a49add8b40df2782a74fcd1aec285eb860495a
chris110408 <lei110408@gmail.com>
2020-09-10 17:17:22 -0400

    cleanup Unit test
    https://github.com/smartprocure/contexture-mongo/pull/86

--- packages/mongo/test/example-types/facet.js
+++ packages/mongo/test/example-types/facet.js
@@ -509,26 +509,26 @@ describe('facet', () => {
         expect(_.includes('5e9dbd76e991760021124966', ids)).to.be.true
       })
       it('when the first and second search result does not  contain the checked value',async()=>{
-        let config ={
+        let mockConfig ={
           getProvider:()=>({runSearch:()=>[ {  label: { name: '5' }, _id: 5 } ],
           }),
-          getSchema:()=>null
+          getSchema:()=>{}
         }
 
         node.label.collection = Data
         node.isMongoId = null
         node.values = [5]
         let result = await facet.result(node, agg =>
-          mingo.aggregate(Data, agg),null,config
+          mingo.aggregate(Data, agg), { },mockConfig
         )
         let ids = _.map(({ name }) => _.toString(name), result.options)
         expect(_.includes('5', ids)).to.be.true
       })
       it('when the first and second search result does not  contain the checked value  and  isMongoId is true',async()=>{
-        let config ={
+        let mockConfig ={
           getProvider:()=>({runSearch:()=>[ {  label: { name: 'test' }, _id: '5ce30b403aa154002d01b9dd' } ],
           }),
-          getSchema:()=>null
+          getSchema:()=>{}
         }
 
         let collection = _.map(
@@ -539,7 +539,7 @@ describe('facet', () => {
         node.label.collection = collection
         node.values = ['5ce30b403aa154002d01b9dd']
         let result = await facet.result(node, agg =>
-          mingo.aggregate(collection, agg),null,config
+          mingo.aggregate(collection, agg),{},mockConfig
         )
         let ids = _.map(({ name }) => _.toString(name), result.options)
         expect(_.includes('5ce30b403aa154002d01b9dd', ids)).to.be.true
