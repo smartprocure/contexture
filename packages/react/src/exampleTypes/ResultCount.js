f872348a50081f35582933129eeda5579133b2ea
Ilian Alexiev <akrion@gmail.com>
2020-10-20 21:44:21 -0700

    Add F.cascade to ResultsCount
    https://github.com/smartprocure/contexture-react/pull/448

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -1,10 +1,11 @@
 import _ from 'lodash/fp'
+import F from 'futil'
 import { observer } from 'mobx-react'
 import { withNode, withInlineLoader } from '../utils/hoc'
 import { toNumber } from '../utils/format'
 
 let ResultCount = ({ node, display = toNumber }) => {
-  let count = _.get('context.response.results.length', node)
+  let count =  F.cascade(['context.response.results.length', 'context.results.length'], node)
   return count ? display(node.context.response.totalRecords) : 'No Results'
 }
 
