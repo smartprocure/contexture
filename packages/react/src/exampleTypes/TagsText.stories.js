be394e92e7ae888026c18b538fb4a57a4f049c14
Decrapifier <decrapifier@govspend.us>
2020-10-24 08:09:35 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/451

--- packages/react/src/exampleTypes/TagsText.stories.js
+++ packages/react/src/exampleTypes/TagsText.stories.js
@@ -2,17 +2,13 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import TestTree from './stories/testTree'
 import ThemePicker from '../stories/themePicker'
-import  TagsText  from './TagsText'
-
-
+import TagsText from './TagsText'
 
 let treeWithValues = TestTree(testTree => {
   testTree.getNode(['tagsText']).values = ['this is a tag']
   return testTree
 })
 
-
 storiesOf('ExampleTypes|Tags Text', module)
   .addDecorator(ThemePicker('greyVest'))
   .add('TagsText', () => <TagsText tree={treeWithValues} path={['tagsText']} />)
-
