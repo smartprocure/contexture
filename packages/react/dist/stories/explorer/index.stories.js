d426d2065a5dfe548398377c1aceddd12a84d93b
Kiryl Yermakou <rma4ok@gmail.com>
2020-11-10 15:36:20 -0500

    [build] removed dist from git
    https://github.com/smartprocure/contexture-react/pull/454

--- packages/react/dist/stories/explorer/index.stories.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-require("babel-polyfill");
-
-var _react = require("@storybook/react");
-
-(0, _react.storiesOf)('Live Demos|Index Explorer', module).add('Advanced Search', require('./advanced')["default"]).add('Basic Search', require('./basic')["default"]);
-//# sourceMappingURL=index.stories.js.map
