2f73be7458c8a99f0beee679d530bcda83f51aeb
Decrapifier <decrapifier@govspend.us>
2020-12-10 19:20:25 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/465

--- packages/react/src/greyVest/Table.js
+++ packages/react/src/greyVest/Table.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-let Table = ({ className = '', ...props}) => (
+let Table = ({ className = '', ...props }) => (
   <div className="gv-table-parent">
     <table className={`gv-table ${className}`} {...props} />
   </div>
