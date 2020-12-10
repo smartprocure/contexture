e6c1ac071f5473372d9dcbec07b6b65c2ed0d217
Decrapifier <decrapifier@govspend.us>
2020-12-10 19:08:52 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/465

--- packages/react/src/greyVest/Table.js
+++ packages/react/src/greyVest/Table.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 let Table = x => (
   <div className="gv-table-parent">
-    <table {...{ ...x, className: x.className ? `gv-table ${x.className}` : 'gv-table' }} />
+    <table
+      {...{
+        ...x,
+        className: x.className ? `gv-table ${x.className}` : 'gv-table',
+      }}
+    />
   </div>
 )
 export default Table
