4217b9b140beedd65749e12f0434d336cad6c36f
Samuel Greene <sam@samuelgreene.info>
2020-12-15 18:07:33 -0500

    add andGroup and runWith utils
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/utils.js
+++ packages/export/src/utils.js
@@ -6,3 +6,16 @@ export const setFilterOnly = x =>
     F.when(_.isPlainObject, F.setOn('filterOnly', true)),
     _.cloneDeep(x)
   )
+
+// Wraps provided node in a new `and` group, using the first node/tree's schema and key
+export let andGroup = (...args) => {
+  let children = _.flatten(args)
+  let [{ schema, key }] = children
+  return { key: `${key}-parent`, join: 'and', schema, children }
+}
+
+let lastChild = _.flow(_.get('children'), _.last)
+
+// Wraps a tree in an AND group with the provided node, runs the search, then returns the added node with results
+export let runWith = async (service, tree, node) =>
+  lastChild(await service(andGroup(setFilterOnly(tree), node)))
