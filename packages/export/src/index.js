915de6a507b37b11ca262c66cb58f33da1891f37
Samuel Greene <sam@samuelgreene.info>
2020-12-24 15:58:13 -0500

    export all modern things for now
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/index.js
+++ packages/export/src/index.js
@@ -1,9 +1,17 @@
 import * as dataStrategies from './dataStrategies'
 import * as exportStrategies from './exportStrategies'
 import * as utils from './utils'
+import results from './modern/results'
+import terms_stats from './modern/terms_stats'
+import * as schemaToCSVTransforms from './modern/schemaToCSVTransforms'
+import * as csv from './modern/fast-csv-wrapper'
 
 export default {
   dataStrategies,
   exportStrategies,
   ...utils,
+  ...schemaToCSVTransforms,
+  results,
+  terms_stats,
+  csv
 }
