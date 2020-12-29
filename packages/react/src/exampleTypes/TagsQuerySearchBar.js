211ae159c76eae1b90ff9395650ed22bd04a09ab
Caden Albaugh <cadenalbaugh@gmail.com>
2020-12-29 10:33:53 -0600

    fix: use inside event handler to stop propagation when appropriate
    https://github.com/smartprocure/contexture-react/pull/466

--- packages/react/src/exampleTypes/TagsQuerySearchBar.js
+++ packages/react/src/exampleTypes/TagsQuerySearchBar.js
@@ -68,8 +68,16 @@ let SearchBar = ({
       onOutsideClick={() => {
         F.on(collapse)()
       }}
+      useCapture={false}
     >
-      <ButtonGroup style={searchBarStyle}>
+      <ButtonGroup
+        style={searchBarStyle}
+        // The outside click handler listens for the onMouseUp event which takes priority over any onClick handlers in the children
+        // So we need to add this handler to ensure that the child events are triggered appropriately
+        onMouseUp={e => {
+          e.stopPropagation()
+        }}
+      >
         <Box style={searchBarBoxStyle} onClick={F.off(collapse)}>
           <ExpandableTagsQuery
             {...{ tree, node, collapse, actionWrapper }}
