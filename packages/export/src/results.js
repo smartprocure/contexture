9ba16553741483c19d8ebdabbf7f4dc3b8721549
Samuel Greene <sam@samuelgreene.info>
2021-01-12 15:31:51 -0500

    lint
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/results.js
+++ packages/export/src/results.js
@@ -26,14 +26,14 @@ export default ({ service, tree, totalPages = 100, ...node }) => {
     },
     hasNext: () => page <= totalPages,
     getNext: async () => {
-        // cache current node so it can be inspected later for easy debugging
-        let node = result.node = await run({ page, scrollId, skipCount: true })
-        scrollId = node.context.scrollId
-        page++
-        // We return _source flattened onto the root result items because we're mostly
-        // interested in the _source properties but may occasionally want other props like _id.
-        // This will be removed with #28 when a contexture-elasticsearch upgrade is complete
-        return _.map(flattenProp('_source'), resultField('results', node))
+      // cache current node so it can be inspected later for easy debugging
+      let node = result.node = await run({ page, scrollId, skipCount: true })
+      scrollId = node.context.scrollId
+      page++
+      // We return _source flattened onto the root result items because we're mostly
+      // interested in the _source properties but may occasionally want other props like _id.
+      // This will be removed with #28 when a contexture-elasticsearch upgrade is complete
+      return _.map(flattenProp('_source'), resultField('results', node))
     },
   }
   return addIterator(result)
