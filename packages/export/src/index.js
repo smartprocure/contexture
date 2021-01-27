41fce23db5478515183de3c566202a0bc873cc1a
Kurt Robert Rudolph <krudolph@govspend.com>
2021-01-27 11:53:06 -0500

    export the modules so they can be imported directly
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/index.js
+++ packages/export/src/index.js
@@ -1,3 +1,9 @@
+export * from './utils'
+export * as results from './results'
+export * as terms_stats from './terms_stats'
+export * from './schemaToCSVTransforms'
+export * from './fast-csv-wrapper'
+
 import * as utils from './utils'
 import results from './results'
 import terms_stats from './terms_stats'
