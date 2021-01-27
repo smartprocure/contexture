35bfe93f38a085007a90068c1e26e568e6981c91
Kurt Robert Rudolph <krudolph@govspend.com>
2021-01-27 13:07:29 -0500

    Fixed export stuff
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/index.js
+++ packages/export/src/index.js
@@ -1,14 +1,17 @@
 export * from './utils'
-export * as results from './results'
-export * as terms_stats from './terms_stats'
 export * from './schemaToCSVTransforms'
-export * from './fast-csv-wrapper'
 
 import * as utils from './utils'
 import results from './results'
 import terms_stats from './terms_stats'
 import * as schemaToCSVTransforms from './schemaToCSVTransforms'
 import * as csv from './fast-csv-wrapper'
+
+export {
+  results,
+  terms_stats,
+  csv
+}
 
 export default {
   ...utils,
