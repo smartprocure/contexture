a7ef28189af279412b2704d4f4db98e9fc8c5587
Kurt Robert Rudolph <krudolph@govspend.com>
2021-02-02 19:12:15 -0500

    Refactored to use F.arrayToObject
    https://github.com/smartprocure/contexture-export/pull/35

--- packages/export/src/schemaToCSVTransforms.js
+++ packages/export/src/schemaToCSVTransforms.js
@@ -1,10 +1,11 @@
 import _ from 'lodash/fp'
 import { updateMany } from './futil'
+import F from 'futil'
 
 // Maps contexture schemas to tranforms for fast-csv
 export let schemaToCSVTransforms = (schema, {logger = _.noop, header = true, include } = {}) => {
   let count = 0
-  let transformedHeaders = include && _.reduce((acc, k) => ({...acc, [k]: _.get(`${k}.label`, schema) || _.startCase(k) }), {}, include)
+  let transformedHeaders =  include && F.arrayToObject(_.identity,v=>_.getOr(_.startCase(v),`${v}.label`,schema),include)
 
   let headers = _.mapValues('label', schema)
   return {
