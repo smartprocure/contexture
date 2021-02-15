febba057a3920033f68c1e62b80689e20264a422
Samuel Greene <sam@samuelgreene.info>
2021-02-15 15:30:17 -0800

    __all -> index
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

--- packages/provider-elasticsearch/src/types.js
+++ packages/provider-elasticsearch/src/types.js
@@ -3,5 +3,5 @@ let F = require('futil')
 module.exports = (config = {}) =>
   F.mapValuesIndexed(
     (x, type) => F.callOrReturn(x, config[type]),
-    require('./example-types/__all')
+    require('./example-types')
   )
