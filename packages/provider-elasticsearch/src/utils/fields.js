077265f05754ff6b45d111a0074e61c2a8732b0d
Samuel Greene <sam@samuelgreene.info>
2021-02-15 15:26:42 -0800

    cleanup and test cases
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

--- packages/provider-elasticsearch/src/utils/fields.js
+++ packages/provider-elasticsearch/src/utils/fields.js
@@ -2,11 +2,12 @@ let _ = require('lodash/fp')
 
 let maybeAppend = (suffix, str) => _.endsWith(suffix, str) ? str : str + suffix
 
-let dot = x => (x ? `.${x}` : '')
+let dot = x => x ? `.${x}` : ''
 
 let path = (schema, field) =>
   dot(_.get(['fields', field, 'elasticsearch', 'notAnalyzedField'], schema))
 
 module.exports = {
+  maybeAppend,
   getField: (schema, field) => maybeAppend(path(schema, field), field),
 }
