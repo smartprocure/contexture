f5b4da974842906c295c7e17ff5c2f01f42ec44e
Samuel Greene <sam@samuelgreene.info>
2021-02-15 22:30:47 -0800

    use stats instead of statistical in groupedByNumberInterval (only get min and max)
    https://github.com/smartprocure/contexture-elasticsearch/pull/155

--- packages/elasticsearch/src/example-types/metricGroups/groupedByNumberInterval.js
+++ packages/elasticsearch/src/example-types/metricGroups/groupedByNumberInterval.js
@@ -1,14 +1,15 @@
 let { statsAggs, simplifyBuckets } = require('./utils')
 let { calcSmartInterval } = require('../../utils/smartInterval')
-let statistical = require('../statistical')
-let getStats = search => field => statistical.result({ field }, search)
+// let statistical = require('../statistical')
+// let getMinMax = search => field => statistical.result({ field }, search)
+let stats = require('./stats')
+let getMinMax = search => field =>
+  stats.result({ statsField: field, stats: ['min', 'max'] }, search)
 
-let buildQuery = async (
-  { statsField, stats, groupField: field, interval = 'smart' },
-  getStats
-) => {
+let buildQuery = async (node, getMinMax) => {
+  let { statsField, stats, groupField: field, interval = 'smart' } = node
   if (interval === 'smart') {
-    let { min, max } = await getStats(field)
+    let { min, max } = await getMinMax(field)
     interval = calcSmartInterval(min, max)
   }
 
@@ -26,7 +27,7 @@ module.exports = {
   buildQuery,
   validContext: node => node.groupField && node.statsField,
   async result(node, search) {
-    let response = await search(await buildQuery(node, getStats(search)))
+    let response = await search(await buildQuery(node, getMinMax(search)))
     return { results: simplifyBuckets(response.aggregations.groups.buckets) }
   },
 }
