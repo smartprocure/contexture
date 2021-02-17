a0fbafb5e5da6fa3d4c0a9de4869d43608cb0893
chris110408 <lei110408@gmail.com>
2021-02-16 22:21:53 -0500

    add unit test
    https://github.com/smartprocure/contexture-mongo/pull/107

--- packages/mongo/src/example-types/facet.js
+++ packages/mongo/src/example-types/facet.js
@@ -141,7 +141,7 @@ module.exports = {
       ]),
     ]).then(([options, cardinality]) => ({
       cardinality: _.get('0.count', cardinality),
-      options: _.map(
+      options: console.log(options)||_.map(
         ({ _id, label, count }) =>
           F.omitNil({
             name: _id,
