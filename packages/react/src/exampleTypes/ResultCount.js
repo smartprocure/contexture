7124f4af72dcaee1956afed4971f7d73c8043b53
Ilian Alexiev <akrion@gmail.com>
2021-03-24 12:54:50 -0700

    ResultsCount support more result nodes
    https://github.com/smartprocure/contexture-react/pull/484

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -4,15 +4,15 @@ import { observer } from 'mobx-react'
 import { withNode, withInlineLoader } from '../utils/hoc'
 import { toNumber } from '../utils/format'
 
-let ResultCount = ({ node, display = toNumber }) => {
+let ResultCount = ({ node = {}, display = toNumber, noResults = 'No Results' }) => {
   let count = F.cascade(
-    ['context.response.results.length', 'context.results.length'],
+    ['context.response.results.length', 'context.results.length', 'context.value'],
     node
-  )
+  ) || _.isNumber(node.context)
   let totalRecords = count
-    ? F.cascade(['context.response.totalRecords', 'context.totalRecords'], node)
+    ? F.cascade(['context.response.totalRecords', 'context.totalRecords', 'context.value', 'context'], node)
     : 0
-  return count ? display(totalRecords) : 'No Results'
+  return count ? display(totalRecords) : noResults
 }
 
 export default _.flow(observer, withNode, withInlineLoader)(ResultCount)
