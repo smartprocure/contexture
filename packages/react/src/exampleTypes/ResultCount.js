7c558843e54623686abdd043c8bf575eaed11a4c
Decrapifier <decrapifier@govspend.us>
2021-03-24 19:58:14 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/484

--- packages/react/src/exampleTypes/ResultCount.js
+++ packages/react/src/exampleTypes/ResultCount.js
@@ -4,13 +4,30 @@ import { observer } from 'mobx-react'
 import { withNode, withInlineLoader } from '../utils/hoc'
 import { toNumber } from '../utils/format'
 
-let ResultCount = ({ node = {}, display = toNumber, noResults = 'No Results' }) => {
-  let count = F.cascade(
-    ['context.response.results.length', 'context.results.length', 'context.value'],
-    node
-  ) || _.isNumber(node.context)
+let ResultCount = ({
+  node = {},
+  display = toNumber,
+  noResults = 'No Results',
+}) => {
+  let count =
+    F.cascade(
+      [
+        'context.response.results.length',
+        'context.results.length',
+        'context.value',
+      ],
+      node
+    ) || _.isNumber(node.context)
   let totalRecords = count
-    ? F.cascade(['context.response.totalRecords', 'context.totalRecords', 'context.value', 'context'], node)
+    ? F.cascade(
+        [
+          'context.response.totalRecords',
+          'context.totalRecords',
+          'context.value',
+          'context',
+        ],
+        node
+      )
     : 0
   return count ? display(totalRecords) : noResults
 }
