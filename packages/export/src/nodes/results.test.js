e7b3e02f60c06c14789a9fb5a69088d9288a3448
Kurt Robert Rudolph <krudolph@govspend.com>
2021-04-06 17:49:38 -0400

    Adjusted the tests so the service is accessible
    In order to put expectation on the what arguments the service is called
    with, we isolate by configuring it in each test.
    
    https://github.com/smartprocure/contexture-export/pull/39

--- packages/export/src/nodes/results.test.js
+++ packages/export/src/nodes/results.test.js
@@ -22,48 +22,48 @@ describe('results', () => {
     { name: 'record3' },
   ]
 
-  let getSimpleService = wrap =>
-    jest.fn(tree => {
-      let response = {
-        totalRecords: 3,
-        results: simpleRecords.map(_source => ({ _source })),
-      }
-      _.last(tree.children).context = wrap ? { response } : response
-      return tree
-    })
+  let resultsTests = ({ wrap }) => {
+    let getSimpleService = () =>
+      jest.fn(tree => {
+        let response = {
+          totalRecords: 3,
+          results: simpleRecords.map(_source => ({ _source })),
+        }
+        _.last(tree.children).context = wrap ? { response } : response
+        return tree
+      })
 
-  let prepareSimpleStrategy = service => async (strategyParams = {}) => {
-    let tree = _.cloneDeep(defaultTree)
-    let include = ['a', 'b', 'c']
-    let strategy = await results({
-      service,
-      tree,
-      pageSize: 3,
-      page: 1,
-      totalPages: 1,
-      include,
-      sortField: 'a',
-      sortDir: 'desc',
-      ...strategyParams,
-    })
-    return strategy
-  }
-  let resultsTests = prepareSimpleStrategy => {
+    let prepareSimpleStrategy = service => async (strategyParams = {}) => {
+      let tree = _.cloneDeep(defaultTree)
+      let include = ['a', 'b', 'c']
+      let strategy = await results({
+        service,
+        tree,
+        pageSize: 3,
+        page: 1,
+        totalPages: 1,
+        include,
+        sortField: 'a',
+        sortDir: 'desc',
+        ...strategyParams,
+      })
+      return strategy
+    }
     it('retrieves the total records', async () => {
-      let strategy = await prepareSimpleStrategy({ totalPages: 1 })
+      let strategy = await prepareSimpleStrategy(getSimpleService())({ totalPages: 1 })
       expect(await strategy.getTotalRecords()).toBe(3)
     })
     it('retrieves records consistently with getNext', async () => {
-      let strategy = await prepareSimpleStrategy({ page: 1 })
+      let strategy = await prepareSimpleStrategy(getSimpleService())({ page: 1 })
       let arr = []
       for await (const i of strategy) arr.push(i)
       expect(arr).toEqual(simpleRecords)
     })
   }
   describe(' with contexts wrapped in `response`', () => {
-    resultsTests(prepareSimpleStrategy(getSimpleService(false)))
+    resultsTests({wrap: false})
   })
   describe(' with contexts not wrapped in `response`', () => {
-    resultsTests(prepareSimpleStrategy(getSimpleService(true)))
+    resultsTests({wrap: true})
   })
 })
