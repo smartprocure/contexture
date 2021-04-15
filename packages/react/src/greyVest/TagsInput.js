5f3017e08480ca31c0f5a33d290cde59d7ac3573
Decrapifier <decrapifier@govspend.com>
2021-04-15 21:35:05 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-react/pull/492

--- packages/react/src/greyVest/TagsInput.js
+++ packages/react/src/greyVest/TagsInput.js
@@ -35,7 +35,9 @@ let TagsInput = forwardRef(
     let sanitizeWords = _.flow(
       _.words,
       _.take(maxWordsPerTag),
-      _.map(word => _.truncate({length: maxCharsPerTagWord, omission: ''}, word)),
+      _.map(word =>
+        _.truncate({ length: maxCharsPerTagWord, omission: '' }, word)
+      ),
       _.join(' ')
     )
 
