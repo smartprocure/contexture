79a15e97582b334b1a2b0c47b0e380d7f25959c0
Douglas Patterson <dpatterson@govspend.com>
2021-04-28 19:14:03 -0700

    feature/174 - WIP regex utils unit tests updates
    https://github.com/smartprocure/contexture-elasticsearch/pull/175

--- packages/elasticsearch/test/utils/regex.js
+++ packages/elasticsearch/test/utils/regex.js
@@ -4,15 +4,15 @@ let { expect } = require('chai')
 describe('regex', () => {
   it('toSafeRegex', () => {
     expect(regex.toSafeRegex('Nuclear ord?')).to.eql(
-      '[Nn][Uu][Cc][Ll][Ee][Aa][Rr] [Oo][Rr][Dd]\\?'
+      'Nuclear ord'
     )
   })
   it('buildRegexQueryForWords', () => {
     expect(regex.buildRegexQueryForWords('_all')('Nuclear Ord')).to.deep.eql({
       bool: {
         must: [
-          { regexp: { _all: '.*([Nn][Uu][Cc][Ll][Ee][Aa][Rr]).*' } },
-          { regexp: { _all: '.*([Oo][Rr][Dd]).*' } },
+          { regexp: { _all: '.*?(Nuclear).*?', case_insensitive: true } },
+          { regexp: { _all: '.*?(Ord).*?', case_insensitive: true } },
         ],
       },
     })
