4a926b5b17ffde5c5cae171ae4daea16f04753bf
Kiryl Yermakou <rma4ok@gmail.com>
2021-05-06 19:27:44 -0400

    [feature] Redis Cache: configurable cache refresh
    https://github.com/smartprocure/contexture-elasticsearch/pull/180

--- packages/elasticsearch/src/utils/redisCache.js
+++ packages/elasticsearch/src/utils/redisCache.js
@@ -1,43 +1,75 @@
+let crypto = require('crypto')
+let _ = require('lodash/fp')
 let F = require('futil')
 let deterministic_stringify = require('json-stable-stringify')
 
-let redisCache = (config, { caching: { ttlSecs } = {} }) => {
-  let redisClient = F.maybeCall(config.getRedisClient)
+let redisCache = (config, schema) => {
   let { redisCache, redisPrefix = 'es-cache:' } = config
+  let redisClient = F.maybeCall(config.getRedisClient)
+  let { caching: { ttlSecs, ttrSecs } = {} } = schema
+  let instaCache = Object.create(null)
 
-  return search => async (request, options) => {
+  return search => (request, options) => {
     if (!redisCache || !redisClient || !ttlSecs) return search(request, options)
 
-    let key = `${redisPrefix}:${deterministic_stringify(request)}`
-    let cachedData = await redisClient.get(key)
+    let key = `${redisPrefix}:${hash(request)}`
 
-    let setData = data => redisClient.setex(key, ttlSecs, JSON.stringify(data))
+    let fetch = async () => {
+      let cachedData = await redisClient.get(key)
 
-    let tryRefresh = async () => {
-      let ttl = await redisClient.ttl(key)
-      // if past half of expiration time
-      if (ttl < ttlSecs / 2) {
-        // this protects from concurrent refreshes
-        // in case ES is down cache will be still working
-        redisClient.expire(key, ttlSecs)
-        // update cache with actual fresh data in background
-        setData(await search(request, options))
+      if (cachedData) {
+        // not awaiting for refresh, so no response delay
+        tryRefresh()
+        return JSON.parse(cachedData)
+      } else {
+        let data = await search(request, options)
+
+        // not awaiting, so we respond to the user faster
+        setData(data)
+
+        return data
       }
     }
 
-    if (cachedData) {
-      // not awaiting for refresh, so no response delay
-      tryRefresh()
-      return JSON.parse(cachedData)
-    } else {
-      let data = await search(request, options)
+    let setData = data =>
+      redisClient.setex(key, ttlSecs, JSON.stringify(data))
 
-      // not awaiting, so we respond to the user faster
-      setData(data)
-
-      return data
+    let tryRefresh = async () => {
+      if (!ttrSecs) return
+      let ttl = await redisClient.ttl(key)
+      // if past specified refresh time
+      if (ttlSecs - ttl >= ttrSecs) {
+        // bumping expiration protects from concurrent refreshes
+        // popular cache is kept alive if ES is down
+        redisClient.expire(key, ttlSecs)
+        // update cache with actual fresh data in background
+        setData(await search(request, {
+          ...options,
+          // delayed non-blocking search
+          runBefore: Date.now() + ttl * 1000
+        }))
+      }
     }
+
+    // to resolve duplicate requests with the same promise
+    let instaCached = f => key => {
+      let promise = instaCache[key]
+      if (!promise) {
+        promise = f(key)
+        promise.finally(() => {
+          delete instaCache[key]
+        })
+      }
+      return promise
+    }
+
+    return instaCached(fetch)()
   }
 }
+
+let hashString = data =>
+  crypto.createHash('sha1').update(data).digest('base64')
+
+let hash = _.pipe(deterministic_stringify, hashString)
 
 module.exports = redisCache
