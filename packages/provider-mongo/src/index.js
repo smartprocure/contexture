9a35fe62951244749146e793adebef4c335b98aa
David Sargeant <david@dsargeant.com>
2021-05-25 16:57:35 -0400

    Debug queries.
    https://github.com/smartprocure/contexture-mongo/pull/118

--- packages/provider-mongo/src/index.js
+++ packages/provider-mongo/src/index.js
@@ -1,12 +1,15 @@
 let Promise = require('bluebird')
+let debugRequest = require('debug')('contexture:mongo:request')
+let debugResponse = require('debug')('contexture:mongo:response')
 
 // Basic function to encapsulate everything needed to run a request - tiny wrapper over raw mongo syntax
 let mongoDSL = (client, dsl) => {
+  debugRequest('Collection: %s. Aggregation: %O', dsl.collection, dsl.aggs)
   let Collection = client.collection(dsl.collection)
   if (dsl.aggs) return Collection.aggregate(dsl.aggs).toArray()
 }
 
-let MongoProvider = config => ({
+let MongoProvider = (config) => ({
   groupCombinator: (group, filters) => ({
     [`$${group.join === 'not' ? 'nor' : group.join}`]: filters,
   }),
@@ -31,7 +34,8 @@ let MongoProvider = config => ({
     node._meta.requests.push(request)
 
     let result = Promise.resolve(mongoDSL(client, request.request))
-    return result.tap(results => {
+    return result.tap((results) => {
+      debugResponse('%O', results)
       // Log response
       request.response = results
     })
