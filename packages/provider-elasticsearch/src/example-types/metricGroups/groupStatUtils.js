326e064bdcf15c57b59ae40423f1006435fde967
Samuel Greene <sam@samuelgreene.info>
2021-09-18 14:45:06 -0700

    fix groupStatUtil bug
    https://github.com/smartprocure/contexture-elasticsearch/pull/190

--- packages/provider-elasticsearch/src/example-types/metricGroups/groupStatUtils.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/groupStatUtils.js
@@ -14,18 +14,22 @@ let buildGroupStatsQuery = buildGroupQuery =>
 // Generic result method - given a buildQuery method, run the search and simplifyBuckets onto results
 let groupStatsResult = buildQuery =>
   async (node, search) => {
-    let response = await search(await buildQuery(node, getStats(search)))
+    let query = await buildQuery(node, getStats(search))
+    let response = await search(query)
     let aggs = response.aggregations.valueFilter || response.aggregations
     return { results: simplifyBuckets( aggs.groups.buckets ) }
   }
 
 // Higher order constructor that stitches it all together - handles most groupStats cases
-let groupStats = buildGroupQuery => ({
-  buildGroupQuery,
-  buildQuery: buildGroupStatsQuery(buildGroupQuery),
-  validContext: node => node.groupField,
-  result: groupStatsResult(buildGroupQuery),
-})
+let groupStats = buildGroupQuery => {
+  let buildQuery = buildGroupStatsQuery(buildGroupQuery)
+  return {
+    buildQuery,
+    buildGroupQuery,
+    validContext: node => node.groupField,
+    result: groupStatsResult(buildQuery),
+  }
+}
 
 module.exports = {
   groupStats,
