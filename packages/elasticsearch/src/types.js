a0c83e5f8a4f7c57b56551b72e70471c52d4098e
Samuel Greene <sam@samuelgreene.info>
2021-09-28 17:19:11 -0700

    register pivot type (can't do it in example types because of circular reference)
    https://github.com/smartprocure/contexture-elasticsearch/pull/190

--- packages/elasticsearch/src/types.js
+++ packages/elasticsearch/src/types.js
@@ -3,5 +3,9 @@ let F = require('futil')
 module.exports = (config = {}) =>
   F.mapValuesIndexed(
     (x, type) => F.callOrReturn(x, config[type]),
-    require('./example-types')
+    { 
+      ...require('./example-types'),
+      // This can't be done in example-types index because it creates a circular dependency (pivot needs all groupStats types)
+      pivot: require('./example-types/metricGroups/pivot')
+    }
   )
