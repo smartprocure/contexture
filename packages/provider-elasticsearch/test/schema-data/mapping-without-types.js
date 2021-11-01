8db6a0b4c43a3da21933dacafa5fea27b8602814
warren vosper <wvosper@govspend.com>
2021-11-01 12:31:50 -0600

    Fix single key mapping
    When an ES mapping only has 1 key (like 'properties') the previous code
    assumed it was an es type which is incorrect and resulted in no schema
    being returned.
    
    This commit:
      - Fixes the bug so is behaves correctly for ES versions 5,6 & 7.
      - Renames the test data files to make them more generic.
        - A future refactor to add improve integration tests will
          bring back the full imdb example.
      - Reduces the amount of test data to make it easier to see the shape.
      - Adds a README file describing the different mapping shapes across
        ES versions.
    
    https://github.com/smartprocure/contexture-elasticsearch/pull/192

---
+++ packages/provider-elasticsearch/test/schema-data/mapping-without-types.js
@@ -0,0 +1,30 @@
+module.exports = {
+  movies: {
+    mappings: {
+      properties: {
+        actors: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 256,
+            },
+          },
+        },
+        awards: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 256,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  weirdEmptyIndexThatWeWillNowIgnore: {
+    mappings: {},
+  },
+}
