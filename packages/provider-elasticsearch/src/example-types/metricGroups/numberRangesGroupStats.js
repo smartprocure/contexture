3ea3ee9dad8667269b8fe3288f21a6e6d9f9a872
Samuel Greene <sam@samuelgreene.info>
2021-12-06 13:12:14 -0500

    add drilldown for fieldValues and numberRanges pivot type
    https://github.com/smartprocure/contexture-elasticsearch/pull/194

--- packages/provider-elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
@@ -1,3 +1,4 @@
+let _ = require('lodash/fp')
 let { groupStats } = require('./groupStatUtils')
 
 let buildGroupQuery = ({ field, ranges }, children) => ({
@@ -9,7 +10,13 @@ let buildGroupQuery = ({ field, ranges }, children) => ({
   },
 })
 
+let drilldown = ({ field, drilldown }) => {
+  let [gte, lte] = _.split('-', drilldown)
+  return ({ range: { [field]: { gte, lte } } })
+}
+
 module.exports = {
   ...groupStats(buildGroupQuery),
   validContext: node => node.groupField && node.ranges,
+  drilldown,
 }
