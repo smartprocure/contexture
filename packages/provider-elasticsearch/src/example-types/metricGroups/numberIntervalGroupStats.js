5f0fc0157c64f24de97ed660ee6089d767a80551
Samuel Greene <sam@samuelgreene.info>
2021-12-07 10:34:34 -0800

    add drilldown to number interval group stats
    https://github.com/smartprocure/contexture-elasticsearch/pull/194

--- packages/provider-elasticsearch/src/example-types/metricGroups/numberIntervalGroupStats.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/numberIntervalGroupStats.js
@@ -1,5 +1,16 @@
+let _ = require('lodash/fp')
 let { calcSmartInterval } = require('../../utils/smartInterval')
 let { groupStats } = require('./groupStatUtils')
+
+let drilldown = async ({ field, drilldown, interval }, schema, getStats) => {
+  let gte = _.toNumber(drilldown)
+  if (interval === 'smart') {
+    let { min, max } = await getStats(field, ['min', 'max'])
+    interval = calcSmartInterval(min, max)
+  }
+  let lt = gte + _.toNumber(interval)
+  return ({ range: { [field]: { gte, lt } } })
+}
 
 let buildGroupQuery = async (node, children, schema, getStats) => {
   let { field, interval = 'smart' } = node
@@ -18,4 +29,7 @@ let buildGroupQuery = async (node, children, schema, getStats) => {
   }
 }
 
-module.exports = groupStats(buildGroupQuery)
+module.exports = {
+  ...groupStats(buildGroupQuery),
+  drilldown,
+}
