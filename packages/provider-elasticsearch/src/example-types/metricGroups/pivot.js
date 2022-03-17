12d3d6c2a1ab1ba68c5a7266a0ed0341bb82999d
Samuel Greene <sam@samuelgreene.info>
2022-03-16 21:11:51 -0700

    crunch
    https://github.com/smartprocure/contexture-elasticsearch/pull/199

--- packages/provider-elasticsearch/src/example-types/metricGroups/pivot.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/pivot.js
@@ -41,28 +41,24 @@ let buildQuery = async (node, schema, getStats) => {
     ? _.take(_.size(node.drilldown) + 1, node.groups)
     : node.groups
 
-  let statsAggBlob = { aggs: aggsForValues(node.values, schema) }
+  let statsAggs = { aggs: aggsForValues(node.values, schema) }
   // buildGroupQuery applied to a list of groups
-  let buildNestedGroupQuery = (statsAggBlob, groups, groupingType) =>
+  let buildNestedGroupQuery = (statsAggs, groups, groupingType) =>
     _.reduce(
       async (children, group) => {
         // Subtotals calculates metrics at each group level, not needed if flattening or in chart
         // Support for per group stats could also be added here - merge on another stats agg blob to children based on group.stats/statsField or group.values
-        if (node.subtotals) children = _.merge(await children, statsAggBlob)
+        if (node.subtotals) children = _.merge(await children, statsAggs)
         let { type } = group
         let build = lookupTypeProp(_.identity, 'buildGroupQuery', type)
         return build(group, await children, groupingType, schema, getStats)
       },
-      statsAggBlob,
+      statsAggs,
       _.reverse(groups)
     )
   if (node.columns)
-    statsAggBlob = await buildNestedGroupQuery(
-      statsAggBlob,
-      node.columns,
-      'columns'
-    )
-  let query = await buildNestedGroupQuery(statsAggBlob, groups, 'groups')
+    statsAggs = await buildNestedGroupQuery(statsAggs, node.columns, 'columns')
+  let query = await buildNestedGroupQuery(statsAggs, groups, 'groups')
 
   let filters = _.compact(
     await Promise.all(
