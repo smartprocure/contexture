fb23c8cd22d0064a8dd53c1d3a8b3af266dcccbf
Samuel Greene <sam@samuelgreene.info>
2022-04-12 20:37:14 -0700

    fix _count -> doc_count bug
    https://github.com/smartprocure/contexture-elasticsearch/pull/205

--- packages/elasticsearch/src/example-types/metricGroups/pivot.js
+++ packages/elasticsearch/src/example-types/metricGroups/pivot.js
@@ -87,7 +87,7 @@ let getSortField = ({
 }) =>
   F.dotJoin([
     _.size(columnValues)
-      ? `sortFilter>${_.isNil(valueIndex) ? '_count' : 'metric'}`
+      ? `sortFilter${_.isNil(valueIndex) ? '.doc_count' : '>metric'}`
       : // If there are no columns, get the generated key for the value or default to _count
         aggKeyForValue(values[valueIndex] || { key: '_count' }),
     valueProp,
