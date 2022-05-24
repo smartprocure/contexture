b3fdd9a5b91f58406e12d6f925404b50518ba570
Samuel Greene <sam@samuelgreene.info>
2022-05-23 18:25:21 -0700

    watchNode deep path and empty array support
    https://github.com/smartprocure/contexture-client/pull/179

--- packages/client/src/listeners.js
+++ packages/client/src/listeners.js
@@ -1,5 +1,5 @@
 let _ = require('lodash/fp')
-import { intersects, eventEmitter } from './util/futil'
+import { eventEmitter, hasSome } from './util/futil'
 import { encode } from './util/tree'
 
 export let setupListeners = tree => {
@@ -8,8 +8,9 @@ export let setupListeners = tree => {
   tree.onChange = (node = {}, delta) => emit(encode(node.path), node, delta)
   // Public API
   tree.watchNode = (path, f, keys) =>
-    on(encode(path), (node, delta) => {
+    on(encode(path), (path, node, delta) => {
       // Trigger watcher if keys match or no keys passed
-      if (!keys || intersects(keys, _.keys(delta))) f(node, delta)
+      if (_.isEmpty(keys) || hasSome(keys, delta)) f(node, delta)
+    })
     })
 }
