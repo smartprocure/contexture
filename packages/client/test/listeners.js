998942c5e4ab68e568941cbdfe8717ddfb914696
Samuel Greene <sam@samuelgreene.info>
2022-05-23 18:28:18 -0700

    test watchNode with keys
    https://github.com/smartprocure/contexture-client/pull/179

--- packages/client/test/listeners.js
+++ packages/client/test/listeners.js
@@ -69,6 +69,60 @@ let AllTests = ContextureClient => {
       expect(filterWatcher).to.have.callCount(4) // mark for update, updating, results, not updating
       expect(resultWatcher).to.have.callCount(9)
     })
+    it('watchNode with keys', async () => {
+      let service = sinon.spy(mockService({ delay: 10 }))
+      let tree = ContextureClient(
+        { service, debounce: 1 },
+        {
+          key: 'root',
+          join: 'and',
+          children: [
+            {
+              key: 'filter',
+              type: 'facet',
+              field: 'facetfield',
+              values: ['some value'],
+            },
+            { key: 'results', type: 'results' },
+          ],
+        }
+      )
+      let filterDom = ''
+      let resultsDom = ''
+      let filterWatcher = sinon.spy(node => {
+        filterDom = `<div>
+  <h1>Facet</h1>
+  <b>Field: ${node.field}</>
+  values: ${_.join(', ', node.values)}
+</div>`
+      })
+      let resultWatcher = sinon.spy((node, delta) => {
+        resultsDom = `<table>${_.map(
+          result =>
+            `\n<tr>${_.map(val => `<td>${val}</td>`, _.values(result))}</tr>`,
+          node.context.results
+        )}
+</table>`
+      })
+      tree.watchNode(['root', 'filter'], filterWatcher, ['field', 'values'])
+      tree.watchNode(['root', 'results'], resultWatcher, ['context.results'])
+      expect(filterDom).to.equal('')
+      let action = tree.mutate(['root', 'filter'], { values: ['other Value'] })
+      expect(filterDom).to.equal(`<div>
+  <h1>Facet</h1>
+  <b>Field: facetfield</>
+  values: other Value
+</div>`)
+      expect(resultsDom).to.equal('') // hasn't run yet
+      await action
+
+      expect(resultsDom).to.equal(`<table>
+<tr><td>some result</td></tr>
+</table>`)
+
+      expect(filterWatcher).to.have.callCount(1) // mark for update, updating, results, not updating
+      expect(resultWatcher).to.have.callCount(1)
+    })
   })
 }
 
