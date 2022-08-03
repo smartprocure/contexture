905e62eaf02107c1716a3bbbdf369d97273a122c
Caden Albaugh <cadenalbaugh@gmail.com>
2022-08-03 09:47:45 -0500

    test: fix all tests associated with this change
    https://github.com/smartprocure/contexture-elasticsearch/pull/221

--- packages/provider-elasticsearch/test/example-types/metricGroups/pivot.js
+++ packages/provider-elasticsearch/test/example-types/metricGroups/pivot.js
@@ -741,11 +741,11 @@ describe('pivot', () => {
               },
             },
           },
-        },
-        aggs: {
-          'pivotMetric-sum-LineItem.TotalPrice': {
-            sum: {
-              field: 'LineItem.TotalPrice',
+          aggs: {
+            'pivotMetric-sum-LineItem.TotalPrice': {
+              sum: {
+                field: 'LineItem.TotalPrice',
+              },
             },
           },
         },
