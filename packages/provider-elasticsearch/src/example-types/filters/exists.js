cec0f47acfc01922e445d666098277bf3ecfd9c9
Kiryl Yermakou <rma4ok@gmail.com>
2022-10-18 19:45:48 -0400

    feat(elasticDSL): and, or, not
    https://github.com/smartprocure/contexture-elasticsearch/pull/223

--- packages/provider-elasticsearch/src/example-types/filters/exists.js
+++ packages/provider-elasticsearch/src/example-types/filters/exists.js
@@ -1,10 +1,10 @@
 let _ = require('lodash/fp')
-let { negate } = require('../../utils/elasticDSL')
+let { not } = require('../../utils/elasticDSL')
 
 module.exports = {
   hasValue: ({ value }) => _.isBoolean(value),
   filter({ field, value }) {
     let filter = { exists: { field } }
-    return value ? filter : negate(filter)
+    return value ? filter : not(filter)
   },
 }
