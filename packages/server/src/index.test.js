39a8fc7b9ed0f80b9768788fb0ebca1dcb7aeed4
Alejandro Hernandez <ahernandez@govspend.com>
2022-10-21 12:23:03 -0400

    Use yarn and esbuild
    https://github.com/smartprocure/contexture/pull/81

--- packages/server/src/index.test.js
+++ packages/server/src/index.test.js
@@ -1,8 +1,8 @@
-let Contexture = require('../src/index')
-let provider = require('../src/provider-debug')
-let _ = require('lodash/fp')
+import Contexture from './index'
+import provider from './provider-debug'
+import _ from 'lodash/fp'
 
-describe('Contexture Core', () => {
+describe('Contexture Server', () => {
   let process = Contexture({
     schemas: {
       test: {
