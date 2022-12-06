73e40b41e7c42c6125a26abbc4ce04598e8d7c24
Kurt Robert Rudolph <krudolph9@gmail.com>
2022-12-06 07:50:07 -0800

    dateIntervalGroupStats federalFiscalQuarter support
    Transform the date interval quarter before sending to elastic
    offset the dates of the date_histogram by 1 quarter forward
    to correspond to the calendarYear date to the federalFiscalYear.
    This allows user to format the date by quarter or year and display
    corresponding federalFiscalYear or federalFiscalQuarter value.
    
    todo:
    - [ ] bucket returned values by year
    
    https://github.com/smartprocure/contexture-elasticsearch/pull/229

--- packages/elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
+++ packages/elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
@@ -1,5 +1,9 @@
-let moment = require('moment-timezone')
+let _ = require('lodash/fp')
+let { set } = require('lodash')
+let F = require('futil')
+let moment = require('moment')
 let { groupStats } = require('./groupStatUtils')
+let { simplifyBuckets } = require('../../utils/elasticDSL')
 
 let drilldown = ({ field, interval, drilldown }) => {
   let gte = drilldown
@@ -9,16 +13,61 @@ let drilldown = ({ field, interval, drilldown }) => {
     .format()
   return { range: { [field]: { gte, lte } } }
 }
-let buildGroupQuery = ({ field, interval = 'year' }, children, groupsKey) => ({
-  aggs: {
-    [groupsKey]: {
-      date_histogram: { field, interval, min_doc_count: 0 },
-      ...children,
-    },
-  },
-})
+let buildGroupQuery = ({ field, interval = 'year' }, children, groupsKey) => {
+  // Federal fiscal year quarters have the
+  // same start and end dates as conventional
+  // calendar year quarters but offset forward by one.
+  // e.g. calendarYear2022Q1 => federalFiscalYear2022Q2
+  //      calendarYear2022Q4 => federalFiscalYear2023Q1
+  // In either federalFiscal interval senario,
+  // elasitc computes a date_histogram using `'quarter'`
+  // and the date offset and
+  // year bucketing (in the case of federalFiscalYear)
+  // are computed in node in the `result` function.
+  if (interval === 'federalFiscalYear' || interval === 'federalFiscalQuarter')
+    interval = 'quarter'
 
+  return {
+    aggs: {
+      [groupsKey]: {
+        date_histogram: { field, interval, min_doc_count: 0 },
+        ...children,
+      },
+    },
+  }
+}
+
+let stats = groupStats(buildGroupQuery)
+let getGroups = aggs => F.unkeyBy('key', aggs.groups.buckets)
 module.exports = {
-  ...groupStats(buildGroupQuery),
+  ...stats,
+  async result(node, search, schema) {
+    let query = stats.buildQuery(node, schema)
+    let response = await search(query)
+
+    if (
+      node.interval === 'federalFiscalYear' ||
+      node.interval === 'federalFiscalQuarter'
+    ) {
+      let offsetBuckets = _.flow(
+        _.get('aggregations.groups.buckets'),
+        _.map(o => {
+          // NOTE: the `TZ` of the running node process must be
+          // `UTC` for this to work as expected.
+          let m = moment(new Date(o.key)).add({ quarter: 1 })
+          return {
+            ...o,
+            key: m.valueOf(),
+            key_as_string: m.toISOString(),
+          }
+        })
+      )(response)
+      set(response, 'aggregations.groups.buckets', offsetBuckets)
+    }
+    if (node.interval === 'federalFiscalYear') {
+      // compute buckets annual buckets
+    }
+    return { results: simplifyBuckets(getGroups(response.aggregations)) }
+  },
   drilldown,
 }
