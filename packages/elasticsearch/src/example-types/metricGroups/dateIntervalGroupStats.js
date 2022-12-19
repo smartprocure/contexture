ed02e1806cc1aa671a38f00a49a6b802aa009835
Kurt Robert Rudolph <krudolph9@gmail.com>
2022-12-19 10:45:09 -0800

    Reworked so it processResponse is pure function that can be called from pivot type
    https://github.com/smartprocure/contexture-elasticsearch/pull/229

--- packages/elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
+++ packages/elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
@@ -1,9 +1,8 @@
 let _ = require('lodash/fp')
-let { set } = require('lodash')
-let F = require('futil')
 let moment = require('moment')
 let { groupStats } = require('./groupStatUtils')
 let { simplifyBuckets } = require('../../utils/elasticDSL')
+let { getStats } = require('./stats')
 
 let drilldown = ({ field, interval, drilldown }) => {
   let gte = drilldown
@@ -92,62 +91,66 @@ let buildGroupQuery = (node, children, groupsKey) => {
   }
 }
 
+let processResponse = (response, node) => {
+  let offsetDates = isOffsetDates(node)
+  if (
+    (node.interval === 'federalFiscalYear' ||
+      node.interval === 'federalFiscalQuarter') &&
+    !offsetDates
+  ) {
+    let offsetBuckets = _.flow(
+      _.get('aggregations.groups.buckets'),
+      _.map(o => {
+        // NOTE: the `TZ` of the running node process must be
+        // `UTC` for this to work as expected.
+        let m = moment(new Date(o.key)).add({ quarter: 1 })
+        return {
+          ...o,
+          key: m.valueOf(),
+          key_as_string: m.toISOString(),
+        }
+      })
+    )(response)
+    response = _.set('aggregations.groups.buckets', offsetBuckets, response)
+  }
+  // bundle up the quarterly bucketed data for a fiscal year
+  if (node.interval === 'federalFiscalYear' && !offsetDates) {
+    let aggregatedBuckets = _.flow(
+      _.get('aggregations.groups.buckets'),
+      _.groupBy(({ key }) => new Date(key).getUTCFullYear()),
+      _.mapValues(
+        _.reduce(
+          (agg, o) => {
+            let m = moment(new Date(o.key)).startOf('year')
+            let rtn = {
+              key: m.valueOf(),
+              key_as_string: m.toISOString(),
+            }
+            rtn.doc_count = agg.doc_count + o.doc_count
+
+            if (o.sum)
+              rtn.sum = { value: _.getOr(0, 'sum.value', agg) + o.sum.value }
+            return rtn
+          },
+          { doc_count: 0 }
+        )
+      ),
+      _.values
+    )(response)
+    response = _.set('aggregations.groups.buckets', aggregatedBuckets, response)
+  }
+  return response
+}
+
 let stats = groupStats(buildGroupQuery)
-let getGroups = aggs => F.unkeyBy('key', aggs.groups.buckets)
 module.exports = {
   ...stats,
-  async result(node, search, schema) {
-    let offsetDates = isOffsetDates(node)
-    let query = stats.buildQuery(node, schema)
-    let response = await search(query)
-
-    if (
-      (node.interval === 'federalFiscalYear' ||
-        node.interval === 'federalFiscalQuarter') &&
-      !offsetDates
-    ) {
-      let offsetBuckets = _.flow(
-        _.get('aggregations.groups.buckets'),
-        _.map(o => {
-          // NOTE: the `TZ` of the running node process must be
-          // `UTC` for this to work as expected.
-          let m = moment(new Date(o.key)).add({ quarter: 1 })
-          return {
-            ...o,
-            key: m.valueOf(),
-            key_as_string: m.toISOString(),
-          }
-        })
-      )(response)
-      set(response, 'aggregations.groups.buckets', offsetBuckets)
-    }
-    // bundle up the quarterly bucketed data for a fiscal year
-    if (node.interval === 'federalFiscalYear' && !isOffsetDates) {
-      let aggregatedBuckets = _.flow(
-        _.get('aggregations.groups.buckets'),
-        _.groupBy(({ key }) => new Date(key).getUTCFullYear()),
-        _.mapValues(
-          _.reduce(
-            (agg, o) => {
-              let m = moment(new Date(o.key)).startOf('year')
-              let rtn = {
-                key: m.valueOf(),
-                key_as_string: m.toISOString(),
-              }
-              rtn.doc_count = agg.doc_count + o.doc_count
-
-              if (o.sum)
-                rtn.sum = { value: _.getOr(0, 'sum.value', agg) + o.sum.value }
-              return rtn
-            },
-            { doc_count: 0 }
-          )
-        ),
-        _.values
-      )(response)
-      set(response, 'aggregations.groups.buckets', aggregatedBuckets)
-    }
-    return { results: simplifyBuckets(getGroups(response.aggregations)) }
-  },
+  processResponse,
   drilldown,
+  async result(node, search, schema) {
+    let query = await stats.buildQuery(node, schema, getStats(search))
+    let response = processResponse(await search(query), node)
+    let aggs = response.aggregations.valueFilter || response.aggregations
+    return { results: simplifyBuckets(aggs.groups.buckets) }
+  },
 }
