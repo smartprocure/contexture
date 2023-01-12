b2efd10581f8e748be38cb9685ddeb07557bdbb0
Caden Albaugh <cadenalbaugh@gmail.com>
2023-01-12 17:42:19 -0600

    feat: implement logic in date/number ranges
    https://github.com/smartprocure/contexture-elasticsearch/pull/247

--- packages/provider-elasticsearch/src/example-types/metricGroups/dateRangesGroupStats.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/dateRangesGroupStats.js
@@ -4,14 +4,35 @@ let { groupStats } = require('./groupStatUtils')
 let splitDrilldown = (drilldown, fn = _.identity) =>
   _.map(fn, _.split(/(?<=Z)-/, drilldown))
 
-let buildGroupQuery = ({ field, ranges }, children, groupingType) => ({
-  aggs: {
-    [groupingType]: {
-      date_range: { field, ranges },
-      ...children,
+let buildGroupQuery = (
+  { field, ranges },
+  children,
+  groupingType,
+  _schema,
+  _getStats,
+  drilldownKey
+) => {
+  if (drilldownKey) {
+    let [from, to] = splitDrilldown(drilldownKey, x =>
+      new Date(x).toISOString()
+    )
+    ranges = _.filter(
+      x =>
+        new Date(x.from).toISOString() === from &&
+        new Date(x.to).toISOString() === to,
+      ranges
+    )
+  }
+
+  return {
+    aggs: {
+      [groupingType]: {
+        date_range: { field, ranges },
+        ...children,
+      },
     },
-  },
-})
+  }
+}
 
 let drilldown = ({ field, drilldown }) => {
   let [gte, lt] = splitDrilldown(drilldown)
@@ -19,23 +40,8 @@ let drilldown = ({ field, drilldown }) => {
   return { range: { [field]: { gte, lt } } }
 }
 
-let filterGroupRanges = (group, drilldown) => {
-  if (drilldown) {
-    let [from, to] = splitDrilldown(drilldown, x => new Date(x).toISOString())
-    group.ranges = _.filter(
-      x =>
-        new Date(x.from).toISOString() === from &&
-        new Date(x.to).toISOString() === to,
-      group.ranges
-    )
-  }
-
-  return group
-}
-
 module.exports = {
   ...groupStats(buildGroupQuery),
   validContext: node => node.groupField && node.ranges,
   drilldown,
-  filterGroupRanges,
 }
--- packages/provider-elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
@@ -1,35 +1,39 @@
 let _ = require('lodash/fp')
 let { groupStats } = require('./groupStatUtils')
 
-let buildGroupQuery = ({ field, ranges }, children, groupingType) => ({
-  aggs: {
-    [groupingType]: {
-      range: { field, ranges },
-      ...children,
+let buildGroupQuery = (
+  { field, ranges },
+  children,
+  groupingType,
+  _schema,
+  _getStats,
+  drilldownKey
+) => {
+  if (drilldownKey) {
+    let [from, to] = _.map(parseFloat, _.split('-', drilldownKey))
+    ranges = _.filter(
+      x => parseFloat(x.from) === from && parseFloat(x.to) === to,
+      ranges
+    )
+  }
+
+  return {
+    aggs: {
+      [groupingType]: {
+        range: { field, ranges },
+        ...children,
+      },
     },
-  },
-})
+  }
+}
 
 let drilldown = ({ field, drilldown }) => {
   let [gte, lt] = _.split('-', drilldown)
   return { range: { [field]: { gte, lt } } }
 }
 
-let filterGroupRanges = (group, drilldownKey) => {
-  if (drilldownKey) {
-    let [from, to] = _.map(parseFloat, _.split('-', drilldownKey))
-    group.ranges = _.filter(
-      x => parseFloat(x.from) === from && parseFloat(x.to) === to,
-      group.ranges
-    )
-  }
-
-  return group
-}
-
 module.exports = {
   ...groupStats(buildGroupQuery),
   validContext: node => node.groupField && node.ranges,
   drilldown,
-  filterGroupRanges,
 }
