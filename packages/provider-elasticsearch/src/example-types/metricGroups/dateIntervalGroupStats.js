1ed76c412a5a92f910aaf98752d83aacf0272bd7
Henry Thornley <hthornley@govspend.com>
2023-01-12 11:47:50 -0800

    Fix Auto Correct Issues
    - Fix code that was auto-formatted 
    https://github.com/smartprocure/contexture-elasticsearch/pull/248

--- packages/provider-elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
+++ packages/provider-elasticsearch/src/example-types/metricGroups/dateIntervalGroupStats.js
@@ -33,14 +33,15 @@ let buildGroupQuery = (node, children, groupsKey) => {
    */
   let untranslatedField = node.field
   field = fieldFiscalMappingOr(field, interval)
-  interval = toElasticInterval(interval) //fiscal only includes quarters and years //should ignore the formatting below, weird formatting needed to be readable wih tests passing.
+  interval = toElasticInterval(interval) //fiscal only includes quarters and years 
 
   /*
    *   hoistProps allows the fields within to be hoisted to top of mapping structure
    *   this is to avoid having issues in which this is not allowed at the same level
    *   of a filter
    */
-  /* prettier-ignore */ let hoistMappings = isFiscal(field) 
+  /* prettier-ignore */ //should ignore the formatting below, weird formatting needed to be readable wih tests passing.
+  let hoistMappings = isFiscal(field) 
     ? {
         hoistProps: {
           runtime_mappings: {
