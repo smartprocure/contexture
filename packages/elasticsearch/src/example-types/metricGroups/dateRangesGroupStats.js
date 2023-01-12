0fdcdf78434c5aad27f330782a832dbfa14d04b4
Caden Albaugh <cadenalbaugh@gmail.com>
2023-01-12 08:37:57 -0600

    refactor: add filterGroupRanges to number/date ranges group stats
    https://github.com/smartprocure/contexture-elasticsearch/pull/247

--- packages/elasticsearch/src/example-types/metricGroups/dateRangesGroupStats.js
+++ packages/elasticsearch/src/example-types/metricGroups/dateRangesGroupStats.js
@@ -16,8 +16,26 @@ let drilldown = ({ field, drilldown }) => {
   return { range: { [field]: { gte, lt } } }
 }
 
+let filterGroupRanges = (group, drilldownKey) => {
+  if (drilldownKey) {
+    let [from, to] = _.map(
+      x => new Date(x).toISOString(),
+      _.split(/(?<=Z)-/, drilldownKey)
+    )
+    group.ranges = _.filter(
+      x =>
+        new Date(x.from).toISOString() === from &&
+        new Date(x.to).toISOString() === to,
+      group.ranges
+    )
+  }
+
+  return group
+}
+
 module.exports = {
   ...groupStats(buildGroupQuery),
   validContext: node => node.groupField && node.ranges,
   drilldown,
+  filterGroupRanges,
 }
--- packages/elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
+++ packages/elasticsearch/src/example-types/metricGroups/numberRangesGroupStats.js
@@ -15,8 +15,21 @@ let drilldown = ({ field, drilldown }) => {
   return { range: { [field]: { gte, lt } } }
 }
 
+let filterGroupRanges = (group, drilldownKey) => {
+  if (drilldownKey) {
+    let [from, to] = _.map(parseFloat, _.split('-', drilldownKey))
+    group.ranges = _.filter(
+      x => parseFloat(x.from) === from && parseFloat(x.to) === to,
+      group.ranges
+    )
+  }
+
+  return group
+}
+
 module.exports = {
   ...groupStats(buildGroupQuery),
   validContext: node => node.groupField && node.ranges,
   drilldown,
+  filterGroupRanges,
 }
