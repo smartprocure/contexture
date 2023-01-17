3a56cf4f9bdeee398d872dccbadaf634ebacdfa4
Caden Albaugh <cadenalbaugh@gmail.com>
2023-01-17 08:37:29 -0600

    chore: convert back to esm and use named export
    https://github.com/smartprocure/contexture-mongo/pull/132

--- packages/provider-mongo/src/example-types/statistical.js
+++ packages/provider-mongo/src/example-types/statistical.js
@@ -1,6 +1,6 @@
 import _ from 'lodash/fp.js'
 
-let statsAgg = (valueField, keyField) => ({
+export let statsAgg = (valueField, keyField) => ({
   $group: {
     _id: keyField ? `$${keyField}` : {},
     count: { $sum: 1 },
@@ -13,7 +13,7 @@ let statsAgg = (valueField, keyField) => ({
 
 let defaults = { count: 0, avg: 0, max: 0, min: 0, sum: 0 }
 
-module.exports = {
+export default {
   statsAgg,
   result: async ({ field }, search) =>
     _.head(await search([statsAgg(field)])) || defaults,
