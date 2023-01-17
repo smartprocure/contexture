b1c145827c2749b03fc79a01193de2bc8110736f
Decrapifier <decrapifier@govspend.com>
2023-01-17 20:58:40 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti
    
    https://github.com/smartprocure/contexture-mongo/pull/134

--- packages/provider-mongo/jest.config.js
+++ packages/provider-mongo/jest.config.js
@@ -10,16 +10,10 @@ export default {
     {
       displayName: 'unit',
       testMatch: ['<rootDir>/test/**/*.js'],
-      transform: {
-        '^.+\\.js?$': ['esbuild-jest', { sourcemap: true, target: 'es2022' }],
-      },
     },
     {
       displayName: 'integration',
       testMatch: ['<rootDir>/integration-test/example-types/**/*.js'],
-      transform: {
-        '^.+\\.js?$': ['esbuild-jest', { sourcemap: true, target: 'es2022' }],
-      },
     },
   ],
   coverageProvider: 'v8',
