92d8f1c23c1fd23847552495edd529c896f7fe8b
Alejandro Hernandez <ahernandez@govspend.com>
2023-01-20 11:20:44 -0500

    Fix exports
    https://github.com/smartprocure/contexture-mongo/pull/135

--- packages/provider-mongo/src/types.js
+++ packages/provider-mongo/src/types.js
@@ -1,10 +1,5 @@
 import F from 'futil'
 import * as exampleTypes from './example-types/index.js'
 
-const unwrapDefault = x => x.default || x
-
 export default (config = {}) =>
-  F.mapValuesIndexed(
-    (x, type) => F.callOrReturn(unwrapDefault(x), config[type]),
-    exampleTypes
-  )
+  F.mapValuesIndexed((x, type) => F.callOrReturn(x, config[type]), exampleTypes)
