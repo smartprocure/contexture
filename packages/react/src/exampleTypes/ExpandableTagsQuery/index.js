6329e0c3b5134aeb21d602caa46d9466e8045991
Lint Action <lint-action@samuelmeuli.com>
2023-06-14 02:30:30 +0000

    Fix code style issues with Prettier

--- packages/react/src/exampleTypes/ExpandableTagsQuery/index.js
+++ packages/react/src/exampleTypes/ExpandableTagsQuery/index.js
@@ -141,7 +141,7 @@ let TagsWrapper = observer(
             `context.keywordGenerations.${props.value}`,
           ],
           node,
-          node.forceFilterOnly ? undefined : 0 
+          node.forceFilterOnly ? undefined : 0
         )
         let tagProps = {
           ...props,
