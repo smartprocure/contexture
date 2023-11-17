8a6b2b470100ce157b9a81833450633f2d68d360
Lint Action <lint-action@samuelmeuli.com>
2023-11-17 04:53:52 +0000

    Fix code style issues with Prettier

--- packages/example-types/build/chunk-2GV37QZA.js
+++ packages/example-types/build/chunk-2GV37QZA.js
@@ -1,7 +1,5 @@
 // src/util/client.js
-import _ from "lodash/fp.js";
-var validateValueExistence = _.flow(_.get("value"), _.negate(_.isNil));
+import _ from 'lodash/fp.js'
+var validateValueExistence = _.flow(_.get('value'), _.negate(_.isNil))
 
-export {
-  validateValueExistence
-};
+export { validateValueExistence }
--- packages/example-types/build/provider-mongo/exists.js
+++ packages/example-types/build/provider-mongo/exists.js
@@ -1,20 +1,21 @@
 // src/types/exists/provider-mongo.js
-import _ from "lodash/fp.js";
+import _ from 'lodash/fp.js'
 var provider_mongo_default = {
   hasValue: ({ value }) => _.isBoolean(value),
-  filter: ({ field, value }) => value ? {
-    $and: [
-      { [field]: { $exists: value, $ne: "" } },
-      { [field]: { $ne: null } }
-    ]
-  } : {
-    $or: [
-      { [field]: { $exists: false } },
-      { [field]: "" },
-      { [field]: null }
-    ]
-  }
-};
-export {
-  provider_mongo_default as default
-};
+  filter: ({ field, value }) =>
+    value
+      ? {
+          $and: [
+            { [field]: { $exists: value, $ne: '' } },
+            { [field]: { $ne: null } },
+          ],
+        }
+      : {
+          $or: [
+            { [field]: { $exists: false } },
+            { [field]: '' },
+            { [field]: null },
+          ],
+        },
+}
+export { provider_mongo_default as default }
