9c32f126d5678cf5943310b7b0633d68399c5210
Henry Thornley <hankthornlee@gmail.com>
2024-07-30 14:53:33 -0700

    Fix Show More Button

--- packages/provider-elasticsearch/src/example-types/filters/facet.js
+++ packages/provider-elasticsearch/src/example-types/filters/facet.js
@@ -61,15 +61,16 @@ export default {
         name: x.key,
         count: x.doc_count,
       })),
-      cardinality: node.includeZeroes
-        ? _.get(
-            'aggregations.facetCardinality.value',
-            await search({
-              aggs: { facetCardinality: { cardinality: { field } } },
-              query: { match_all: {} },
-            })
-          )
-        : agg.facetCardinality.value,
+      cardinality:
+        node.includeZeroes && !node.optionsFilter
+          ? _.get(
+              'aggregations.facetCardinality.value',
+              await search({
+                aggs: { facetCardinality: { cardinality: { field } } },
+                query: { match_all: {} },
+              })
+            )
+          : agg.facetCardinality.value,
     }
 
     // Get missing counts for values sent up but not included in the results
