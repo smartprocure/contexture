70d9500a90dfceef1721682d3092121198945393
Sandhya Govindaraju <sgovindaraju@govspend.com>
2024-09-19 13:15:56 -0500

    DEV: setup workspaces and coverage

---
+++ vitest.workspace.ts
@@ -0,0 +1,41 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  {
+    test: {
+      name: 'client',
+      include: ['packages/client/src/**/*.test.js'],
+    },
+  },
+  {
+    test: {
+      name: 'server',
+      include: ['packages/server/src/**/*.test.js'],
+    },
+  },
+  {
+    test: {
+      name: 'provider-elasticsearch',
+      include: ['packages/provider-elasticsearch/src/**/*.test.js'],
+    },
+  },
+  {
+    test: {
+      name: 'export',
+      include: ['packages/export/src/**/*.test.js'],
+    },
+  },
+  {
+    test: {
+      name: 'provider-mongo',
+      include: ['packages/provider-mongo/src/**/*.test.js'],
+      globalSetup: './packages/provider-mongo/jest/globalSetup.js',
+    },
+  },
+  {
+    test: {
+      name: 'util',
+      include: ['packages/util/src/**/*.test.js'],
+    },
+  },
+])
